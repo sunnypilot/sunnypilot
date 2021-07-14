a61d0845961ee13a9f07ee4508d5135b51205afb
sunnyhaibin <haibin.wen3@gmail.com>
2021-07-14 15:13:19 -0400

    Revert "Create /data/openpilot/preset1 for panda flashing"
    This reverts commit 91fd2e11d723c7e8e72806e211ad904159dbaedf.

--- save_preset1.sh
+++ save_preset1.sh
@@ -7,13 +7,13 @@ export PYTHONPATH=/data/openpilot
 
 cd /data/openpilot
 
-if [ -e /data/openpilot/preset1 ]; then
-rm -f /data/openpilot/preset1
+if [ -e /data/preset1 ]; then
+rm -f /data/preset1
 fi
 
 /data/data/com.termux/files/usr/bin/cat param_init_value | while read line
 do
 ParamName=$(/data/data/com.termux/files/usr/bin/echo $line | /data/data/com.termux/files/usr/bin/applets/awk -F ':' '{print $1}')
 ParamValue=$(/data/data/com.termux/files/usr/bin/cat /data/params/d/${ParamName})
-/data/data/com.termux/files/usr/bin/echo "$ParamName:$ParamValue" >> /data/openpilot/preset1
+/data/data/com.termux/files/usr/bin/echo "$ParamName:$ParamValue" >> /data/preset1
 done
