146506c21987719e323bfd934e474169cb1b90e8
sunnyhaibin <haibin.wen3@gmail.com>
2021-07-18 10:02:47 -0400

    Update SteerRatioAdj & SteerRatioMaxAdj: 2021 Hyundai Sonata N Line SR 12.35:1

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -180,7 +180,7 @@ class Controls:
     
     self.mpc_frame = 0
 
-    self.steerRatio_Max = float(Decimal(params.get("SteerRatioMaxAdj", encoding="utf8")) * Decimal('0.1'))
+    self.steerRatio_Max = float(Decimal(params.get("SteerRatioMaxAdj", encoding="utf8")) * Decimal('0.0001'))
     self.angle_differ_range = [0, 15]
     self.steerRatio_range = [self.CP.steerRatio, self.steerRatio_Max]
     self.new_steerRatio = self.CP.steerRatio
