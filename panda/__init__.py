ae20f41a4f7170a125dfe3d6afd6d07938843e4b
sunnyhaibin <haibin.wen3@gmail.com>
2021-08-22 00:12:03 -0400

    Remove files

--- panda/__init__.py
+++
@@ -1,3 +0,0 @@
-# flake8: noqa
-# pylint: skip-file
-from .python import Panda, PandaWifiStreaming, PandaDFU, flash_release, BASEDIR, ensure_st_up_to_date, PandaSerial
--- panda/board/pedal/flash_can.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-set -e
-
-cd ..
-PEDAL=1 scons -u
-cd pedal
-
-../../tests/pedal/enter_canloader.py ../obj/pedal.bin.signed
