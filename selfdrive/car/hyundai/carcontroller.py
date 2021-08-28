6fd250685388cac6d1cc6715707ed04004fb6a5d
sunnyhaibin <haibin.wen3@gmail.com>
2021-08-28 03:16:13 -0400

    MADS (MAD Safety) Mode - Hyundai/Kia/Genesis

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -37,6 +37,8 @@ class CarController():
     self.p = CarControllerParams(CP)
     self.packer = CANPacker(dbc_name)
 
+    self.signal_last = 0.
+    self.disengage_blink = 0.
     self.apply_steer_last = 0
     self.car_fingerprint = CP.carFingerprint
     self.steer_rate_limited = False
@@ -49,8 +51,12 @@ class CarController():
     apply_steer = apply_std_steer_torque_limits(new_steer, self.apply_steer_last, CS.out.steeringTorque, self.p)
     self.steer_rate_limited = new_steer != apply_steer
 
+    cur_time = frame * DT_CTRL
+    if (CS.leftBlinkerOn or CS.rightBlinkerOn):
+      self.signal_last = cur_time
+
     # disable when temp fault is active, or below LKA minimum speed
-    lkas_active = enabled and not CS.out.steerWarning and CS.out.vEgo >= CS.CP.minSteerSpeed
+    lkas_active = enabled and not CS.out.steerWarning and CS.out.vEgo >= CS.CP.minSteerSpeed and (CS.lfaEnabled or CS.accMainEnabled) and ((CS.automaticLaneChange and not CS.belowLaneChangeSpeed) or ((not ((cur_time - self.signal_last) < 1) or not CS.belowLaneChangeSpeed) and not (CS.leftBlinkerOn or CS.rightBlinkerOn)))
 
     if not lkas_active:
       apply_steer = 0
@@ -62,8 +68,20 @@ class CarController():
                         left_lane, right_lane, left_lane_depart, right_lane_depart)
 
     can_sends = []
+
+    # show LFA "white_wheel" and LKAS "White car + lanes" when disengageFromBrakes
+    disengage_from_brakes = (CS.lfaEnabled or CS.accMainEnabled) and not lkas_active
+
+    # show LFA "white_wheel" and LKAS "White car + lanes" when belowLaneChangeSpeed and (leftBlinkerOn or rightBlinkerOn)
+    below_lane_change_speed = (CS.lfaEnabled or CS.accMainEnabled) and CS.belowLaneChangeSpeed and (CS.leftBlinkerOn or CS.rightBlinkerOn)
+
+    if not (disengage_from_brakes or below_lane_change_speed):
+      self.disengage_blink = cur_time
+
+    disengage_blinking_icon = (disengage_from_brakes or below_lane_change_speed) and not ((cur_time - self.disengage_blink) > 1)
+
     can_sends.append(create_lkas11(self.packer, frame, self.car_fingerprint, apply_steer, lkas_active,
-                                   CS.lkas11, sys_warning, sys_state, enabled,
+                                   CS.lkas11, sys_warning, sys_state, enabled, lkas_active, disengage_from_brakes, below_lane_change_speed, disengage_blinking_icon,
                                    left_lane, right_lane,
                                    left_lane_warning, right_lane_warning))
 
@@ -80,6 +98,6 @@ class CarController():
     if frame % 5 == 0 and self.car_fingerprint in [CAR.SONATA, CAR.PALISADE, CAR.IONIQ, CAR.KIA_NIRO_EV, CAR.KIA_NIRO_HEV_2021,
                                                    CAR.IONIQ_EV_2020, CAR.IONIQ_PHEV, CAR.KIA_CEED, CAR.KIA_SELTOS, CAR.KONA_EV,
                                                    CAR.ELANTRA_2021, CAR.ELANTRA_HEV_2021, CAR.SONATA_HYBRID, CAR.KONA_HEV]:
-      can_sends.append(create_lfahda_mfc(self.packer, enabled))
+      can_sends.append(create_lfahda_mfc(self.packer, enabled, lkas_active, disengage_from_brakes, below_lane_change_speed, disengage_blinking_icon))
 
     return can_sends
