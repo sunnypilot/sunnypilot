f2efcc9a3be1b15df2c29ab34fe56c38385d2886
Jason Wen <haibin.wen3@gmail.com>
2022-04-05 02:46:50 -0400

    Temp - Turn off lowBattery alert

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -205,10 +205,10 @@ class Controls:
       return
 
     # Create events for battery, temperature, disk space, and memory
-    if EON and (self.sm['peripheralState'].pandaType != PandaType.uno) and \
-       self.sm['deviceState'].batteryPercent < 1 and self.sm['deviceState'].chargingError:
-      # at zero percent battery, while discharging, OP should not allowed
-      self.events.add(EventName.lowBattery)
+    #if EON and (self.sm['peripheralState'].pandaType != PandaType.uno) and \
+    #   self.sm['deviceState'].batteryPercent < 1 and self.sm['deviceState'].chargingError:
+    #  # at zero percent battery, while discharging, OP should not allowed
+    #  self.events.add(EventName.lowBattery)
     if self.sm['deviceState'].thermalStatus >= ThermalStatus.red:
       self.events.add(EventName.overheat)
     if self.sm['deviceState'].freeSpacePercent < 7 and not SIMULATION:
