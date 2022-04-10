rarul05/openpilot

sunnypilot v0.8.12-4 update

selfdrive/gpxd/gpx_uploader.py
@@ -1,0 +1,146 @@
+#!/usr/bin/env python3
+# The MIT License
+#
+# Copyright (c) 2019-, Rick Lan, dragonpilot community, and a number of other of contributors.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+import os
+import time
+from common.params import Params
+from selfdrive.version import get_version
+
+# for uploader
+from selfdrive.loggerd.xattr_cache import getxattr, setxattr
+import glob
+import requests
+
+# customisable values
+GPX_LOG_PATH = '/data/media/0/gpx_logs/'
+LOG_HERTZ = 1/10 # 0.1 Hz = 10 sec, higher for higher accuracy, 10hz seems fine
+
+# uploader
+UPLOAD_ATTR_NAME = 'user.upload'
+UPLOAD_ATTR_VALUE = b'1'
+LOG_PATH = '/data/media/0/gpx_logs/'
+
+# osm api
+API_HEADER = {'Authorization': 'Bearer DjlXVi1kL9bQ4GBj_3ixiKufVXzsHhpYCTi3ayNYw7k'}
+VERSION_URL = 'https://api.openstreetmap.org/api/versions'
+UPLOAD_URL = 'https://api.openstreetmap.org/api/0.6/gpx/create'
+
+_DEBUG = False
+
+def _debug(msg):
+  if not _DEBUG:
+    return
+  print(msg, flush=True)
+
+class GpxUploader():
+  def __init__(self):
+    self._delete_after_upload = Params().get_bool('GpxDeleteAfterUpload')
+    self._delete_if_uploaded = Params().get_bool("GpxDeleteIfUploaded")
+    self._car_model = Params().get("CarModel", encoding='utf8')
+    self._dp_version = get_version()
+    _debug("GpxUploader init - _delete_after_upload = %s" % self._delete_after_upload)
+    _debug("GpxUploader init - _car_model = %s" % self._car_model)
+
+  def _is_online(self):
+    try:
+      r = requests.get(VERSION_URL, headers=API_HEADER)
+      _debug("is_online? status_code = %s" % r.status_code)
+      return r.status_code >= 200
+    except:
+      return False
+
+  def _get_is_uploaded(self, filename):
+    _debug("%s is uploaded: %s" % (filename, getxattr(filename, UPLOAD_ATTR_NAME) is not None))
+    return getxattr(filename, UPLOAD_ATTR_NAME) is not None
+
+  def _set_is_uploaded(self, filename):
+    _debug("%s set to uploaded" % filename)
+    setxattr(filename, UPLOAD_ATTR_NAME, UPLOAD_ATTR_VALUE)
+
+  def _get_files(self):
+    return sorted( filter( os.path.isfile, glob.glob(LOG_PATH + '*') ) )
+
+  def _get_files_to_be_uploaded(self):
+    files = self._get_files()
+    files_to_be_uploaded = []
+    for file in files:
+      if not self._get_is_uploaded(file):
+        files_to_be_uploaded.append(file)
+    return files_to_be_uploaded
+
+  def _get_files_to_be_deleted(self):
+    files = self._get_files()
+    files_to_be_deleted = []
+    for file in files:
+      if self._get_is_uploaded(file):
+        files_to_be_deleted.append(file)
+    return files_to_be_deleted
+
+  def _do_upload(self, filename):
+    fn = os.path.basename(filename)
+    data = {
+      'description': "Routes from sunnypilot %s (%s)." % (self._dp_version, self._car_model),
+      'visibility': 'identifiable'
+    }
+    files = {
+      "file": (fn, open(filename, 'rb'))
+    }
+    try:
+      r = requests.post(UPLOAD_URL, files=files, data=data, headers=API_HEADER)
+      _debug("do_upload - %s - %s" % (filename, r.status_code))
+      return r.status_code == 200
+    except:
+      return False
+
+  def run(self):
+    while True:
+      files = self._get_files_to_be_uploaded()
+      files_mark_delete = self._get_files_to_be_deleted()
+      for file in files_mark_delete:
+        if self._delete_if_uploaded:
+          _debug("run - _delete_if_uploaded")
+          os.remove(file)
+      if len(files) == 0 or not self._is_online():
+        _debug("run - not online or no files")
+      elif not Params().get_bool("DisableOnroadUploads") or Params().get_bool("IsOffroad"):
+        for file in files:
+          if self._do_upload(file):
+            if self._delete_after_upload:
+              _debug("run - _delete_after_upload")
+              os.remove(file)
+            else:
+              _debug("run - set_is_uploaded")
+              self._set_is_uploaded(file)
+      # sleep for 300 secs if offroad
+      # otherwise sleep 60 secs
+      time.sleep(300 if Params().get_bool("IsOffroad") else 60)
+
+def gpx_uploader_thread():
+  gpx_uploader = GpxUploader()
+  gpx_uploader.run()
+
+def main():
+  gpx_uploader_thread()
+
+if __name__ == "__main__":
+  main()

selfdrive/ui/qt/onroad.h
@@ -2,12 +2,28 @@
 
 #include <QStackedLayout>
 #include <QWidget>
+#include <QPushButton>
 
 #include "selfdrive/ui/qt/widgets/cameraview.h"
 #include "selfdrive/ui/ui.h"
 
 
 // ***** onroad widgets *****
+
+class ButtonsWindow : public QWidget {
+  Q_OBJECT
+
+public:
+  ButtonsWindow(QWidget* parent = 0);
+
+private:
+  QPushButton *dlpBtn;
+
+  const QStringList dlpBtnColors = {"#007d00", "#c92231", "#7d007d"};
+
+public slots:
+  void updateState(const UIState &s);
+};
 
 class OnroadHud : public QWidget {
   Q_OBJECT
@@ -19,6 +35,50 @@ class OnroadHud : public QWidget {
   Q_PROPERTY(bool dmActive MEMBER dmActive NOTIFY valueChanged);
   Q_PROPERTY(bool hideDM MEMBER hideDM NOTIFY valueChanged);
   Q_PROPERTY(int status MEMBER status NOTIFY valueChanged);
+  Q_PROPERTY(bool is_brakelight_on MEMBER is_brakelight_on NOTIFY valueChanged);
+  Q_PROPERTY(bool madsEnabled MEMBER madsEnabled NOTIFY valueChanged);
+  Q_PROPERTY(bool suspended MEMBER suspended NOTIFY valueChanged);
+
+  Q_PROPERTY(bool showHowAlert MEMBER showHowAlert NOTIFY valueChanged);
+  Q_PROPERTY(bool howWarning MEMBER howWarning NOTIFY valueChanged);
+
+  Q_PROPERTY(bool showVTC MEMBER showVTC NOTIFY valueChanged);
+  Q_PROPERTY(QString vtcSpeed MEMBER vtcSpeed NOTIFY valueChanged);
+  Q_PROPERTY(QColor vtcColor MEMBER vtcColor NOTIFY valueChanged);
+  Q_PROPERTY(bool showDebugUI MEMBER showDebugUI NOTIFY valueChanged);
+
+Q_PROPERTY(QString roadName MEMBER roadName NOTIFY valueChanged);
+
+  Q_PROPERTY(bool showSpeedLimit MEMBER showSpeedLimit NOTIFY valueChanged);
+  Q_PROPERTY(QString speedLimit MEMBER speedLimit NOTIFY valueChanged);
+  Q_PROPERTY(QString slcSubText MEMBER slcSubText NOTIFY valueChanged);
+  Q_PROPERTY(float slcSubTextSize MEMBER slcSubTextSize NOTIFY valueChanged);
+  Q_PROPERTY(bool mapSourcedSpeedLimit MEMBER mapSourcedSpeedLimit NOTIFY valueChanged);
+  Q_PROPERTY(bool slcActive MEMBER slcActive NOTIFY valueChanged);
+
+  Q_PROPERTY(bool showTurnSpeedLimit MEMBER showTurnSpeedLimit NOTIFY valueChanged);
+  Q_PROPERTY(QString turnSpeedLimit MEMBER turnSpeedLimit NOTIFY valueChanged);
+  Q_PROPERTY(QString tscSubText MEMBER tscSubText NOTIFY valueChanged);
+  Q_PROPERTY(bool tscActive MEMBER tscActive NOTIFY valueChanged);
+  Q_PROPERTY(int curveSign MEMBER curveSign NOTIFY valueChanged);
+
+  Q_PROPERTY(int lead_status MEMBER lead_status NOTIFY valueChanged);
+  Q_PROPERTY(float lead_d_rel MEMBER lead_d_rel NOTIFY valueChanged);
+  Q_PROPERTY(float lead_v_rel MEMBER lead_v_rel NOTIFY valueChanged);
+  Q_PROPERTY(float angleSteers MEMBER angleSteers NOTIFY valueChanged);
+  Q_PROPERTY(float steerAngleDesired MEMBER steerAngleDesired NOTIFY valueChanged);
+  Q_PROPERTY(int engineRPM MEMBER engineRPM NOTIFY valueChanged);
+  Q_PROPERTY(int devUiEnabled MEMBER devUiEnabled NOTIFY valueChanged);
+  Q_PROPERTY(float gpsAccuracy MEMBER gpsAccuracy NOTIFY valueChanged);
+  Q_PROPERTY(float altitude MEMBER altitude NOTIFY valueChanged);
+  Q_PROPERTY(float vEgo MEMBER vEgo NOTIFY valueChanged);
+  Q_PROPERTY(float aEgo MEMBER aEgo NOTIFY valueChanged);
+  Q_PROPERTY(float steeringTorque MEMBER steeringTorque NOTIFY valueChanged);
+  Q_PROPERTY(float bearingAccuracyDeg MEMBER bearingAccuracyDeg NOTIFY valueChanged);
+  Q_PROPERTY(float bearingDeg MEMBER bearingDeg NOTIFY valueChanged);
+
+  Q_PROPERTY(bool standStill MEMBER standStill NOTIFY valueChanged);
+  Q_PROPERTY(int standstillElapsedTime MEMBER standstillElapsedTime NOTIFY valueChanged);
 
 public:
   explicit OnroadHud(QWidget *parent);
@@ -27,12 +87,36 @@ public:
 private:
   void drawIcon(QPainter &p, int x, int y, QPixmap &img, QBrush bg, float opacity);
   void drawText(QPainter &p, int x, int y, const QString &text, int alpha = 255);
+  void drawSpeedText(QPainter &p, int x, int y, const QString &text, QColor color);
+  void drawCenteredText(QPainter &p, int x, int y, const QString &text, QColor color);
+  void drawVisionTurnControllerUI(QPainter &p, int x, int y, int size, const QColor &color, const QString &speed,
+                                  int alpha);
+  void drawCircle(QPainter &p, int x, int y, int r, QBrush bg);
+  void drawSpeedSign(QPainter &p, QRect rc, const QString &speed, const QString &sub_text, int subtext_size,
+                     bool is_map_sourced, bool is_active);
+  void drawTrunSpeedSign(QPainter &p, QRect rc, const QString &speed, const QString &sub_text, int curv_sign,
+                         bool is_active);
+  void drawMadsIcon(QPainter &p, int x, int y, QPixmap &img, QBrush bg, float opacity);
+  void drawStandstillTimerText(QPainter &p, int x, int y, const char* label, const char* value, QColor &color1, QColor &color2);
+  void drawRightDevUi(QPainter &p, int x, int y);
+  void drawRightDevUi2(QPainter &p, int x, int y);
+  void drawRightDevUiBorder(QPainter &p, int x, int y);
+  void drawStandstillTimer(QPainter &p, int x, int y);
+  int drawDevUiElementRight(QPainter &p, int x, int y, const char* value, const char* label, const char* units, QColor &color);
+  int drawDevUiElementLeft(QPainter &p, int x, int y, const char* value, const char* label, const char* units, QColor &color);
+  void drawColoredText(QPainter &p, int x, int y, const QString &text, QColor &color);
   void paintEvent(QPaintEvent *event) override;
 
   QPixmap engage_img;
   QPixmap dm_img;
+  QPixmap how_img;
+  QPixmap map_img;
+  QPixmap left_img;
+  QPixmap right_img;
+  QPixmap mads_imgs[2];
   const int radius = 192;
   const int img_size = (radius / 2) * 1.5;
+  const int subsign_img_size = 35;
   QString speed;
   QString speedUnit;
   QString maxSpeed;
@@ -41,6 +125,50 @@ private:
   bool dmActive = false;
   bool hideDM = false;
   int status = STATUS_DISENGAGED;
+  bool is_brakelight_on = false;
+  bool madsEnabled = false;
+  bool suspended = false;
+
+  bool showHowAlert = false;
+  bool howWarning = false;
+
+  bool showVTC = false;
+  QString vtcSpeed;
+  QColor vtcColor;
+  bool showDebugUI = false;
+
+  QString roadName;
+
+  bool showSpeedLimit = false;
+  QString speedLimit;
+  QString slcSubText;
+  float slcSubTextSize = 0.0;
+  bool mapSourcedSpeedLimit = false;
+  bool slcActive = false;
+
+  bool showTurnSpeedLimit = false;
+  QString turnSpeedLimit;
+  QString tscSubText;
+  bool tscActive = false;
+  int curveSign = 0;
+
+  int lead_status;
+  float lead_d_rel = 0;
+  float lead_v_rel = 0;
+  float angleSteers = 0;
+  float steerAngleDesired = 0;
+  int engineRPM = 0;
+  int devUiEnabled;
+  float gpsAccuracy;
+  float altitude;
+  float vEgo;
+  float aEgo;
+  float steeringTorque;
+  float bearingAccuracyDeg;
+  float bearingDeg;
+
+  bool standStill;
+  int standstillElapsedTime;
 
 signals:
   void valueChanged();
@@ -76,7 +204,13 @@ protected:
   void drawLaneLines(QPainter &painter, const UIScene &scene);
   void drawLead(QPainter &painter, const cereal::ModelDataV2::LeadDataV3::Reader &lead_data, const QPointF &vd);
   inline QColor redColor(int alpha = 255) { return QColor(201, 34, 49, alpha); }
+  inline QColor blackColor(int alpha = 255) { return QColor(0, 0, 0, alpha); }
+  inline QColor greenColor(int alpha = 255) { return QColor(49, 201, 34, alpha); }
+  inline QColor graceBlueColor(int alpha = 255) { return QColor (34, 49, 201, alpha); }
   double prev_draw_t = 0;
+
+signals:
+  void resizeSignal(int w);
 };
 
 // container for all onroad widgets
@@ -93,6 +227,7 @@ private:
   OnroadHud *hud;
   OnroadAlerts *alerts;
   NvgWindow *nvg;
+  ButtonsWindow *buttons;
   QColor bg = bg_colors[STATUS_DISENGAGED];
   QWidget *map = nullptr;
   QHBoxLayout* split;

sunnypilot/sunnypilot

selfdrive/gpxd/gpx_uploader.py
@@ -1,0 +1,146 @@
+#!/usr/bin/env python3
+# The MIT License
+#
+# Copyright (c) 2019-, Rick Lan, dragonpilot community, and a number of other of contributors.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+import os
+import time
+from common.params import Params
+from selfdrive.version import get_version
+
+# for uploader
+from selfdrive.loggerd.xattr_cache import getxattr, setxattr
+import glob
+import requests
+
+# customisable values
+GPX_LOG_PATH = '/data/media/0/gpx_logs/'
+LOG_HERTZ = 1/10 # 0.1 Hz = 10 sec, higher for higher accuracy, 10hz seems fine
+
+# uploader
+UPLOAD_ATTR_NAME = 'user.upload'
+UPLOAD_ATTR_VALUE = b'1'
+LOG_PATH = '/data/media/0/gpx_logs/'
+
+# osm api
+API_HEADER = {'Authorization': 'Bearer DjlXVi1kL9bQ4GBj_3ixiKufVXzsHhpYCTi3ayNYw7k'}
+VERSION_URL = 'https://api.openstreetmap.org/api/versions'
+UPLOAD_URL = 'https://api.openstreetmap.org/api/0.6/gpx/create'
+
+_DEBUG = False
+
+def _debug(msg):
+  if not _DEBUG:
+    return
+  print(msg, flush=True)
+
+class GpxUploader():
+  def __init__(self):
+    self._delete_after_upload = Params().get_bool('GpxDeleteAfterUpload')
+    self._delete_if_uploaded = Params().get_bool("GpxDeleteIfUploaded")
+    self._car_model = Params().get("LastCarModel", encoding='utf8')
+    self._dp_version = get_version()
+    _debug("GpxUploader init - _delete_after_upload = %s" % self._delete_after_upload)
+    _debug("GpxUploader init - _car_model = %s" % self._car_model)
+
+  def _is_online(self):
+    try:
+      r = requests.get(VERSION_URL, headers=API_HEADER)
+      _debug("is_online? status_code = %s" % r.status_code)
+      return r.status_code >= 200
+    except:
+      return False
+
+  def _get_is_uploaded(self, filename):
+    _debug("%s is uploaded: %s" % (filename, getxattr(filename, UPLOAD_ATTR_NAME) is not None))
+    return getxattr(filename, UPLOAD_ATTR_NAME) is not None
+
+  def _set_is_uploaded(self, filename):
+    _debug("%s set to uploaded" % filename)
+    setxattr(filename, UPLOAD_ATTR_NAME, UPLOAD_ATTR_VALUE)
+
+  def _get_files(self):
+    return sorted( filter( os.path.isfile, glob.glob(LOG_PATH + '*') ) )
+
+  def _get_files_to_be_uploaded(self):
+    files = self._get_files()
+    files_to_be_uploaded = []
+    for file in files:
+      if not self._get_is_uploaded(file):
+        files_to_be_uploaded.append(file)
+    return files_to_be_uploaded
+
+  def _get_files_to_be_deleted(self):
+    files = self._get_files()
+    files_to_be_deleted = []
+    for file in files:
+      if self._get_is_uploaded(file):
+        files_to_be_deleted.append(file)
+    return files_to_be_deleted
+
+  def _do_upload(self, filename):
+    fn = os.path.basename(filename)
+    data = {
+      'description': "Routes from sunnypilot %s (%s)." % (self._dp_version, self._car_model),
+      'visibility': 'identifiable'
+    }
+    files = {
+      "file": (fn, open(filename, 'rb'))
+    }
+    try:
+      r = requests.post(UPLOAD_URL, files=files, data=data, headers=API_HEADER)
+      _debug("do_upload - %s - %s" % (filename, r.status_code))
+      return r.status_code == 200
+    except:
+      return False
+
+  def run(self):
+    while True:
+      files = self._get_files_to_be_uploaded()
+      files_mark_delete = self._get_files_to_be_deleted()
+      for file in files_mark_delete:
+        if self._delete_if_uploaded:
+          _debug("run - _delete_if_uploaded")
+          os.remove(file)
+      if len(files) == 0 or not self._is_online():
+        _debug("run - not online or no files")
+      elif not Params().get_bool("DisableOnroadUploads") or Params().get_bool("IsOffroad"):
+        for file in files:
+          if self._do_upload(file):
+            if self._delete_after_upload:
+              _debug("run - _delete_after_upload")
+              os.remove(file)
+            else:
+              _debug("run - set_is_uploaded")
+              self._set_is_uploaded(file)
+      # sleep for 300 secs if offroad
+      # otherwise sleep 60 secs
+      time.sleep(300 if Params().get_bool("IsOffroad") else 60)
+
+def gpx_uploader_thread():
+  gpx_uploader = GpxUploader()
+  gpx_uploader.run()
+
+def main():
+  gpx_uploader_thread()
+
+if __name__ == "__main__":
+  main()

selfdrive/ui/qt/onroad.h
@@ -2,12 +2,28 @@
 
 #include <QStackedLayout>
 #include <QWidget>
+#include <QPushButton>
 
 #include "selfdrive/ui/qt/widgets/cameraview.h"
 #include "selfdrive/ui/ui.h"
 
 
 // ***** onroad widgets *****
+
+class ButtonsWindow : public QWidget {
+  Q_OBJECT
+
+public:
+  ButtonsWindow(QWidget* parent = 0);
+
+private:
+  QPushButton *dlpBtn;
+
+  const QStringList dlpBtnColors = {"#007d00", "#c92231", "#7d007d"};
+
+public slots:
+  void updateState(const UIState &s);
+};
 
 class OnroadHud : public QWidget {
   Q_OBJECT
@@ -19,6 +35,50 @@ class OnroadHud : public QWidget {
   Q_PROPERTY(bool dmActive MEMBER dmActive NOTIFY valueChanged);
   Q_PROPERTY(bool hideDM MEMBER hideDM NOTIFY valueChanged);
   Q_PROPERTY(int status MEMBER status NOTIFY valueChanged);
+  Q_PROPERTY(bool is_brakelight_on MEMBER is_brakelight_on NOTIFY valueChanged);
+  Q_PROPERTY(bool madsEnabled MEMBER madsEnabled NOTIFY valueChanged);
+  Q_PROPERTY(bool suspended MEMBER suspended NOTIFY valueChanged);
+
+  Q_PROPERTY(bool showHowAlert MEMBER showHowAlert NOTIFY valueChanged);
+  Q_PROPERTY(bool howWarning MEMBER howWarning NOTIFY valueChanged);
+
+  Q_PROPERTY(bool showVTC MEMBER showVTC NOTIFY valueChanged);
+  Q_PROPERTY(QString vtcSpeed MEMBER vtcSpeed NOTIFY valueChanged);
+  Q_PROPERTY(QColor vtcColor MEMBER vtcColor NOTIFY valueChanged);
+  Q_PROPERTY(bool showDebugUI MEMBER showDebugUI NOTIFY valueChanged);
+
+Q_PROPERTY(QString roadName MEMBER roadName NOTIFY valueChanged);
+
+  Q_PROPERTY(bool showSpeedLimit MEMBER showSpeedLimit NOTIFY valueChanged);
+  Q_PROPERTY(QString speedLimit MEMBER speedLimit NOTIFY valueChanged);
+  Q_PROPERTY(QString slcSubText MEMBER slcSubText NOTIFY valueChanged);
+  Q_PROPERTY(float slcSubTextSize MEMBER slcSubTextSize NOTIFY valueChanged);
+  Q_PROPERTY(bool mapSourcedSpeedLimit MEMBER mapSourcedSpeedLimit NOTIFY valueChanged);
+  Q_PROPERTY(bool slcActive MEMBER slcActive NOTIFY valueChanged);
+
+  Q_PROPERTY(bool showTurnSpeedLimit MEMBER showTurnSpeedLimit NOTIFY valueChanged);
+  Q_PROPERTY(QString turnSpeedLimit MEMBER turnSpeedLimit NOTIFY valueChanged);
+  Q_PROPERTY(QString tscSubText MEMBER tscSubText NOTIFY valueChanged);
+  Q_PROPERTY(bool tscActive MEMBER tscActive NOTIFY valueChanged);
+  Q_PROPERTY(int curveSign MEMBER curveSign NOTIFY valueChanged);
+
+  Q_PROPERTY(int lead_status MEMBER lead_status NOTIFY valueChanged);
+  Q_PROPERTY(float lead_d_rel MEMBER lead_d_rel NOTIFY valueChanged);
+  Q_PROPERTY(float lead_v_rel MEMBER lead_v_rel NOTIFY valueChanged);
+  Q_PROPERTY(float angleSteers MEMBER angleSteers NOTIFY valueChanged);
+  Q_PROPERTY(float steerAngleDesired MEMBER steerAngleDesired NOTIFY valueChanged);
+  Q_PROPERTY(int distanceTraveled MEMBER distanceTraveled NOTIFY valueChanged);
+  Q_PROPERTY(int devUiEnabled MEMBER devUiEnabled NOTIFY valueChanged);
+  Q_PROPERTY(float gpsAccuracy MEMBER gpsAccuracy NOTIFY valueChanged);
+  Q_PROPERTY(float altitude MEMBER altitude NOTIFY valueChanged);
+  Q_PROPERTY(float vEgo MEMBER vEgo NOTIFY valueChanged);
+  Q_PROPERTY(float aEgo MEMBER aEgo NOTIFY valueChanged);
+  Q_PROPERTY(float steeringTorque MEMBER steeringTorque NOTIFY valueChanged);
+  Q_PROPERTY(float bearingAccuracyDeg MEMBER bearingAccuracyDeg NOTIFY valueChanged);
+  Q_PROPERTY(float bearingDeg MEMBER bearingDeg NOTIFY valueChanged);
+
+  Q_PROPERTY(bool standStill MEMBER standStill NOTIFY valueChanged);
+  Q_PROPERTY(int standstillElapsedTime MEMBER standstillElapsedTime NOTIFY valueChanged);
 
 public:
   explicit OnroadHud(QWidget *parent);
@@ -27,12 +87,36 @@ public:
 private:
   void drawIcon(QPainter &p, int x, int y, QPixmap &img, QBrush bg, float opacity);
   void drawText(QPainter &p, int x, int y, const QString &text, int alpha = 255);
+  void drawSpeedText(QPainter &p, int x, int y, const QString &text, QColor color);
+  void drawCenteredText(QPainter &p, int x, int y, const QString &text, QColor color);
+  void drawVisionTurnControllerUI(QPainter &p, int x, int y, int size, const QColor &color, const QString &speed,
+                                  int alpha);
+  void drawCircle(QPainter &p, int x, int y, int r, QBrush bg);
+  void drawSpeedSign(QPainter &p, QRect rc, const QString &speed, const QString &sub_text, int subtext_size,
+                     bool is_map_sourced, bool is_active);
+  void drawTrunSpeedSign(QPainter &p, QRect rc, const QString &speed, const QString &sub_text, int curv_sign,
+                         bool is_active);
+  void drawMadsIcon(QPainter &p, int x, int y, QPixmap &img, QBrush bg, float opacity);
+  void drawStandstillTimerText(QPainter &p, int x, int y, const char* label, const char* value, QColor &color1, QColor &color2);
+  void drawRightDevUi(QPainter &p, int x, int y);
+  void drawRightDevUi2(QPainter &p, int x, int y);
+  void drawRightDevUiBorder(QPainter &p, int x, int y);
+  void drawStandstillTimer(QPainter &p, int x, int y);
+  int drawDevUiElementRight(QPainter &p, int x, int y, const char* value, const char* label, const char* units, QColor &color);
+  int drawDevUiElementLeft(QPainter &p, int x, int y, const char* value, const char* label, const char* units, QColor &color);
+  void drawColoredText(QPainter &p, int x, int y, const QString &text, QColor &color);
   void paintEvent(QPaintEvent *event) override;
 
   QPixmap engage_img;
   QPixmap dm_img;
+  QPixmap how_img;
+  QPixmap map_img;
+  QPixmap left_img;
+  QPixmap right_img;
+  QPixmap mads_imgs[2];
   const int radius = 192;
   const int img_size = (radius / 2) * 1.5;
+  const int subsign_img_size = 35;
   QString speed;
   QString speedUnit;
   QString maxSpeed;
@@ -41,6 +125,50 @@ private:
   bool dmActive = false;
   bool hideDM = false;
   int status = STATUS_DISENGAGED;
+  bool is_brakelight_on = false;
+  bool madsEnabled = false;
+  bool suspended = false;
+
+  bool showHowAlert = false;
+  bool howWarning = false;
+
+  bool showVTC = false;
+  QString vtcSpeed;
+  QColor vtcColor;
+  bool showDebugUI = false;
+
+  QString roadName;
+
+  bool showSpeedLimit = false;
+  QString speedLimit;
+  QString slcSubText;
+  float slcSubTextSize = 0.0;
+  bool mapSourcedSpeedLimit = false;
+  bool slcActive = false;
+
+  bool showTurnSpeedLimit = false;
+  QString turnSpeedLimit;
+  QString tscSubText;
+  bool tscActive = false;
+  int curveSign = 0;
+
+  int lead_status;
+  float lead_d_rel = 0;
+  float lead_v_rel = 0;
+  float angleSteers = 0;
+  float steerAngleDesired = 0;
+  int distanceTraveled = 0;
+  int devUiEnabled;
+  float gpsAccuracy;
+  float altitude;
+  float vEgo;
+  float aEgo;
+  float steeringTorque;
+  float bearingAccuracyDeg;
+  float bearingDeg;
+
+  bool standStill;
+  int standstillElapsedTime;
 
 signals:
   void valueChanged();
@@ -76,7 +204,13 @@ protected:
   void drawLaneLines(QPainter &painter, const UIScene &scene);
   void drawLead(QPainter &painter, const cereal::ModelDataV2::LeadDataV3::Reader &lead_data, const QPointF &vd);
   inline QColor redColor(int alpha = 255) { return QColor(201, 34, 49, alpha); }
+  inline QColor blackColor(int alpha = 255) { return QColor(0, 0, 0, alpha); }
+  inline QColor greenColor(int alpha = 255) { return QColor(49, 201, 34, alpha); }
+  inline QColor graceBlueColor(int alpha = 255) { return QColor (34, 49, 201, alpha); }
   double prev_draw_t = 0;
+
+signals:
+  void resizeSignal(int w);
 };
 
 // container for all onroad widgets
@@ -93,6 +227,7 @@ private:
   OnroadHud *hud;
   OnroadAlerts *alerts;
   NvgWindow *nvg;
+  ButtonsWindow *buttons;
   QColor bg = bg_colors[STATUS_DISENGAGED];
   QWidget *map = nullptr;
   QHBoxLayout* split;