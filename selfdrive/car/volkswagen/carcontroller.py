be4362b910687ca652e82df4610cb6a4d5e09e3e
Jason Wen <haibin.wen3@gmail.com>
2022-04-13 22:45:03 -0400

    Cleanup 14 - tune

--- selfdrive/car/volkswagen/carcontroller.py
+++ selfdrive/car/volkswagen/carcontroller.py
@@ -264,7 +264,7 @@ class CarController():
     if self.target_speed == self.v_set_dis:
       self.button_count = 0
       self.button_type = 3
-    elif self.button_count > 20:
+    elif self.button_count > 30:
       cruise_button = 1
       self.button_count = 0
       self.button_type = 3
@@ -276,7 +276,7 @@ class CarController():
     if self.target_speed == self.v_set_dis:
       self.button_count = 0
       self.button_type = 3
-    elif self.button_count > 20:
+    elif self.button_count > 30:
       cruise_button = 2
       self.button_count = 0
       self.button_type = 3
