346e2bb0b46aa426a13ebf8c600b4d54e3683538
Jason Wen <haibin.wen3@gmail.com>
2022-04-21 16:59:07 -0400

    Final speed tuning

--- selfdrive/car/volkswagen/carcontroller.py
+++ selfdrive/car/volkswagen/carcontroller.py
@@ -274,7 +274,7 @@ class CarController():
     if self.target_speed == self.v_set_dis:
       self.button_count = 0
       self.button_type = 3
-    elif self.button_count > 30:
+    elif self.button_count > 25:
       cruise_button = 1
       self.button_count = 0
       self.button_type = 3
@@ -286,7 +286,7 @@ class CarController():
     if self.target_speed == self.v_set_dis:
       self.button_count = 0
       self.button_type = 3
-    elif self.button_count > 30:
+    elif self.button_count > 25:
       cruise_button = 2
       self.button_count = 0
       self.button_type = 3
