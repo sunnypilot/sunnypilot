23837e955cdc72f8c2c8eb199cf92b01f75f8a4e
Jason Wen <haibin.wen3@gmail.com>
2022-04-25 21:01:51 -0400

    Add 10 frames to counter?

--- selfdrive/car/volkswagen/carcontroller.py
+++ selfdrive/car/volkswagen/carcontroller.py
@@ -274,7 +274,7 @@ class CarController():
     if self.target_speed == self.v_set_dis:
       self.button_count = 0
       self.button_type = 3
-    elif self.button_count > 10:
+    elif self.button_count > 20:
       cruise_button = 1
       self.button_count = 0
       self.button_type = 3
@@ -286,7 +286,7 @@ class CarController():
     if self.target_speed == self.v_set_dis:
       self.button_count = 0
       self.button_type = 3
-    elif self.button_count > 10:
+    elif self.button_count > 20:
       cruise_button = 2
       self.button_count = 0
       self.button_type = 3
