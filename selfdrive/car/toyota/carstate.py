00775ba039a5d3b41a9bdb05297db499bb56d863
Jason Wen <haibin.wen3@gmail.com>
2022-05-21 16:08:30 -0400

    Expand support to SDSU
    Signed-off-by: Jason Wen <haibin.wen3@gmail.com>

--- selfdrive/car/toyota/carstate.py
+++ selfdrive/car/toyota/carstate.py
@@ -157,6 +157,8 @@ class CarState(CarStateBase):
       if self.CP.carFingerprint in TSS2_CAR:
         # KRKeegan - Add support for toyota distance button
         self.gap_adjust_cruise_tr = 1 if cp_cam.vl["ACC_CONTROL"]["DISTANCE"] == 1 else 0
+      elif self.CP.smartDsu:
+        self.distance_btn = 1 if cp.vl["SDSU"]["FD_BUTTON"] == 1 else 0
       ret.gapAdjustCruiseTr = cp.vl["PCM_CRUISE_SM"]["DISTANCE_LINES"]
 
     # some TSS2 cars have low speed lockout permanently set, so ignore on those cars
