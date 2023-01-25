6b5fe40dd1793cac9f5746499818db61ce64e37d
Radek Chybicki <rchybicki@gmail.com>
2023-01-25 12:42:45 +0100

    Acceleration profile +  ret.vEgoStopping = 0.05

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -25,9 +25,18 @@ EventName = car.CarEvent.EventName
 
 LON_MPC_STEP = 0.2  # first step is 0.2s
 AWARENESS_DECEL = -0.2  # car smoothly decel at .2m/s^2 when user is distracted
-A_CRUISE_MIN = -1.2
-A_CRUISE_MAX_VALS = [1.4, 1.0, 0.7, 0.5] # [1.6, 1.2, 0.8, 0.6]
-A_CRUISE_MAX_BP = [0., 10.0, 25., 40.] # 0km/h, 36km/h, 90km/h, 144km/h
+A_CRUISE_MIN = -0.75 # was -1.2
+
+A_CRUISE_MAX_VALS =   [3.0, 1.7, 1.3, 0.7, 0.6, 0.44, 0.32, 0.22, 0.16, 0.0078]
+A_CRUISE_MAX_BP = [0.,  3,   6.,  8.,  11., 15.,  20.,  25.,  30.,  55.]
+
+# _DP_CRUISE_MAX_V =       [3.5, 3.4, 2.1, 1.6, 1.1, 0.91, 0.68, 0.44, 0.34, 0.13]
+# _DP_CRUISE_MAX_V_ECO =   [3.0, 1.7, 1.3, 0.7, 0.6, 0.44, 0.32, 0.22, 0.16, 0.0078]
+# _DP_CRUISE_MAX_V_SPORT = [3.5, 3.5, 3.4, 3.0, 2.1, 1.61, 1.1,  0.63, 0.50, 0.33]
+# _DP_CRUISE_MAX_BP =      [0.,  3,   6.,  8.,  11., 15.,  20.,  25.,  30.,  55.]
+
+#A_CRUISE_MAX_VALS = [1.4, 1.0, 0.7, 0.5] # was [1.6, 1.2, 0.8, 0.6]
+#A_CRUISE_MAX_BP = [0., 10.0, 25., 40.] # 0km/h, 36km/h, 90km/h, 144km/h
 
 # Lookup table for turns
 _A_TOTAL_MAX_V = [1.7, 3.2]
