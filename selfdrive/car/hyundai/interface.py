d1ff80424d57ea0425976c808f90ff0c11cf9d70
Jason Wen <haibin.wen3@gmail.com>
2023-02-16 23:21:14 -0500

    add stoppingDecelRate like Toyota

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -244,6 +244,7 @@ class CarInterface(CarInterfaceBase):
     ret.startingState = True
     ret.vEgoStarting = 0.1
     ret.vEgoStopping = 0.1
+    ret.stoppingDecelRate = 0.009
     ret.startAccel = 1.0
     ret.longitudinalActuatorDelayLowerBound = 0.5
     ret.longitudinalActuatorDelayUpperBound = 0.5
