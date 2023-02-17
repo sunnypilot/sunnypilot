15166d7228ea5e6af1acd52c2ff2b64906906814
Jason Wen <haibin.wen3@gmail.com>
2023-02-16 23:22:55 -0500

    some old tested tune

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -233,9 +233,12 @@ class CarInterface(CarInterfaceBase):
       ret.longitudinalTuning.kiV = [0.0]
       ret.experimentalLongitudinalAvailable = candidate in (HYBRID_CAR | EV_CAR) and candidate not in CANFD_RADAR_SCC_CAR
     else:
-      ret.longitudinalTuning.kpBP = [0., 5., 20.]
-      ret.longitudinalTuning.kpV = [0.05, 0.025, 0.01]
-      ret.longitudinalTuning.kiV = [0.0]
+      ret.longitudinalTuning.kpBP = [0., 10., 40.]
+      ret.longitudinalTuning.kpV = [1.2, 0.6, 0.2]
+      ret.longitudinalTuning.kiBP = [0., 10., 30., 40.]
+      ret.longitudinalTuning.kiV = [0.05, 0.02, 0.01, 0.005]
+      ret.longitudinalTuning.deadzoneBP = [0., 40]
+      ret.longitudinalTuning.deadzoneV = [0., 0.02]
       ret.experimentalLongitudinalAvailable = candidate not in (LEGACY_SAFETY_MODE_CAR | CAMERA_SCC_CAR)
     ret.openpilotLongitudinalControl = experimental_long and ret.experimentalLongitudinalAvailable
     ret.pcmCruise = not ret.openpilotLongitudinalControl
