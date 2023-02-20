1f93707c38884a6c18ed3d700bbf55789330d733
Jacob Pfeifer <jacob@pfeifer.dev>
2023-02-19 23:35:22 -0500

    mapd consistency adjustments:
    * Project gps location onto map route line for determining distance to
      next node.
    * Increase distance for determining if we have left a route. Helpful for
      when osm route line is not perfectly centered on road.
    * Match max lateral accel against vision turn speed controller max
      lateral accel.

--- selfdrive/mapd/lib/geo.py
+++ selfdrive/mapd/lib/geo.py
@@ -47,6 +47,18 @@ def bearing_to_points(point, points):
   y = np.cos(point[0]) * np.sin(points[:, 0]) - (np.sin(point[0]) * np.cos(points[:, 0]) * np.cos(delta[:, 1]))
   return np.arctan2(x, y)
 
+def point_on_line(start_points, end_points, point, extend_line = False):
+  """project a single point onto each line for an np array of start points and end points
+  ref: https://stackoverflow.com/a/61342198
+  """
+  ap = np.subtract(point, start_points)
+  ab = np.subtract(end_points, start_points)
+  t = np.array([np.dot(ap[i], ab[i]) / np.dot(ab[i], ab[i]) for i in range(len(ap))])
+  # if you need the the closest point belonging to the segment
+  if not extend_line:
+    t = np.maximum(0, np.minimum(1, t))
+  result = np.add(start_points, np.array([t[i] * ab[i] for i in range(len(t))]))
+  return result
 
 def distance_to_points(point, points):
   """Calculate the distance of the vectors between `point` and each one of the entries in `points`.
