42cc0e296a3776ea0a7494eb5f3a8284544c720e
Jason Wen <haibin.wen3@gmail.com>
2023-02-21 22:53:13 -0500

    mapd: use correct path

--- selfdrive/manager/manager.py
+++ selfdrive/manager/manager.py
@@ -23,7 +23,7 @@ from system.version import is_dirty, get_commit, get_version, get_origin, get_sh
                               terms_version, training_version, is_tested_branch, is_release_branch
 
 
-sys.path.append(os.path.join(BASEDIR, "third_party"))
+sys.path.append(os.path.join(BASEDIR, "third_party/mapd"))
 
 
 def manager_init() -> None:
