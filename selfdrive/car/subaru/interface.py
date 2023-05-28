4796f674dacd6d549ac6b552491ceb763b165325
Jason Wen <haibin.wen3@gmail.com>
2023-05-28 19:51:22 -0400

    Higher torque limit (3071) for 2018 Crosstrek / 2017 Impreza @ClockeNessMnstr (#146)
    * Higher torque limit (3071) for 2018 Crosstrek / 2017 Impreza @ClockeNessMnstr
    
    * bump panda
    
    * set with int flag
    
    * bump panda
    
    * flag
    
    * sync tune
    
    * bump panda
    
    * fix
    
    * fix
    
    * oops
    
    * update FW
    
    * List comprehension
    
    * bump panda

--- selfdrive/car/subaru/interface.py
+++ selfdrive/car/subaru/interface.py
@@ -52,6 +52,12 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.pid.kf = 0.00005
       ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0., 20.], [0., 20.]]
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.2, 0.3], [0.02, 0.03]]
+      # Certain Impreza / Crosstrek EPS use 3071 max value and do not work with stock value/scaling.
+      if any(fw.ecu == "eps" and fw.fwVersion in (b'z\xc0\x00\x00', b'z\xc0\x04\x00', b'z\xc0\x08\x00', b'\x8a\xc0\x00\x00') for fw in car_fw):
+        ret.safetyConfigs[0].safetyParam = Panda.FLAG_SUBARU_MAX_STEER_IMPREZA_2018
+        ret.steerActuatorDelay = 0.18  # measured
+        ret.lateralTuning.pid.kf = 0.00003333
+        ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.133, 0.2], [0.0133, 0.02]]
 
     elif candidate == CAR.IMPREZA_2020:
       ret.mass = 1480. + STD_CARGO_KG
