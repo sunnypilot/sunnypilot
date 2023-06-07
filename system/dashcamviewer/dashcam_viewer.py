7753a71c7357fee78faab941e35920e8398c62bf
Jason Wen <haibin.wen3@gmail.com>
2023-06-06 23:21:26 -0400

    skeleton

--- system/dashcamviewer/dashcam_viewer.py
+++ system/dashcamviewer/dashcam_viewer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 import os
+import random
+import secrets
 import system.dashcamviewer.helpers as dashcam
-from flask import Flask, render_template, Response, request, send_from_directory
+from flask import Flask, render_template, Response, request, send_from_directory, session, redirect, url_for
 from system.loggerd.config import ROOT as REALDATA
 
 app = Flask(__name__)
@@ -9,11 +11,36 @@ app = Flask(__name__)
 
 @app.route("/")
 def index_page():
+  if session.get("logged_in"):
+    return redirect(url_for("home_page"))
+  return render_template("login.html")
+
+
+@app.route("/index")
+def home_page():
+  if not session.get("logged_in"):
+    return redirect(url_for("index_page"))
   return render_template("index.html")
+
+
+@app.route("/login", methods=["POST"])
+def login():
+  inputted_pin = request.form.get("pin")
+  with open(dashcam.PIN_PATH + "otp.conf", "r") as file:
+    correct_pin = file.read().strip()
+
+  if inputted_pin == correct_pin:
+    session["logged_in"] = True
+    return redirect(url_for("home_page"))
+  else:
+    error_message = "Incorrect PIN. Please try again."
+    return render_template("login.html", error=error_message)
 
 
 @app.route("/footage/full/<cameratype>/<route>")
 def full(cameratype, route):
+  if not session.get("logged_in"):
+    return redirect(url_for("index_page"))
   chunk_size = 1024 * 512  # 5KiB
   file_name = cameratype + (".ts" if cameratype == "qcamera" else ".hevc")
   vidlist = "|".join(REALDATA + "/" + segment + "/" + file_name for segment in dashcam.segments_in_route(route))
@@ -27,6 +54,8 @@ def full(cameratype, route):
 
 @app.route("/footage/<cameratype>/<segment>")
 def fcamera(cameratype, segment):
+  if not session.get("logged_in"):
+    return redirect(url_for("index_page"))
   if not dashcam.is_valid_segment(segment):
     return render_template("error.html", error="invalid segment")
   file_name = REALDATA + "/" + segment + "/" + cameratype + (".ts" if cameratype == "qcamera" else ".hevc")
@@ -35,6 +64,8 @@ def fcamera(cameratype, segment):
 
 @app.route("/footage/<route>")
 def route(route):
+  if not session.get("logged_in"):
+    return redirect(url_for("index_page"))
   if len(route) != 20:
     return render_template("error.html", error="route not found")
 
@@ -55,11 +86,15 @@ def route(route):
 
 @app.route("/footage")
 def footage():
+  if not session.get("logged_in"):
+    return redirect(url_for("index_page"))
   return render_template("footage.html", rows=dashcam.all_routes())
 
 
 @app.route("/screenrecords")
 def screenrecords():
+  if not session.get("logged_in"):
+    return redirect(url_for("index_page"))
   rows = dashcam.all_screenrecords()
   if not rows:
     return render_template("error.html", error="no screenrecords found at:<br><br>" + dashcam.SCREENRECORD_PATH)
@@ -68,26 +103,45 @@ def screenrecords():
 
 @app.route("/screenrecords/<clip>")
 def screenrecord(clip):
+  if not session.get("logged_in"):
+    return redirect(url_for("index_page"))
   return render_template("screenrecords.html", rows=dashcam.all_screenrecords(), clip=clip)
 
 
 @app.route("/screenrecords/play/pipe/<file>")
 def videoscreenrecord(file):
+  if not session.get("logged_in"):
+    return redirect(url_for("index_page"))
   file_name = dashcam.SCREENRECORD_PATH + file
   return Response(dashcam.ffplay_mp4_wrap_process_builder(file_name).stdout.read(), status=200, mimetype='video/mp4')
 
 
 @app.route("/screenrecords/download/<clip>")
 def download_file(clip):
+  if not session.get("logged_in"):
+    return redirect(url_for("index_page"))
   return send_from_directory(dashcam.SCREENRECORD_PATH, clip, as_attachment=True)
-  
+
 
 @app.route("/about")
 def about():
+  if not session.get("logged_in"):
+    return redirect(url_for("index_page"))
   return render_template("about.html")
 
 
 def main():
+  if not os.path.exists(dashcam.PIN_PATH):
+    os.makedirs(dashcam.PIN_PATH)
+  pin = str(random.randint(100000, 999999))
+  with open(dashcam.PIN_PATH + "otp.conf", "w") as file:
+    file.write(pin)
+
+  print(pin)
+
+  secret = secrets.token_hex(32)
+  app.secret_key = secret
+
   app.run(host="0.0.0.0", port=5050)
 
 
