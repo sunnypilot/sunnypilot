88821fb8d813c4c41f2a65272b9533bf9fc77630
Jason Wen <haibin.wen3@gmail.com>
2023-06-09 18:02:53 -0400

    ui: display CPU temperature on sidebar prerequisites

--- common/params.cc
+++ common/params.cc
@@ -274,6 +274,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"ReverseAccChange", PERSISTENT},
     {"ReverseDmCam", PERSISTENT},
     {"ShowDebugUI", PERSISTENT},
+    {"SidebarCpuTemp", PERSISTENT},
     {"SpeedLimitControl", PERSISTENT},
     {"SpeedLimitPercOffset", PERSISTENT},
     {"SpeedLimitStyle", PERSISTENT},
