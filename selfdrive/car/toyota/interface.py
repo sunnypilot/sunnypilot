40bac85284a49c944b02553699c6615decccfa40
Jason Wen <haibin.wen3@gmail.com>
2023-06-26 11:55:37 -0400

    cleanup

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -235,16 +235,10 @@ class CarInterface(CarInterfaceBase):
     tune.deadzoneBP = [0., 9.]
     tune.deadzoneV = [.0, .15]
     if candidate in TSS2_CAR or ret.enableGasInterceptor:
-      if sp_tss2_long_tune:
-        tune.kpBP = [0., 5., 20., 30.]
-        tune.kpV = [1.3, 1.0, 0.7, 0.1]
-        tune.kiBP = [0., 8.3, 11.9, 13.8, 19.7, 30., 33., 40.]
-        tune.kiV = [.02, .03, .14, .18, .18, .06, .01, .001]
-      else:
-        tune.kpBP = [0., 5., 20.]
-        tune.kpV = [1.3, 1.0, 0.7]
-        tune.kiBP = [0., 5., 12., 20., 27.]
-        tune.kiV = [.35, .23, .20, .17, .1]
+      tune.kpBP = [0., 5., 20., 30.] if sp_tss2_long_tune else [0., 5., 20.]
+      tune.kpV = [1.3, 1.0, 0.7, 0.1] if sp_tss2_long_tune else [1.3, 1.0, 0.7]
+      tune.kiBP = [0., 8.3, 11.9, 13.8, 19.7, 30., 33., 40.] if sp_tss2_long_tune else [0., 5., 12., 20., 27.]
+      tune.kiV = [.02, .03, .14, .18, .18, .06, .01, .001] if sp_tss2_long_tune else [.35, .23, .20, .17, .1]
       if candidate in TSS2_CAR:
         ret.vEgoStopping = 0.25
         ret.vEgoStarting = 0.25
