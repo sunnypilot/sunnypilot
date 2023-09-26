040ac7472d464bfe02fda4a8e27937c8f4f9370b
Jason Wen <haibin.wen3@gmail.com>
2023-09-25 23:57:37 -0400

    Disable blindspot for now

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -277,7 +277,7 @@ class CarInterface(CarInterfaceBase):
 
     # *** feature detection ***
     if candidate in CANFD_CAR:
-      ret.enableBsm = 0x1e5 in fingerprint[CAN.ECAN]
+      ret.enableBsm = False  # 0x1e5 in fingerprint[CAN.ECAN]
     else:
       ret.enableBsm = 0x58b in fingerprint[0]
 
