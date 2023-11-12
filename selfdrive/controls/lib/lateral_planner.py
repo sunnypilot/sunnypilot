8f1de6b9e2665d0228813890f59db0878bbf99ad
Jason Wen <haibin.wen3@gmail.com>
2023-11-12 00:32:26 -0500

    Revert "lat planner retune"
    This reverts commit 1088d425f445aac4002b79d24942b876c6ba378d.

--- selfdrive/controls/lib/lateral_planner.py
+++ selfdrive/controls/lib/lateral_planner.py
@@ -18,13 +18,13 @@ LaneChangeState = log.LateralPlan.LaneChangeState
 
 PATH_COST = 1.0
 LATERAL_MOTION_COST = 0.11
-LATERAL_ACCEL_COST = 1.0
+LATERAL_ACCEL_COST = 0.0
 LATERAL_JERK_COST = 0.04
 # Extreme steering rate is unpleasant, even
 # when it does not cause bad jerk.
 # TODO this cost should be lowered when low
 # speed lateral control is stable on all cars
-STEERING_RATE_COST = 8.0
+STEERING_RATE_COST = 700.0
 
 
 class LateralPlanner:
