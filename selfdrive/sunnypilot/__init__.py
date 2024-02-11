8e82b3abd99708b3a4ec1e24c487a8fe22c6de33
Jason Wen <haibin.wen3@gmail.com>
2024-02-11 12:30:32 -0500

    Driving Model Selector: Handle dynamic dictionary inputs

--- selfdrive/sunnypilot/__init__.py
+++ selfdrive/sunnypilot/__init__.py
@@ -1,8 +1,8 @@
 from openpilot.common.params import Params
 
 
-def get_model_generation(default_gen=3):  # TODO: SP - Refactor for cleaner code/future model adoption
+def get_model_generation():
   params = Params()
   custom_model = params.get_bool("CustomDrivingModel")
-  gen = int(params.get("DrivingModelGeneration", encoding="utf8")) or default_gen
+  gen = int(params.get("DrivingModelGeneration", encoding="utf8"))
   return custom_model, gen
