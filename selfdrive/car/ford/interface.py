6e2ba55a99d69f9e9f58809c0dd02cd73f1db3c7
Isaac Barham <isaac.barham@sunnypilot.ai>
2024-02-20 23:01:28 +0000

    Update interface.py

--- selfdrive/car/ford/interface.py
+++ selfdrive/car/ford/interface.py
@@ -49,7 +49,7 @@ class CarInterface(CarInterfaceBase):
     if ret.spFlags & FordFlagsSP.SP_ENHANCED_LAT_CONTROL:
       ret.safetyConfigs[-1].safetyParam |= Panda.FLAG_FORD_ENHANCED_LAT_CONTROL
 
-    if Params().get("DongleId", encoding='utf8') in ("4fde83db16dc0802", "112e4d6e0cad05e1", "e36b272d5679115f", "24574459dd7fb3e0", "83a4e056c7072678")
+    if Params().get("DongleId", encoding='utf8') in ("4fde83db16dc0802", "112e4d6e0cad05e1", "e36b272d5679115f", "24574459dd7fb3e0", "83a4e056c7072678"):
       ret.spFlags |= FordFlagsSP.SP_ENHANCED_LAT_CONTROL.value
 
     if candidate == CAR.BRONCO_SPORT_MK1:
