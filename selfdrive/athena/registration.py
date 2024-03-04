325d8612a325c8308b2af55d00e512c72b0aa0e6
ya787499883 <62471252+ya787499883@users.noreply.github.com>
2024-03-04 15:41:56 +0800

    Update registration.py

--- selfdrive/athena/registration.py
+++ selfdrive/athena/registration.py
@@ -29,7 +29,7 @@ def register(show_spinner=False) -> str | None:
   HardwareSerial = params.get("HardwareSerial", encoding='utf8')
   dongle_id: str | None = params.get("DongleId", encoding='utf8')
   needs_registration = None in (IMEI, HardwareSerial, dongle_id)
-
+  return UNREGISTERED_DONGLE_ID
   pubkey = Path(Paths.persist_root()+"/comma/id_rsa.pub")
   if not pubkey.is_file():
     dongle_id = UNREGISTERED_DONGLE_ID
