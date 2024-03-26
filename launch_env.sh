57962e51bc6247a26408b50ff01215cb2c1690f2
ya787499883 <62471252+ya787499883@users.noreply.github.com>
2024-03-26 22:18:01 +0800

    添加车型林肯

--- launch_env.sh
+++ launch_env.sh
@@ -5,6 +5,7 @@ export MKL_NUM_THREADS=1
 export NUMEXPR_NUM_THREADS=1
 export OPENBLAS_NUM_THREADS=1
 export VECLIB_MAXIMUM_THREADS=1
+export FINGERPRINT="Lincoln Navigator 2ND GEN"
 
 if [ -z "$AGNOS_VERSION" ]; then
   export AGNOS_VERSION="9.6"
