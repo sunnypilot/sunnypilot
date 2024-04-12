531a75134faa1310c3122db97830cea6ea92c465
Jason Wen <haibin.wen3@gmail.com>
2024-04-12 00:11:30 -0400

    Gate behind flag

--- selfdrive/ui/qt/offroad/sunnypilot/display_settings.cc
+++ selfdrive/ui/qt/offroad/sunnypilot/display_settings.cc
@@ -13,14 +13,15 @@ DisplayPanel::DisplayPanel(QWidget *parent) : ListWidget(parent, false) {
       .arg(tr("Enabled: Wake the brightness of the screen to display all events."))
       .arg(tr("Disabled: Wake the brightness of the screen to display critical events.")),
       "../assets/offroad/icon_blank.png",
+#ifdef ENABLE_DASHCAM
     },
-    // Disabling due to https://discord.com/channels/880416502577266699/1223238728852574269
-    //{
-    //  "ScreenRecorder",
-    //  tr("Enable Screen Recorder"),
-    //  tr("Enable this will display a button on the onroad screen to toggle on or off real-time screen recording with UI elements."),
-    //  "../assets/offroad/icon_blank.png"
-    //}
+    {
+      "ScreenRecorder",
+      tr("Enable Screen Recorder"),
+      tr("Enable this will display a button on the onroad screen to toggle on or off real-time screen recording with UI elements."),
+      "../assets/offroad/icon_blank.png"
+#endif
+    }
   };
 
   // General: Max Time Offroad (Shutdown timer)
