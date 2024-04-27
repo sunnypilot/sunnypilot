f0b4f075b58ae0b10695b68aa2de348fd304ba84
Jason Wen <haibin.wen3@gmail.com>
2024-04-27 11:11:17 -0400

    unnecessary

--- selfdrive/car/hyundai/hyundaicanfd.py
+++ selfdrive/car/hyundai/hyundaicanfd.py
@@ -208,7 +208,6 @@ def create_acc_commands_can_canfd_hybrid(packer, CAN, enabled, accel, accel_last
       "aReqValue": a_val,
       "JerkUpperLimit": upper_jerk,
       "JerkLowerLimit": jerk if enabled else 1,
-      "OBJ_LOCATION": 2,
     }),
 
     ("SCC12", {
