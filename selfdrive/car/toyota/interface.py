5e73ef52ed2755fcb48c734aa9d718c0e26e0e50
dsa302010 <164147796+dsa302010@users.noreply.github.com>
2024-05-20 12:20:31 +0800

    旁路後的門

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -61,7 +61,7 @@ class CarInterface(CarInterfaceBase):
     # In TSS2 cars, the camera does long control
     found_ecus = [fw.ecu for fw in car_fw]
     ret.enableDsu = len(found_ecus) > 0 and Ecu.dsu not in found_ecus and candidate not in (NO_DSU_CAR | UNSUPPORTED_DSU_CAR) \
-                                        and not (ret.flags & ToyotaFlags.SMART_DSU)
+                                        and not (ret.flags & ToyotaFlags.SMART_DSU) and not (ret.flags & ToyotaFlags.DSU_BYPASS)
 
     if candidate == CAR.TOYOTA_PRIUS:
       zss = ret.spFlags & ToyotaFlagsSP.SP_ZSS
