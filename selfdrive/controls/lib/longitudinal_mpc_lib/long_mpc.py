5761a2a8da641e14357e1cbfddf0b484ebbefd11
dsa302010 <164147796+dsa302010@users.noreply.github.com>
2024-05-20 11:23:04 +0800

    車距

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -54,7 +54,7 @@ T_IDXS = np.array(T_IDXS_LST)
 FCW_IDXS = T_IDXS < 5.0
 T_DIFFS = np.diff(T_IDXS, prepend=[0.])
 COMFORT_BRAKE = 2.5
-STOP_DISTANCE = 6.0
+STOP_DISTANCE = 6.5
 
 def get_jerk_factor(personality=custom.LongitudinalPersonalitySP.standard):
   if personality==custom.LongitudinalPersonalitySP.relaxed:
@@ -62,20 +62,20 @@ def get_jerk_factor(personality=custom.LongitudinalPersonalitySP.standard):
   elif personality==custom.LongitudinalPersonalitySP.standard:
     return 1.0
   elif personality==custom.LongitudinalPersonalitySP.moderate:
-    return 0.5
+    return 0.9
   elif personality==custom.LongitudinalPersonalitySP.aggressive:
-    return 0.222
+    return 0.6
   else:
     raise NotImplementedError("Longitudinal personality not supported")
 
 
 def get_T_FOLLOW(personality=custom.LongitudinalPersonalitySP.standard):
   if personality==custom.LongitudinalPersonalitySP.relaxed:
-    return 1.75
+    return 1.95
   elif personality==custom.LongitudinalPersonalitySP.standard:
-    return 1.45
+    return 1.55
   elif personality==custom.LongitudinalPersonalitySP.moderate:
-    return 1.25
+    return 1.15
   elif personality==custom.LongitudinalPersonalitySP.aggressive:
     return 1.0
   else:
