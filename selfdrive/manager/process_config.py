eede9d0abf916ca91f74778be55a0017d0d976eb
dsa302010 <164147796+dsa302010@users.noreply.github.com>
2024-05-23 12:36:29 +0800

    取消日誌紀錄

--- selfdrive/manager/process_config.py
+++ selfdrive/manager/process_config.py
@@ -60,7 +60,7 @@ procs = [
   PythonProcess("dmonitoringmodeld", "selfdrive.modeld.dmonitoringmodeld", driverview, enabled=(not PC or WEBCAM)),
   NativeProcess("encoderd", "system/loggerd", ["./encoderd"], only_onroad),
   NativeProcess("stream_encoderd", "system/loggerd", ["./encoderd", "--stream"], notcar),
-  NativeProcess("loggerd", "system/loggerd", ["./loggerd"], logging),
+  #NativeProcess("loggerd", "system/loggerd", ["./loggerd"], logging),
   NativeProcess("modeld", "selfdrive/modeld", ["./modeld"], only_onroad),
   NativeProcess("mapsd", "selfdrive/navd", ["./mapsd"], model_use_nav),
   PythonProcess("navmodeld", "selfdrive.modeld.navmodeld", model_use_nav),
