3f7bf0c1673054fa0aba623515b20d44ba095d6c
Jason Wen <haibin.wen3@gmail.com>
2024-05-24 01:02:27 -0400

    Params: Fix for SP-35

--- selfdrive/manager/manager.py
+++ selfdrive/manager/manager.py
@@ -26,7 +26,7 @@ def manager_init() -> None:
   build_metadata = get_build_metadata()
 
   params = Params()
-  params.clear_all(ParamKeyType.CLEAR_ON_MANAGER_START)("CustomDrivingModel", "0"),
+  params.clear_all(ParamKeyType.CLEAR_ON_MANAGER_START)
   params.clear_all(ParamKeyType.CLEAR_ON_ONROAD_TRANSITION)
   params.clear_all(ParamKeyType.CLEAR_ON_OFFROAD_TRANSITION)
   if build_metadata.release_channel:
