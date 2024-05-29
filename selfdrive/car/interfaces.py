01d58f5d592228eed4886386fc61267175f74414
Jason Wen <haibin.wen3@gmail.com>
2024-05-29 00:10:50 -0400

    Remove unused

--- selfdrive/car/interfaces.py
+++ selfdrive/car/interfaces.py
@@ -13,7 +13,7 @@ from cereal import car
 from openpilot.common.basedir import BASEDIR
 from openpilot.common.conversions import Conversions as CV
 from openpilot.common.simple_kalman import KF1D, get_kalman_gain
-from openpilot.common.numpy_fast import clip, interp
+from openpilot.common.numpy_fast import clip
 from openpilot.common.params import Params
 from openpilot.common.realtime import DT_CTRL
 from openpilot.selfdrive.car import apply_hysteresis, gen_empty_fingerprint, scale_rot_inertia, scale_tire_stiffness, STD_CARGO_KG
