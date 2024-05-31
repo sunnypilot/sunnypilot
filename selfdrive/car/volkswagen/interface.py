57b31abaa89a75e6b79d9a96f442b128e9ed249f
Jason Wen <haibin.wen3@gmail.com>
2024-05-30 23:38:36 -0400

    Volkswagen: Support for distance button hold to toggle between Chill & Experimental Mode

--- selfdrive/car/volkswagen/interface.py
+++ selfdrive/car/volkswagen/interface.py
@@ -157,7 +157,7 @@ class CarInterface(CarInterfaceBase):
         self.CS.accEnabled = False
       self.CS.accEnabled = ret.cruiseState.enabled or self.CS.accEnabled
 
-    ret, self.CS = self.get_sp_common_state(ret, self.CS)
+    ret, self.CS = self.get_sp_common_state(ret, self.CS, gap_button=bool(self.CS.buttonStates["gapAdjustCruise"]))
 
     # MADS BUTTON
     if self.CS.out.madsEnabled != self.CS.madsEnabled:
