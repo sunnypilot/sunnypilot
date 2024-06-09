aae38ac5ab3cc8551a56e7072387e55248bc2718
Jason Wen <haibin.wen3@gmail.com>
2024-06-09 00:00:12 -0400

    GitLab Pipelines: Add script for syncing LFS between comma and sunnypilot
    This commit adds a new script, 'sync-lfs.sh'. This script assists by syncing the git large file storage (LFS) system. It backs up the current LFS configuration, uses the comma-separated configuration for operations like fetch and pull, and then restores the original configuration before pushing to the origin's main branch.

---
+++ release/ci/sync-lfs.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+mv .lfsconfig .lfsconfig.bak
+mv .lfsconfig-comma .lfsconfig
+git lfs fetch --all; git lfs pull
+mv .lfsconfig .lfsconfig-comma
+mv .lfsconfig.bak .lfsconfig
+git lfs fetch --all; git lfs push origin main
