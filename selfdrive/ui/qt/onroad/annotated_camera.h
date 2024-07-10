ba129989868274ed4bf8d91806ffeeb0620b3cbd
Jason Wen <haibin.wen3@gmail.com>
2024-07-09 22:59:44 -0400

    ui: onroad: `drawLead` cleanup

--- selfdrive/ui/qt/onroad/annotated_camera.h
+++ selfdrive/ui/qt/onroad/annotated_camera.h
@@ -211,7 +211,8 @@ protected:
   void showEvent(QShowEvent *event) override;
   void updateFrameMat() override;
   void drawLaneLines(QPainter &painter, const UIState *s);
-  void drawLead(QPainter &painter, const cereal::RadarState::LeadData::Reader &lead_data, const QPointF &vd, int num, float radar_d_rel, float v_ego, float radar_v_rel, int chevron_data, bool isMetric);
+  void drawLead(QPainter &painter, const cereal::RadarState::LeadData::Reader &lead_data, const QPointF &vd,
+                int num, const cereal::CarState::Reader &car_data, int chevron_data);
   void drawHud(QPainter &p);
   void drawDriverState(QPainter &painter, const UIState *s);
   inline QColor redColor(int alpha = 255) { return QColor(201, 34, 49, alpha); }
