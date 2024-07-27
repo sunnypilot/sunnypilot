f242f6b55d0d5d6dddb0f8c2f738bbe461946881
DevTekVE <devtekve@gmail.com>
2024-07-27 23:07:19 +0200

    No more rave

--- selfdrive/ui/sunnypilot/ui.cc
+++ selfdrive/ui/sunnypilot/ui.cc
@@ -293,7 +293,21 @@ DeviceSP::DeviceSP(QObject *parent) : Device(parent){
 
 //todo: revisit this
 void DeviceSP::updateBrightness(const UIStateSP &s) {
-  Device::updateBrightness(s);
+  float clipped_brightness = offroad_brightness;
+  if (s.scene.started && s.scene.light_sensor > 0) {
+    clipped_brightness = s.scene.light_sensor;
+
+    // CIE 1931 - https://www.photonstophotos.net/GeneralTopics/Exposure/Psychometric_Lightness_and_Gamma.htm
+    if (clipped_brightness <= 8) {
+      clipped_brightness = (clipped_brightness / 903.3);
+    } else {
+      clipped_brightness = std::pow((clipped_brightness + 16.0) / 116.0, 3.0);
+    }
+
+    // Scale back to 10% to 100%
+    clipped_brightness = std::clamp(100.0f * clipped_brightness, 10.0f, 100.0f);
+  }
+
   int brightness = brightness_filter.update(clipped_brightness);
   if (!awake) {
     brightness = 0;
