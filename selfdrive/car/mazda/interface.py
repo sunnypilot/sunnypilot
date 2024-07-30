edddc496d6f3d1b8ede1ad6a78b91a402b28baae
Jason Wen <haibin.wen3@gmail.com>
2024-07-30 06:58:35 -0800

    MADS: Map cruise main and lateral buttons to button events * MADS: Map cruise main and lateral buttons to button events
    * different style
    
    * nah
    
    (cherry picked from commit 4d7afe3a7bbcd4f31ee309a6aa2f5db93367daae)

--- selfdrive/car/mazda/interface.py
+++ selfdrive/car/mazda/interface.py
@@ -40,10 +40,8 @@ class CarInterface(CarInterfaceBase):
     self.sp_update_params()
 
     # TODO: add button types for inc and dec
-    self.CS.button_events = [
-      *self.CS.button_events,
-      *create_button_events(self.CS.distance_button, self.CS.prev_distance_button, {1: ButtonType.gapAdjustCruise})
-    ]
+    self.CS.button_events.extend(create_button_events(self.CS.distance_button, self.CS.prev_distance_button, {1: ButtonType.gapAdjustCruise}))
+    self.CS.button_events.extend(create_button_events(self.CS.lkas_enabled, self.CS.prev_lkas_enabled, {1: ButtonType.altButton1}))
 
     self.CS.mads_enabled = self.get_sp_cruise_main_state(ret, self.CS)
 
@@ -54,7 +52,7 @@ class CarInterface(CarInterfaceBase):
       if self.enable_mads:
         if not self.CS.prev_mads_enabled and self.CS.mads_enabled:
           self.CS.madsEnabled = True
-        if self.CS.prev_lkas_enabled != self.CS.lkas_enabled:
+        if any(b.type == ButtonType.altButton1 and b.pressed for b in self.CS.button_events):
           self.CS.madsEnabled = not self.CS.madsEnabled
         self.CS.madsEnabled = self.get_acc_mads(ret.cruiseState.enabled, self.CS.accEnabled, self.CS.madsEnabled)
     else:
