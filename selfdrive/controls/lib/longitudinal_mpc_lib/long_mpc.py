1937140890c5132bde3f4d950f617a1450d7e902
Kumar <36933347+rav4kumar@users.noreply.github.com>
2024-08-10 22:17:14 -0700

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -63,11 +63,11 @@ def get_jerk_factor(personality=custom.LongitudinalPersonalitySP.standard):
   elif personality==custom.LongitudinalPersonalitySP.standard:
     return 1.0
   elif personality==custom.LongitudinalPersonalitySP.moderate:
-    return 0.8
+    return 1.0
   elif personality==custom.LongitudinalPersonalitySP.aggressive:
-    return 0.65
+    return 0.8
   elif personality==custom.LongitudinalPersonalitySP.overtake:
-    return 0.1
+    return 0.3
   else:
     raise NotImplementedError("Longitudinal personality not supported")
 
@@ -89,17 +89,17 @@ def get_T_FOLLOW(personality=custom.LongitudinalPersonalitySP.standard):
 
 def get_dynamic_personality(v_ego, personality=custom.LongitudinalPersonalitySP.standard):
   if personality==custom.LongitudinalPersonalitySP.relaxed:
-    x_vel =  [0,    20.,   27.7]
-    y_dist = [1.75, 1.75,  1.83]
+    x_vel =  [0,    3.,   3.01,  20.,   27.7]
+    y_dist = [1.0,  1.0,  1.75,  1.75,  1.83]
   elif personality==custom.LongitudinalPersonalitySP.standard:
-    x_vel =  [0,    20.,   27.7]
-    y_dist = [1.75, 1.75,  1.80]
+    x_vel =  [0,    3.,   3.01,  20.,   27.7]
+    y_dist = [1.0,  1.0,  1.75,  1.75,  1.80]
   elif personality==custom.LongitudinalPersonalitySP.moderate:
-    x_vel =  [0,    20.,   27.7]
-    y_dist = [1.45, 1.45,  1.50]
+    x_vel =  [0,    3.,   3.01,  20.,   27.7]
+    y_dist = [1.0,  1.0,  1.45,  1.45,  1.50]
   elif personality==custom.LongitudinalPersonalitySP.aggressive:
-    x_vel =  [0,    20.,   27.7]
-    y_dist = [1.20, 1.20,  1.30]
+    x_vel =  [0,    3.,   3.01,  20.,   27.7]
+    y_dist = [1.0,  1.0,  1.20,  1.20,  1.30]
   else:
     raise NotImplementedError("Dynamic personality not supported")
   return np.interp(v_ego, x_vel, y_dist)
