28234524bc1dc9452d4420f98851bdc74a28450f
Jason Wen <haibin.wen3@gmail.com>
2024-08-12 10:41:37 -0400

    less

--- selfdrive/car/sunnypilot/fingerprinting.py
+++ selfdrive/car/sunnypilot/fingerprinting.py
@@ -1,6 +1,5 @@
 from collections.abc import Callable
 
-from openpilot.selfdrive.car.fingerprints import eliminate_incompatible_cars, all_legacy_fingerprint_cars
 import cereal.messaging as messaging
 from openpilot.selfdrive.car import gen_empty_fingerprint
 
@@ -16,9 +15,7 @@ def get_one_can(logcan):
 
 def can_fingerprint(next_can: Callable) -> tuple[str | None, dict[int, dict]]:
   finger = gen_empty_fingerprint()
-  candidate_cars = {i: all_legacy_fingerprint_cars() for i in [0, 1]}  # attempt fingerprint on both bus 0 and 1
   frame = 0
-  car_fingerprint = None
   done = False
 
   while not done:
@@ -32,23 +29,9 @@ def can_fingerprint(next_can: Callable) -> tuple[str | None, dict[int, dict]]:
           finger[can.src] = {}
         finger[can.src][can.address] = len(can.dat)
 
-      for b in candidate_cars:
-        # Ignore extended messages and VIN query response.
-        if can.src == b and can.address < 0x800 and can.address not in (0x7df, 0x7e0, 0x7e8):
-          candidate_cars[b] = eliminate_incompatible_cars(can, candidate_cars[b])
-
-    # if we only have one car choice and the time since we got our first
-    # message has elapsed, exit
-    for b in candidate_cars:
-      if len(candidate_cars[b]) == 1 and frame > FRAME_FINGERPRINT:
-        # fingerprint done
-        car_fingerprint = candidate_cars[b][0]
-
-    # bail if no cars left or we've been waiting for more than 2s
-    failed = (all(len(cc) == 0 for cc in candidate_cars.values()) and frame > FRAME_FINGERPRINT) or frame > 200
-    succeeded = car_fingerprint is not None
-    done = failed or succeeded
+    # bail if we've been waiting for more than 2s
+    done = frame > 100
 
     frame += 1
 
-  return car_fingerprint, finger
+  return finger
