d7f502f4eaa306cb77624dd7f4714210e180cdad
Jason Wen <haibin.wen3@gmail.com>
2024-08-13 01:19:10 -0400

    diff

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -646,10 +646,9 @@ class Controls:
     if not self.joystick_mode:
       speeds = long_plan.speeds
       a_lead = self.sm['radarState'].leadOne.aLeadK
-      resume = self.enabled_long and CS.standstill and self.CP.carName == "hyundai"
+      resume = False
       if len(speeds):
-        resume = resume and speeds[-1] > 0.1
-      resume = resume and a_lead > 0.1
+        resume = self.enabled_long and CS.standstill and self.CP.carName == "hyundai" and speeds[-1] > 0.1 and a_lead > 0.1
 
       # accel PID loop
       pid_accel_limits = self.CI.get_pid_accel_limits(self.CP, CS.vEgo, self.v_cruise_helper.v_cruise_kph * CV.KPH_TO_MS)
