a176851afe2e0264966d661565da521efba7a62e
rav4kumar <meetkumardesai@gmail.com>
2024-08-15 05:32:29 -0700

    readjust low speed

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -98,8 +98,8 @@ def get_dynamic_personality(v_ego, personality=custom.LongitudinalPersonalitySP.
     x_vel =  [0,    3.,   3.01,  20.,   27.7]
     y_dist = [1.0,  1.0,  1.45,  1.45,  1.50]
   elif personality==custom.LongitudinalPersonalitySP.aggressive:
-    x_vel =  [0,    2.,   2.01,  20.,   27.7]
-    y_dist = [1.0,  1.0,  1.20,  1.20,  1.30]
+    x_vel =  [0,    2.,   2.01,  14,   14.01,  20.,   27.7]
+    y_dist = [0.8,  0.8,  1.00,  1.00, 1.10,   1.10,  1.30]
   else:
     raise NotImplementedError("Dynamic personality not supported")
   return np.interp(v_ego, x_vel, y_dist)
