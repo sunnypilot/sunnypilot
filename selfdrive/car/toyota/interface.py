929ea507f23704eb490641eb6bcd9ae9c29e32d0
Kumar <36933347+rav4kumar@users.noreply.github.com>
2024-09-04 07:33:11 -0700

    tt

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -162,8 +162,8 @@ class CarInterface(CarInterfaceBase):
 
     # hand tuned (August 12, 2024)
     def custom_tss2_longitudinal_tuning():
-      ret.vEgoStopping = 0.25
-      ret.vEgoStarting = 0.25
+      ret.vEgoStopping = 0.01
+      ret.vEgoStarting = 0.01
       ret.stoppingDecelRate = 0.0074
 
     def default_tss2_longitudinal_tuning():
@@ -178,8 +178,8 @@ class CarInterface(CarInterfaceBase):
     tune = ret.longitudinalTuning
     if candidate in TSS2_CAR or ret.enableGasInterceptorDEPRECATED:
       if sp_tss2_long_tune:
-        tune.kiBP = [ 0.,  5.,   12.,  20., 27., 40.]
-        tune.kiV =  [.35, .23,  .20, .17, .10, .06]
+        tune.kiBP = [ 0.,  2.,   12.,  20.,  27.,  40.]
+        tune.kiV =  [.35,  .306, .215, .186, .102, .06]
         custom_tss2_longitudinal_tuning()
       else:
         tune.kiBP = [5., 35.]
--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -63,9 +63,9 @@ def get_jerk_factor(personality=custom.LongitudinalPersonalitySP.standard):
   elif personality==custom.LongitudinalPersonalitySP.standard:
     return 1.0
   elif personality==custom.LongitudinalPersonalitySP.moderate:
-    return 0.5
+    return 0.8
   elif personality==custom.LongitudinalPersonalitySP.aggressive:
-    return 0.1
+    return 0.2
   elif personality==custom.LongitudinalPersonalitySP.overtake:
     return 0.1
   else:
@@ -89,17 +89,17 @@ def get_T_FOLLOW(personality=custom.LongitudinalPersonalitySP.standard):
 
 def get_dynamic_personality(v_ego, personality=custom.LongitudinalPersonalitySP.standard):
   if personality==custom.LongitudinalPersonalitySP.relaxed:
-    x_vel =  [0,    5.,   5.01,  20.,   27.7]
-    y_dist = [1.0,  1.0,  1.75,  1.75,  1.83]
+    x_vel =  [0,    5.,   5.01, 8.33,  8.34,  27.69, 27.7]
+    y_dist = [0.1,  0.1,  1.2,  1.2,   1.75,  1.75,  1.83]
   elif personality==custom.LongitudinalPersonalitySP.standard:
-    x_vel =  [0,    20.,   27.7]
-    y_dist = [1.75, 1.75,  1.70]
+    x_vel =  [0,    5.0,  5.01, 8.33,  8.34,  27.69, 27.7]
+    y_dist = [0.1,  0.1,  1.2,  1.2,   1.75,  1.75,  1.80]
   elif personality==custom.LongitudinalPersonalitySP.moderate:
-    x_vel =  [0,    27.69, 27.7]
-    y_dist = [1.40, 1.40,  1.45]
+    x_vel =  [0,    5.,   5.01, 8.33,  8.34,  27.69, 27.7]
+    y_dist = [0.1,  0.1,  1.2,  1.2,   1.45,  1.45,  1.50]
   elif personality==custom.LongitudinalPersonalitySP.aggressive:
-    x_vel =  [0,    8.,   20.0, 20.01, 27.69, 27.7]
-    y_dist = [0,    1.12, 1.12, 1.12,  1.12,  1.20]
+    x_vel =  [0,    5.,   5.01,  20.0, 20.01, 27.69, 27.7]
+    y_dist = [0.1,  0.1,  1.2,   1.2,  1.25,  1.25,  1.3]
   else:
     raise NotImplementedError("Dynamic personality not supported")
   return np.interp(v_ego, x_vel, y_dist)
