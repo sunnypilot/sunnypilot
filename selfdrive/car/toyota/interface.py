9ed43705c4d3d091f2e2367c4cbd9a34a93366b4
Kumar <36933347+rav4kumar@users.noreply.github.com>
2024-09-06 07:27:07 -0700

    finnefriyah!

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -162,8 +162,8 @@ class CarInterface(CarInterfaceBase):
 
     # hand tuned (August 12, 2024)
     def custom_tss2_longitudinal_tuning():
-      ret.vEgoStopping = 0.01
-      ret.vEgoStarting = 0.01
+      ret.vEgoStopping = 0.25
+      ret.vEgoStarting = 0.25
       ret.stoppingDecelRate = 0.3
 
     def default_tss2_longitudinal_tuning():
@@ -178,10 +178,8 @@ class CarInterface(CarInterfaceBase):
     tune = ret.longitudinalTuning
     if candidate in TSS2_CAR or ret.enableGasInterceptorDEPRECATED:
       if sp_tss2_long_tune:
-        #tune.kiBP = [0., 5., 35.]
-        #tune.kiV = [2.0, 1.5, 0.5]
-        tune.kiBP = [0.,   2.,   8.,   12.,  20.,  27.,  40.]
-        tune.kiV =  [.34,  .300, .212, .20, .184, .101, .06]
+        tune.kiBP = [0.,   1.,   8.,   12.,  20.,  27.,  40.]
+        tune.kiV =  [.34,  .32, .20, .198, .184, .101, .06]
         custom_tss2_longitudinal_tuning()
       else:
         tune.kiBP = [5., 35.]
