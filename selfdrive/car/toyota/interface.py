2dcc838f73f3ecbd90aa66ba1fdf3697ab600085
rav4kumar <meetkumardesai@gmail.com>
2024-09-10 07:03:32 -0700

    less bp

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -162,9 +162,9 @@ class CarInterface(CarInterfaceBase):
 
     # hand tuned (August 12, 2024)
     def custom_tss2_longitudinal_tuning():
-      ret.vEgoStopping = 0.10
+      ret.vEgoStopping = 0.01
       ret.vEgoStarting = 0.01
-      ret.stoppingDecelRate = 0.2
+      ret.stoppingDecelRate = 0.1
 
     def default_tss2_longitudinal_tuning():
       ret.vEgoStopping = 0.25
