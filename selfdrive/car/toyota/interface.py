187cefb2dffc688b70fb072e33a1ef409902cae3
Kumar <36933347+rav4kumar@users.noreply.github.com>
2024-09-13 11:42:37 -0700

    Update interface.py

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -167,9 +167,9 @@ class CarInterface(CarInterfaceBase):
       ret.stoppingDecelRate = 0.1
 
     def default_tss2_longitudinal_tuning():
-      ret.vEgoStopping = 0.25
-      ret.vEgoStarting = 0.25
-      ret.stoppingDecelRate = 0.3  # reach stopping target smoothly
+      ret.vEgoStopping = 0.01
+      ret.vEgoStarting = 0.01
+      ret.stoppingDecelRate = 0.1  # reach stopping target smoothly
 
     def default_longitudinal_tuning():
       tune.kiBP = [0., 5., 35.]
