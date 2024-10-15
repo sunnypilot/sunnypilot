cb1b1aaa7b0146bb79658d35d5ac6f69090d8b1a
Kumar <36933347+rav4kumar@users.noreply.github.com>
2024-10-15 16:11:53 -0700

    Update interface.py

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -179,7 +179,7 @@ class CarInterface(CarInterfaceBase):
     if candidate in TSS2_CAR or ret.enableGasInterceptorDEPRECATED:
       if sp_tss2_long_tune:
         tune.kiBP = [0.,   2.,    12.,  20.,   27.]
-        tune.kiV = [0.348,  0.308, 0.218, 0.174, 0.101]
+        tune.kiV = [0.348,  0.308, 0.218, 0.174, 0.10]
         custom_tss2_longitudinal_tuning()
       else:
         tune.kpV = [0.0]
