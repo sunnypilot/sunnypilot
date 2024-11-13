ca0020e6139119c780bc7e89c9eb7ccdd6bd25c3
Jason Wen <haibin.wen3@gmail.com>
2024-11-13 13:12:24 -0500

    should be the whole class

--- sunnypilot/selfdrive/car/interfaces.py
+++ sunnypilot/selfdrive/car/interfaces.py
@@ -8,5 +8,5 @@ from openpilot.sunnypilot.mads.mads import MadsParams
 class CarStateBaseSP(ABC):
   def __init__(self, CP: structs.CarParams):
     self.alt_button = 0
-    self.mads_enabled_toggle = MadsParams.read_enabled_param()
+    self.mads_enabled_toggle = MadsParams().read_enabled_param()
     self.button_events: list[structs.CarState.ButtonEvent] = []
