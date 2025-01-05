767f78bbcf17b08edbc20f283d10a1990bf4264f
Jason Wen <haibin.wen3@gmail.com>
2025-01-05 08:30:28 -0500

    try this

--- sunnypilot/modeld/run_compile.py
+++ sunnypilot/modeld/run_compile.py
@@ -2,6 +2,10 @@ import sys
 from subprocess import run
 
 correct_path = "/data/openpilot/sunnypilot/tinygrad"
+incorrect_path = "/data/sunnypilot/tinygrad"
+
+sys.path = [p for p in sys.path if p != incorrect_path]
+
 if correct_path not in sys.path:
   sys.path.insert(0, correct_path)
 
