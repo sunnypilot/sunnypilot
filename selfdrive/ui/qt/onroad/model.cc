34e415acf52b6e5602bf7a6a393752f07d83ab13
rav4kumar <meetkumardesai@gmail.com>
2025-01-11 11:34:38 -0700

    gg

--- selfdrive/ui/qt/onroad/model.cc
+++ selfdrive/ui/qt/onroad/model.cc
@@ -107,13 +107,13 @@ void ModelRenderer::drawLaneLines(QPainter &painter) {
 
 void ModelRenderer::drawPath(QPainter &painter, const cereal::ModelDataV2::Reader &model, int height) {
 
-  //auto *s = uiState();
-  //auto &sm = *(s->sm);
-  //const auto long_plan_sp = sm["longitudinalPlanSP"].getLongitudinalPlanSP();
-  //bool exp_mode_path = (long_plan_sp.getDynamicExperimentalControl() && long_plan_sp.getMpcSource() == cereal::MpcSource::BLENDED) ||
-  //                     (!long_plan_sp.getDynamicExperimentalControl() && sm["selfdriveState"].getSelfdriveState().getExperimentalMode());
+  auto *s = uiState();
+  auto &sm = *(s->sm);
+  const auto long_plan_sp = sm["longitudinalPlanSP"].getLongitudinalPlanSP();
+  bool exp_mode_path = (long_plan_sp.getDynamicExperimentalControl() && long_plan_sp.getMpcSource() == cereal::MpcSource::BLENDED) ||
+                       (!long_plan_sp.getDynamicExperimentalControl() && sm["selfdriveState"].getSelfdriveState().getExperimentalMode());
   QLinearGradient bg(0, height, 0, 0);
-  if (experimental_mode) {
+  if (exp_mode_path) {
     // The first half of track_vertices are the points for the right side of the path
     const auto &acceleration = model.getAcceleration().getX();
     const int max_len = std::min<int>(track_vertices.length() / 2, acceleration.size());
