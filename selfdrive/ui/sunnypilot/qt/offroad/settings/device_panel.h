5be56620622deae5e2dada8524798226f6212846
Jason Wen <haibin.wen3@gmail.com>
2025-01-12 10:04:35 -0500

    Revert "try this"
    This reverts commit 3ce67680675bcecc04515c798276ffef79442c72.

--- selfdrive/ui/sunnypilot/qt/offroad/settings/device_panel.h
+++ selfdrive/ui/sunnypilot/qt/offroad/settings/device_panel.h
@@ -14,7 +14,7 @@ class DevicePanelSP : public DevicePanel {
   Q_OBJECT
 
 public:
-  explicit DevicePanelSP(SettingsWindow *parent = 0);
+  explicit DevicePanelSP(SettingsWindowSP *parent = 0);
 
 private:
   std::map<QString, PushButtonSP*> buttons;
