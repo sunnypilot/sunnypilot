7113d4a76ac7af3a640822ce327f1dd784bd73f6
rav4kumar <meetkumardesai@gmail.com>
2025-01-12 15:54:50 -0700

    smoother trans

--- sunnypilot/selfdrive/controls/lib/dec/dec.py
+++ sunnypilot/selfdrive/controls/lib/dec/dec.py
@@ -90,7 +90,7 @@ class WeightedMovingAverageCalculator:
   def __init__(self, window_size):
     self.window_size = window_size
     self.data = []
-    self.weights = np.linspace(1, 3, window_size)  # Linear weights, adjust as needed
+    self.weights = np.linspace(1, 5, window_size)  # Linear weights, adjust as needed
 
   def add_data(self, value: float) -> None:
     if len(self.data) == self.window_size:
@@ -171,7 +171,7 @@ class DynamicExperimentalController:
     """
     Adapts the slow-down threshold based on vehicle speed and recent behavior.
     """
-    return interp(self._v_ego_kph, SLOW_DOWN_BP, SLOW_DOWN_DIST) * (1.0 + 0.03 * np.log(1 + len(self._slow_down_gmac.data)))
+    return interp(self._v_ego_kph, SLOW_DOWN_BP, SLOW_DOWN_DIST) * (1.0 + 0.05 * np.log(1 + len(self._slow_down_gmac.data)))
 
   def _smoothed_lead_detection(self, lead_prob: float, smoothing_factor: float = 0.2) -> bool:
     """
@@ -207,9 +207,9 @@ class DynamicExperimentalController:
 
     # lead detection with smoothing
     self._lead_gmac.add_data(lead_one.status)
-    self._has_lead_filtered = self._lead_gmac.get_weighted_average() > LEAD_PROB
-    # lead_prob = self._lead_gmac.get_weighted_average() or 0
-    # self._has_lead_filtered = self._smoothed_lead_detection(lead_prob)
+    #self._has_lead_filtered = self._lead_gmac.get_weighted_average() > LEAD_PROB
+    lead_prob = self._lead_gmac.get_weighted_average() or 0
+    self._has_lead_filtered = self._smoothed_lead_detection(lead_prob)
 
     # adaptive slow down detection
     adaptive_threshold = self._adaptive_slowdown_threshold()
