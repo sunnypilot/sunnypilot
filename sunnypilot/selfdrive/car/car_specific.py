16d692f076846bb4fa1d30f40f915e6711ae212f
Jason Wen <haibin.wen3@gmail.com>
2025-01-12 01:22:39 -0500

    comments

--- sunnypilot/selfdrive/car/car_specific.py
+++ sunnypilot/selfdrive/car/car_specific.py
@@ -1,4 +1,11 @@
-from cereal import car, log
+"""
+Copyright (c) 2021-, Haibin Wen, sunnypilot, and a number of other contributors.
+
+This file is part of sunnypilot and is licensed under the MIT License.
+See the LICENSE.md file in the root directory for more details.
+"""
+
+from cereal import log
 from opendbc.car import structs
 
 from openpilot.selfdrive.selfdrived.events import Events
--- sunnypilot/selfdrive/car/interfaces.py
+++ sunnypilot/selfdrive/car/interfaces.py
@@ -1,3 +1,10 @@
+"""
+Copyright (c) 2021-, Haibin Wen, sunnypilot, and a number of other contributors.
+
+This file is part of sunnypilot and is licensed under the MIT License.
+See the LICENSE.md file in the root directory for more details.
+"""
+
 from opendbc.car import Bus, structs
 from opendbc.car.can_definitions import CanRecvCallable, CanSendCallable
 from opendbc.car.car_helpers import can_fingerprint
