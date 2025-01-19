ae65b7bc3302f6fe8528df490d97c5b10f181446
Jason Wen <haibin.wen3@gmail.com>
2025-01-19 12:19:42 -0500

    Revert "Refactor: Introduce ButtonHoldTracker to manage button hold durations (#593)"
    This reverts commit 7ee7e73ce708f9b52aabed23e398ccf94b1834cf.

--- sunnypilot/selfdrive/car/experimental_switcher.py
+++ sunnypilot/selfdrive/car/experimental_switcher.py
@@ -5,25 +5,37 @@ This file is part of sunnypilot and is licensed under the MIT License.
 See the LICENSE.md file in the root directory for more details.
 """
 
-from openpilot.sunnypilot.selfdrive.car.button_hold_tracker import ButtonHoldTracker, ButtonType, EventName
+from cereal import car, log
+
+ButtonType = car.CarState.ButtonEvent.Type
+EventName = log.OnroadEvent.EventName
 
 DISTANCE_LONG_PRESS = 50
 
 
 class ExperimentalSwitcher:
-  def __init__(self, params, button_type=ButtonType.gapAdjustCruise):
+  def __init__(self, params):
     self.params = params
-    self.button_type = button_type
-    self._button_tracker = ButtonHoldTracker()
-    self.experimental_mode_switched = False  # Meant to be toggled off by this class's children.
+
+    self.button_timers = {ButtonType.gapAdjustCruise: 0}
+    self.experimental_mode_switched = False
 
   def update(self, CS, events, experimental_mode):
-    self._button_tracker.update(CS)
     if CS.cruiseState.available:
+      self.update_button_timers(CS)
       self.update_mode(events, experimental_mode)
 
+  def update_button_timers(self, CS) -> None:
+    for k in self.button_timers:
+      if self.button_timers[k] > 0:
+        self.button_timers[k] += 1
+
+    for b in CS.buttonEvents:
+      if b.type.raw in self.button_timers:
+        self.button_timers[b.type.raw] = 1 if b.pressed else 0
+
   def update_mode(self, events, experimental_mode) -> None:
-    if self._button_tracker.get_hold_duration(self.button_type) >= DISTANCE_LONG_PRESS and not self.experimental_mode_switched:
+    if self.button_timers[ButtonType.gapAdjustCruise] >= DISTANCE_LONG_PRESS and not self.experimental_mode_switched:
       experimental_mode = not experimental_mode
       self.params.put_bool_nonblocking("ExperimentalMode", experimental_mode)
       events.add(EventName.experimentalModeSwitched)
