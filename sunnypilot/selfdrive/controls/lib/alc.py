4324dc3918ed9801dc6bd44b26b47ba5d7990ac1
rav4kumar <meetkumardesai@gmail.com>
2025-03-09 21:28:23 -0700

    only for sunny

--- sunnypilot/selfdrive/controls/lib/alc.py
+++ sunnypilot/selfdrive/controls/lib/alc.py
@@ -1,3 +1,10 @@
+"""
+Copyright (c) 2021-, Haibin Wen, sunnypilot, and a number of other contributors.
+
+This file is part of sunnypilot and is licensed under the MIT License.
+See the LICENSE.md file in the root directory for more details.
+"""
+
 from openpilot.common.realtime import DT_MDL
 from openpilot.common.params import Params
 
