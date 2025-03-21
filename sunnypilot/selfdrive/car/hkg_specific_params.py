ae5fe3a6fcbcb4fb41eb4b9d14febbb1a07ba19e
discountchubbs <alexgrant990@gmail.com>
2025-03-21 07:01:04 -0700

    CP to carcontroller.py

--- sunnypilot/selfdrive/car/hkg_specific_params.py
+++ sunnypilot/selfdrive/car/hkg_specific_params.py
@@ -2,13 +2,13 @@ from opendbc.car import structs
 from opendbc.sunnypilot.car.hyundai.values import HyundaiFlagsSP
 from openpilot.common.params import Params
 
-
 class HyundaiLongTuneParams:
     @staticmethod
     def param(params, key: str) -> bool:
         val = params.get(key)
-        return val in [b"1", b"2"]
-
+        if isinstance(val, bytes):
+            val = val.decode("utf-8")
+        return val in ["1", "2"]
 
 def set_hyundai_long_tune_flag(CP: structs.CarParams):
     params = Params()
