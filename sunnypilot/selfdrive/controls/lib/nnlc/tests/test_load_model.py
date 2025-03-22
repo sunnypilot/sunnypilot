e9918ad45ea8b24d09a76ac6eb4afaf41e33581d
Jason Wen <haibin.wen3@gmail.com>
2025-03-22 07:39:01 -0400

    test to verify model loading

---
+++ sunnypilot/selfdrive/controls/lib/nnlc/tests/test_load_model.py
@@ -0,0 +1,29 @@
+from parameterized import parameterized
+
+from opendbc.car.car_helpers import interfaces
+from opendbc.car.honda.values import CAR as HONDA
+from opendbc.car.toyota.values import CAR as TOYOTA
+from openpilot.common.params import Params
+from openpilot.selfdrive.car.helpers import convert_to_capnp
+from openpilot.selfdrive.controls.lib.latcontrol_torque import LatControlTorque
+from openpilot.sunnypilot.selfdrive.car import interfaces as sunnypilot_interfaces
+
+
+class TestNNTorqueModel:
+
+  @parameterized.expand([HONDA.HONDA_CIVIC, TOYOTA.TOYOTA_RAV4, TOYOTA.TOYOTA_ALPHARD_TSS2])
+  def test_load_model(self, car_name):
+    Params().put_bool("NeuralNetworkLateralControl", True)
+
+    CarInterface = interfaces[car_name]
+    CP = CarInterface.get_non_essential_params(car_name)
+    CP_SP = CarInterface.get_non_essential_params_sp(CP, car_name)
+    CI = CarInterface(CP, CP_SP)
+
+    sunnypilot_interfaces.initialize_neural_network_lateral_control(CP, CP_SP, Params())
+
+    CP_SP = convert_to_capnp(CP_SP)
+
+    controller = LatControlTorque(CP.as_reader(), CP_SP.as_reader(), CI)
+
+    assert controller.extension.model is not None
