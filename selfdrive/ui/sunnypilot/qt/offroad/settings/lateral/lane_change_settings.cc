02e4bdba42976e0c997de4e2c04d78ae7a761d8d
Kumar <36933347+rav4kumar@users.noreply.github.com>
2025-03-22 21:22:25 -0700

    Update selfdrive/ui/sunnypilot/qt/offroad/settings/lateral/lane_change_settings.cc
    Co-authored-by: Jason Wen <haibin.wen3@gmail.com>

--- selfdrive/ui/sunnypilot/qt/offroad/settings/lateral/lane_change_settings.cc
+++ selfdrive/ui/sunnypilot/qt/offroad/settings/lateral/lane_change_settings.cc
@@ -1,11 +1,10 @@
-//
-// Created by kumar on 3/10/25.
-//
 /**
  * Copyright (c) 2021-, Haibin Wen, sunnypilot, and a number of other contributors.
  *
  * This file is part of sunnypilot and is licensed under the MIT License.
  * See the LICENSE.md file in the root directory for more details.
+ *
+ * Created by kumar on March 10, 2025
  */
 
 #include "selfdrive/ui/sunnypilot/qt/offroad/settings/lateral/lane_change_settings.h"
