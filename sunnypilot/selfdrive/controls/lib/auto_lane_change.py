a1dfa251277dc383290be5601b39af250e878992
Jason Wen <haibin.wen3@gmail.com>
2025-03-23 23:49:21 -0400

    take out magic numbers

--- sunnypilot/selfdrive/controls/lib/auto_lane_change.py
+++ sunnypilot/selfdrive/controls/lib/auto_lane_change.py
@@ -4,18 +4,34 @@ Copyright (c) 2021-, Haibin Wen, sunnypilot, and a number of other contributors.
 This file is part of sunnypilot and is licensed under the MIT License.
 See the LICENSE.md file in the root directory for more details.
 """
+from enum import IntEnum
+
 from cereal import log
 from openpilot.common.realtime import DT_MDL
 from openpilot.common.params import Params
 
+
+class AutoLaneChangeState(IntEnum):
+  OFF = -1
+  NUDGE = 0
+  NUDGELESS = 1
+  HALF_SECOND = 2
+  ONE_SECOND = 3
+  ONE_POINT_FIVE_SECOND = 4
+  TWO_SECONDS = 5  # default
+
+
 AUTO_LANE_CHANGE_TIMER = {
-  -1: 0.0,
-  0: 0.0,
-  1: 0.1,
-  2: 0.5,
-  3: 1.0,
-  4: 1.5,
+  AutoLaneChangeState.OFF: 0.0,                    # Off
+  AutoLaneChangeState.NUDGE: 0.0,                  # Nudge
+  AutoLaneChangeState.NUDGELESS: 0.1,              # Nudgeless
+  AutoLaneChangeState.HALF_SECOND: 0.5,            # 0.5-second delay
+  AutoLaneChangeState.ONE_SECOND: 1.0,             # 1-second delay
+  AutoLaneChangeState.ONE_POINT_FIVE_SECOND: 1.5,  # 1.5-second delay
+  AutoLaneChangeState.TWO_SECONDS: 2.0,            # 2 second delay default
 }
+
+TIMER_DISABLED = -1
 
 
 class AutoLaneChangeController:
@@ -24,7 +40,7 @@ class AutoLaneChangeController:
     self.param_s = Params()
     self.lane_change_wait_timer = 0.0
     self.param_read_counter = 0
-    self.lane_change_set_timer = 1
+    self.lane_change_set_timer = AutoLaneChangeState.NUDGE
     self.lane_change_bsm_delay = False
     self.prev_brake_pressed = False
     self.read_params()
@@ -38,20 +54,21 @@ class AutoLaneChangeController:
     try:
       self.lane_change_set_timer = int(self.param_s.get("AutoLaneChangeTimer", encoding="utf8"))
     except (ValueError, TypeError):
-      self.lane_change_set_timer = 1
+      self.lane_change_set_timer = AutoLaneChangeState.NUDGE
 
   def update(self, blindspot_detected: bool, brake_pressed: bool):
     if self.param_read_counter % 50 == 0:
       self.read_params()
     self.param_read_counter += 1
 
-    lane_change_auto_timer = AUTO_LANE_CHANGE_TIMER.get(self.lane_change_set_timer, 2.0)
+    lane_change_auto_timer = AUTO_LANE_CHANGE_TIMER.get(self.lane_change_set_timer,
+                                                        AUTO_LANE_CHANGE_TIMER[AutoLaneChangeState.TWO_SECONDS])
 
     self.lane_change_wait_timer += DT_MDL
 
     if self.lane_change_bsm_delay and blindspot_detected and lane_change_auto_timer:
-      if lane_change_auto_timer == 0.1:
-        self.lane_change_wait_timer = -1
+      if lane_change_auto_timer == AUTO_LANE_CHANGE_TIMER[AutoLaneChangeState.NUDGELESS]:
+        self.lane_change_wait_timer = TIMER_DISABLED
       else:
         self.lane_change_wait_timer = lane_change_auto_timer - 1
 
