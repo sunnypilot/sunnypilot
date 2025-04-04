fda41bc019af2074fbb8377f6e80cdd8a682e9d3
Jason Wen <haibin.wen3@gmail.com>
2025-04-04 14:39:52 -0400

    Revert "np"
    This reverts commit c407796f2db200728b191a47afdae12ac56e2b2f.

--- selfdrive/ui/tests/test_ui/run.py
+++ selfdrive/ui/tests/test_ui/run.py
@@ -10,7 +10,6 @@ import pyautogui
 import pickle
 import time
 from collections import namedtuple
-import numpy as np
 
 from cereal import car, log, custom
 from msgq.visionipc import VisionIpcServer, VisionStreamType
