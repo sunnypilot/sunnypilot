ed4905ea0f1ac4c8f019bf261411ab6eaea3ec2c
discountchubbs <alexgrant990@gmail.com>
2025-04-15 05:44:45 -0700

    Error log clear on manager start

--- selfdrive/ui/qt/offroad/developer_panel.cc
+++ selfdrive/ui/qt/offroad/developer_panel.cc
@@ -63,7 +63,9 @@ DeveloperPanel::DeveloperPanel(SettingsWindow *parent) : ListWidget(parent) {
   // error log button
   errorLogBtn = new ButtonControl(tr("Error Log"), tr("VIEW"), tr("View the error log for sunnypilot crashes."));
   connect(errorLogBtn, &ButtonControl::clicked, [=]() {
-    std::string txt = util::read_file("/data/community/crashes/error.log");
+    std::string content = util::read_file("/data/community/crashes/error.log");
+    params.put("ErrorLog", content);
+    std::string txt = params.get("ErrorLog");
     ConfirmationDialog::rich(QString::fromStdString(txt), this);
   });
   addItem(errorLogBtn);
