30d3d2d2eb01b0dbb8631f85c68e60b07bcd38b3
Jason Wen <haibin.wen3@gmail.com>
2025-04-19 14:50:32 -0400

    test

--- sunnypilot/selfdrive/controls/lib/nnlc/tests/test_nnlc.py
+++ sunnypilot/selfdrive/controls/lib/nnlc/tests/test_nnlc.py
@@ -1,6 +1,7 @@
+import numpy as np
 from parameterized import parameterized
 
-from cereal import car, log
+from cereal import car, log, messaging
 from opendbc.car.car_helpers import interfaces
 from opendbc.car.honda.values import CAR as HONDA
 from opendbc.car.hyundai.values import CAR as HYUNDAI
@@ -12,6 +13,25 @@ from openpilot.selfdrive.controls.lib.latcontrol_torque import LatControlTorque
 from openpilot.selfdrive.locationd.helpers import Pose
 from openpilot.common.mock.generators import generate_livePose
 from openpilot.sunnypilot.selfdrive.car import interfaces as sunnypilot_interfaces
+from openpilot.system.manager.process_config import managed_processes
+from openpilot.selfdrive.modeld.constants import ModelConstants
+
+
+def generate_modelV2():
+  model = messaging.new_message('modelV2')
+  position = log.XYZTData.new_message()
+  speed = 30
+  position.x = [float(x) for x in (speed + 0.5) * np.array(ModelConstants.T_IDXS)]
+  model.modelV2.position = position
+  velocity = log.XYZTData.new_message()
+  velocity.x = [float(x) for x in (speed + 0.5) * np.ones_like(ModelConstants.T_IDXS)]
+  velocity.x[0] = float(speed)  # always start at current speed
+  model.modelV2.velocity = velocity
+  acceleration = log.XYZTData.new_message()
+  acceleration.x = [float(x) for x in np.zeros_like(ModelConstants.T_IDXS)]
+  model.modelV2.acceleration = acceleration
+
+  return model
 
 
 class TestNeuralNetworkLateralControl:
@@ -20,6 +40,9 @@ class TestNeuralNetworkLateralControl:
   def test_saturation(self, car_name):
     params = Params()
     params.put_bool("NeuralNetworkLateralControl", True)
+
+    sm = messaging.SubMaster(['modelV2'])
+    managed_processes['modeld'].start()
 
     CarInterface = interfaces[car_name]
     CP = CarInterface.get_non_essential_params(car_name)
@@ -42,15 +65,20 @@ class TestNeuralNetworkLateralControl:
     lp = generate_livePose()
     pose = Pose.from_live_pose(lp.livePose)
 
+    mdl = sm['modelV2']
+
     # Saturate for curvature limited and controller limited
     for _ in range(1000):
+      controller.extension.update_model_v2(mdl)
       _, _, lac_log = controller.update(True, CS, VM, params, False, 0, pose, True)
     assert lac_log.saturated
 
     for _ in range(1000):
+      controller.extension.update_model_v2(mdl)
       _, _, lac_log = controller.update(True, CS, VM, params, False, 0, pose, False)
     assert not lac_log.saturated
 
     for _ in range(1000):
+      controller.extension.update_model_v2(mdl)
       _, _, lac_log = controller.update(True, CS, VM, params, False, 1, pose, False)
     assert lac_log.saturated
