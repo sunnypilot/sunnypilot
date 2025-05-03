4d1b4fb363c63bd90839b85ef1e543efba9ff32a
Jason Wen <haibin.wen3@gmail.com>
2025-05-03 00:00:40 -0400

    rename to Cruise

--- selfdrive/ui/sunnypilot/qt/offroad/settings/settings.cc
+++ selfdrive/ui/sunnypilot/qt/offroad/settings/settings.cc
@@ -80,7 +80,7 @@ SettingsWindowSP::SettingsWindowSP(QWidget *parent) : SettingsWindow(parent) {
     PanelInfo("   " + tr("Toggles"), toggles, "../../sunnypilot/selfdrive/assets/offroad/icon_toggle.png"),
     PanelInfo("   " + tr("Software"), new SoftwarePanelSP(this), "../../sunnypilot/selfdrive/assets/offroad/icon_software.png"),
     PanelInfo("   " + tr("Steering"), new LateralPanel(this), "../../sunnypilot/selfdrive/assets/offroad/icon_lateral.png"),
-    PanelInfo("   " + tr("Driving"), new LongitudinalPanel(this), "../assets/offroad/icon_speed_limit.png"),
+    PanelInfo("   " + tr("Cruise"), new LongitudinalPanel(this), "../assets/offroad/icon_speed_limit.png"),
     PanelInfo("   " + tr("Trips"), new TripsPanel(this), "../../sunnypilot/selfdrive/assets/offroad/icon_trips.png"),
     PanelInfo("   " + tr("Vehicle"), new VehiclePanel(this), "../../sunnypilot/selfdrive/assets/offroad/icon_vehicle.png"),
     PanelInfo("   " + tr("Firehose"), new FirehosePanel(this), "../../sunnypilot/selfdrive/assets/offroad/icon_firehose.svg"),
