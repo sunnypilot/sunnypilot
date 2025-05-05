d173bd3efa5e976cc82332d87fe271c1d42ca900
nayan8teen <nayan8teen@gmail.com>
2025-05-05 09:05:31 -0400

    use model helper

--- sunnypilot/selfdrive/controls/lib/longitudinal_planner.py
+++ sunnypilot/selfdrive/controls/lib/longitudinal_planner.py
@@ -8,6 +8,7 @@ See the LICENSE.md file in the root directory for more details.
 from cereal import messaging, custom
 from opendbc.car import structs
 from openpilot.selfdrive.controls.lib.drive_helpers import get_accel_from_plan
+from openpilot.sunnypilot.models.helpers import get_active_model_runner
 from openpilot.sunnypilot.selfdrive.controls.lib.dec.dec import DynamicExperimentalController
 
 DecState = custom.LongitudinalPlanSP.DynamicExperimentalControl.DynamicExperimentalControlState
@@ -42,12 +43,9 @@ class LongitudinalPlannerSP:
 
     pm.send('longitudinalPlanSP', plan_sp_send)
 
-  def get_supercombo_accel_from_plan(self, sm: messaging.SubMaster, output_a_target, output_should_stop, speeds, accels, t_idxs, action_t, vEgoStopping):
+  def get_supercombo_accel_from_plan(self, output_a_target, output_should_stop, speeds, accels, t_idxs, action_t, vEgoStopping):
 
-    try:
-      is_supercombo = sm['modelManagerSP'].activeBundle.runner == custom.ModelManagerSP.Runner.snpe
-    except Exception:
-      is_supercombo = False
+    is_supercombo = get_active_model_runner() == custom.ModelManagerSP.Runner.snpe
 
     if (is_supercombo): # get accel for legacy models
       return get_accel_from_plan(speeds, accels, t_idxs, action_t, vEgoStopping)
