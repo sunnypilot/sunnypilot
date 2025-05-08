cc0c01be336d3e0b7b82913c62ab4d15c83e05f6
Jason Wen <haibin.wen3@gmail.com>
2025-05-07 23:26:53 -0400

    no longer

--- system/manager/manager.py
+++ system/manager/manager.py
@@ -46,7 +46,6 @@ def manager_init() -> None:
     ("AutoLaneChangeBsmDelay", "0"),
     ("DynamicExperimentalControl", "0"),
     ("HyundaiLongitudinalTuning", "0"),
-    ("HyundaiSmootherBraking", "0"),
     ("Mads", "1"),
     ("MadsMainCruiseAllowed", "1"),
     ("MadsPauseLateralOnBrake", "0"),
