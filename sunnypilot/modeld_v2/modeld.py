97ca7cb54b33a665f4dd983b8ef4bc01f8eb6173
discountchubbs <alexgrant990@gmail.com>
2025-05-09 10:44:04 -0700

    Refactor to allow TR (soon TM)

--- sunnypilot/modeld_v2/modeld.py
+++ sunnypilot/modeld_v2/modeld.py
@@ -12,13 +12,14 @@ from opendbc.car.car_helpers import get_demo_car_params
 from openpilot.common.swaglog import cloudlog
 from openpilot.common.params import Params
 from openpilot.common.filter_simple import FirstOrderFilter
-from openpilot.common.realtime import config_realtime_process
+from openpilot.common.realtime import config_realtime_process, DT_MDL
 from openpilot.common.transformations.camera import DEVICE_CAMERAS
 from openpilot.common.transformations.model import get_warp_matrix
 from openpilot.system import sentry
 from openpilot.selfdrive.controls.lib.desire_helper import DesireHelper
+from openpilot.selfdrive.controls.lib.drive_helpers import get_accel_from_plan, smooth_value, get_curvature_from_plan
 from openpilot.sunnypilot.modeld_v2.fill_model_msg import fill_model_msg, fill_pose_msg, PublishState
-from openpilot.sunnypilot.modeld_v2.constants import ModelConstants
+from openpilot.sunnypilot.modeld_v2.constants import ModelConstants, Plan
 from openpilot.sunnypilot.modeld_v2.models.commonmodel_pyx import DrivingModelFrame, CLContext
 
 from openpilot.sunnypilot.modeld_v2.meta_helper import load_meta_constants
@@ -27,7 +28,31 @@ from openpilot.sunnypilot.models.runners.helpers import get_model_runner
 from openpilot.sunnypilot.models.SplitModelConstants import ModelConstants as SplitModelConstants
 
 PROCESS_NAME = "selfdrive.modeld.modeld"
-LAT_SMOOTH_SECONDS = 0.0
+MODEL_RUNNER = get_model_runner()
+LAT_SMOOTH_SECONDS = 0.3 if MODEL_RUNNER.is_20hz_3d else 0.0
+LONG_SMOOTH_SECONDS = 0.3 if MODEL_RUNNER.is_20hz_3d else 0.0
+MIN_LAT_CONTROL_SPEED = 0.3
+
+
+def get_action_from_model(model_output: dict[str, np.ndarray], prev_action: log.ModelDataV2.Action,
+                          lat_action_t: float, long_action_t: float, v_ego: float) -> log.ModelDataV2.Action:
+  plan = model_output['plan'][0]
+  desired_accel, should_stop = get_accel_from_plan(plan[:,Plan.VELOCITY][:,0],
+                                                   plan[:,Plan.ACCELERATION][:,0],
+                                                   ModelConstants.T_IDXS,
+                                                   action_t=long_action_t)
+  desired_accel = smooth_value(desired_accel, prev_action.desiredAcceleration, LONG_SMOOTH_SECONDS)
+  desired_curvature = get_curvature_from_plan(plan[:, Plan.T_FROM_CURRENT_EULER][:, 2],
+                                              plan[:, Plan.ORIENTATION_RATE][:, 2],
+                                              ModelConstants.T_IDXS, v_ego, lat_action_t)
+  if v_ego > MIN_LAT_CONTROL_SPEED:
+    desired_curvature = smooth_value(desired_curvature, prev_action.desiredCurvature, LAT_SMOOTH_SECONDS)
+  else:
+    desired_curvature = prev_action.desiredCurvature
+
+  return log.ModelDataV2.Action(desiredCurvature=float(desired_curvature),
+                                desiredAcceleration=float(desired_accel),
+                                shouldStop=bool(should_stop))
 
 
 class FrameMeta:
@@ -90,11 +115,11 @@ class ModelState:
       self.full_desire[:-1] = self.full_desire[1:]
       self.full_desire[-1] = new_desire
       self.numpy_inputs['desire'][:] = self.full_desire.reshape(self.desire_reshape_dims).max(axis=2)
-    elif self.model_runner.is_20hz and self.model_runner.is_20hz_3d: # is_20hz and is_20hz_3d
+    elif self.model_runner.is_20hz and self.model_runner.is_20hz_3d:
       self.full_desire[0,:-1] = self.full_desire[0,1:]
       self.full_desire[0,-1] = new_desire
       self.numpy_inputs['desire'][:] = self.full_desire.reshape((1,SplitModelConstants.INPUT_HISTORY_BUFFER_LEN,SplitModelConstants.TEMPORAL_SKIP,-1)).max(axis=2)
-    elif not self.model_runner.is_20hz: # not 20hz
+    elif not self.model_runner.is_20hz:
       length = inputs['desire'].shape[0]
       self.numpy_inputs['desire'][0, :-1] = self.numpy_inputs['desire'][0, 1:]
       self.numpy_inputs['desire'][0, -1, :length] = new_desire[:length]
@@ -210,6 +235,10 @@ def main(demo=False):
   # Enable lagd support for modeld_v2
   steer_delay = sm["liveDelay"].lateralDelay + LAT_SMOOTH_SECONDS
 
+  # TODO Move smooth seconds to action function
+  long_delay = CP.longitudinalActuatorDelay + LONG_SMOOTH_SECONDS
+  prev_action = log.ModelDataV2.Action()
+
   DH = DesireHelper()
 
   while True:
@@ -294,7 +323,10 @@ def main(demo=False):
       modelv2_send = messaging.new_message('modelV2')
       drivingdata_send = messaging.new_message('drivingModelData')
       posenet_send = messaging.new_message('cameraOdometry')
-      fill_model_msg(drivingdata_send, modelv2_send, model_output, v_ego, steer_delay,
+
+      action = get_action_from_model(model_output, prev_action, steer_delay + DT_MDL, long_delay + DT_MDL, v_ego)
+      prev_action = action
+      fill_model_msg(drivingdata_send, modelv2_send, model_output, action,
                      publish_state, meta_main.frame_id, meta_extra.frame_id, frame_id,
                      frame_drop_ratio, meta_main.timestamp_eof, model_execution_time, live_calib_seen, load_meta_constants())
 
