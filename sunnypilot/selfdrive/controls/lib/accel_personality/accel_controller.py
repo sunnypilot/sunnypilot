e9b08aea07f81f26f2426478fca42301db7044e6
discountchubbs <alexgrant990@gmail.com>
2025-05-12 21:38:16 -0700

    interpolate

--- sunnypilot/selfdrive/controls/lib/accel_personality/accel_controller.py
+++ sunnypilot/selfdrive/controls/lib/accel_personality/accel_controller.py
@@ -4,8 +4,8 @@ Copyright (c) 2021-, rav4kumar, Haibin Wen, sunnypilot, and a number of other co
 This file is part of sunnypilot and is licensed under the MIT License.
 See the LICENSE.md file in the root directory for more details.
 """
+import numpy as np
 from cereal import custom
-from numpy import interp
 from openpilot.common.realtime import DT_MDL
 from openpilot.common.params import Params
 
@@ -18,6 +18,35 @@ from openpilot.sunnypilot.selfdrive.controls.lib.accel_personality.accel_profile
 
 AccelPersonality = custom.LongitudinalPlanSP.AccelerationPersonality
 
+def compute_symmetric_slopes(x, y):
+  n = len(x)
+  m = np.zeros(n)
+  for i in range(n):
+    if i == 0:
+      m[i] = (y[i+1] - y[i]) / (x[i+1] - x[i])
+    elif i == n-1:
+      m[i] = (y[i] - y[i-1]) / (x[i] - x[i-1])
+    else:
+      m[i] = ((y[i+1] - y[i]) / (x[i+1] - x[i]) + (y[i] - y[i-1]) / (x[i] - x[i-1])) / 2
+  return m
+
+def hermite_interpolate(x, xp, yp, slopes):
+  x = np.clip(x, xp[0], xp[-1])
+  idx = np.searchsorted(xp, x) - 1
+  idx = np.clip(idx, 0, len(slopes) - 2)
+
+  x0, x1 = xp[idx], xp[idx+1]
+  y0, y1 = yp[idx], yp[idx+1]
+  m0, m1 = slopes[idx], slopes[idx+1]
+
+  t = (x - x0) / (x1 - x0)
+  h00 = 2*t**3 - 3*t**2 + 1
+  h10 = t**3 - 2*t**2 + t
+  h01 = -2*t**3 + 3*t**2
+  h11 = t**3 - t**2
+
+  return (h00 * y0) + (h10 * (x1 - x0) * m0) + (h01 * y1) + (h11 * (x1 - x0) * m1)
+
 def clamp(val: float, lower: float, upper: float) -> float:
   return max(lower, min(val, upper))
 
@@ -26,6 +55,20 @@ class AccelController:
     self.params = Params()
     self.personality = AccelPersonality.stock
     self.frame = 0
+
+    # Pre-compute slopes for smoother interpolation
+    self.max_accel_slopes = {
+      'eco': compute_symmetric_slopes(MAX_ACCEL_BREAKPOINTS, MAX_ACCEL_ECO),
+      'normal': compute_symmetric_slopes(MAX_ACCEL_BREAKPOINTS, MAX_ACCEL_NORMAL),
+      'sport': compute_symmetric_slopes(MAX_ACCEL_BREAKPOINTS, MAX_ACCEL_SPORT)
+    }
+
+    self.min_accel_slopes = {
+      'eco': compute_symmetric_slopes(MIN_ACCEL_BREAKPOINTS, MIN_ACCEL_ECO),
+      'normal': compute_symmetric_slopes(MIN_ACCEL_BREAKPOINTS, MIN_ACCEL_NORMAL),
+      'sport': compute_symmetric_slopes(MIN_ACCEL_BREAKPOINTS, MIN_ACCEL_SPORT),
+      'stock': compute_symmetric_slopes(MIN_ACCEL_BREAKPOINTS, MIN_ACCEL_STOCK)
+    }
 
   def _update_personality_from_param(self):
     if self.frame % int(1. / DT_MDL) == 0:
@@ -43,12 +86,15 @@ class AccelController:
 
     if self.personality == AccelPersonality.eco:
       accel_profile = MAX_ACCEL_ECO
+      slopes = self.max_accel_slopes['eco']
     elif self.personality == AccelPersonality.sport:
       accel_profile = MAX_ACCEL_SPORT
+      slopes = self.max_accel_slopes['sport']
     else:
       accel_profile = MAX_ACCEL_NORMAL
+      slopes = self.max_accel_slopes['normal']
 
-    return float(interp(v_ego, MAX_ACCEL_BREAKPOINTS, accel_profile))
+    return float(hermite_interpolate(v_ego, MAX_ACCEL_BREAKPOINTS, accel_profile, slopes))
 
   def _get_min_accel_for_speed(self, v_ego: float) -> float:
     self._update_personality_from_param()
@@ -58,14 +104,18 @@ class AccelController:
 
     if self.personality == AccelPersonality.eco:
       accel_profile = MIN_ACCEL_ECO
+      slopes = self.min_accel_slopes['eco']
     elif self.personality == AccelPersonality.sport:
       accel_profile = MIN_ACCEL_SPORT
+      slopes = self.min_accel_slopes['sport']
     elif self.personality == AccelPersonality.normal:
       accel_profile = MIN_ACCEL_NORMAL
+      slopes = self.min_accel_slopes['normal']
     else:
       accel_profile = MIN_ACCEL_STOCK
+      slopes = self.min_accel_slopes['stock']
 
-    return float(interp(v_ego, MIN_ACCEL_BREAKPOINTS, accel_profile))
+    return float(hermite_interpolate(v_ego, MIN_ACCEL_BREAKPOINTS, accel_profile, slopes))
 
   def get_accel_limits(self, v_ego: float, accel_limits: list[float]) -> tuple[float, float]:
     self._update_personality_from_param()
