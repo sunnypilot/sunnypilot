fa96646741b5a05dbb8ea738b0b7a9c5c35adfb6
discountchubbs <alexgrant990@gmail.com>
2025-05-13 16:22:01 -0700

    vRel for relative velocity

--- sunnypilot/selfdrive/controls/lib/dec/dec.py
+++ sunnypilot/selfdrive/controls/lib/dec/dec.py
@@ -329,7 +329,7 @@ class DynamicExperimentalController:
 
     # If there is a filtered lead, the vehicle is not in standstill, and the lead vehicle's yRel meets the condition,
     if self._has_lead_filtered and not self._has_standstill:
-      if self.sm is not None and (self.sm['radarState'].leadOne.dRel < 15.0):
+      if self.sm is not None and (self.sm['radarState'].leadOne.vRel < 0):
         self._set_mode('blended')
       else:
         self._set_mode('acc')
@@ -355,7 +355,7 @@ class DynamicExperimentalController:
 
     # car driving at speed lower than set speed: acc
     if self._has_slowness:
-      if self.sm is not None and (self.sm['radarState'].leadOne.dRel < 30.0):
+      if self.sm is not None and (self.sm['radarState'].leadOne.dRel < 15.0):
         self._set_mode('blended')
       else:
         self._set_mode('acc')
