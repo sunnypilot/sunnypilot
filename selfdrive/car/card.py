4c0abbc785793bfa7556f9d9ce4c1f3805fee330
Jason Wen <haibin.wen3@gmail.com>
2025-05-28 23:40:56 -0400

    move init to opendbc

--- selfdrive/car/card.py
+++ selfdrive/car/card.py
@@ -87,6 +87,8 @@ class Car:
 
     self.can_callbacks = can_comm_callbacks(self.can_sock, self.pm.sock['sendcan'])
 
+    init_params_list_sp = sunnypilot_interfaces.get_init_params(self.params)
+
     if CI is None:
       # wait for one pandaState and one CAN packet
       print("Waiting for CAN messages...")
@@ -106,7 +108,8 @@ class Car:
 
       fixed_fingerprint = json.loads(self.params.get("CarPlatformBundle", encoding='utf-8') or "{}").get("platform", None)
 
-      self.CI = get_car(*self.can_callbacks, obd_callback(self.params), alpha_long_allowed, num_pandas, cached_params, fixed_fingerprint)
+      self.CI = get_car(*self.can_callbacks, obd_callback(self.params), alpha_long_allowed, num_pandas, cached_params,
+                        fixed_fingerprint, init_params_list_sp)
       sunnypilot_interfaces.setup_interfaces(self.CI, self.params)
       self.RI = interfaces[self.CI.CP.carFingerprint].RadarInterface(self.CI.CP, self.CI.CP_SP)
       self.CP = self.CI.CP
