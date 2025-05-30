465e923bce05a27be82b58c6688500363e1f2b83
DevTekVE <devtekve@gmail.com>
2025-05-30 11:31:08 +0200

    Let's backup the BlindSpot setting

--- common/params_keys.h
+++ common/params_keys.h
@@ -171,5 +171,5 @@ inline static std::unordered_map<std::string, uint32_t> keys = {
     {"HyundaiRadarTracksToggle", PERSISTENT},
 
     {"DynamicExperimentalControl", PERSISTENT},
-    {"BlindSpot", PERSISTENT},
+    {"BlindSpot", PERSISTENT | BACKUP},
 };
