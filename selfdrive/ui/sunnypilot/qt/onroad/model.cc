84c5a6069aa1e067db27b893a0982357d1ffd387
DevTekVE <devtekve@gmail.com>
2025-05-30 11:09:25 +0200

    Cleanup format
    Clean
    
    Cleanup and fixes

--- selfdrive/ui/sunnypilot/qt/onroad/model.cc
+++ selfdrive/ui/sunnypilot/qt/onroad/model.cc
@@ -10,14 +10,16 @@
 
 void ModelRendererSP::update_model(const cereal::ModelDataV2::Reader &model, const cereal::RadarState::LeadData::Reader &lead) {
   ModelRenderer::update_model(model, lead);
+  const auto &model_position = model.getPosition();
+  const auto &lane_lines = model.getLaneLines();
+  float max_distance = std::clamp(*(model_position.getX().end() - 1), MIN_DRAW_DISTANCE, MAX_DRAW_DISTANCE);
   int max_idx = get_path_length_idx(lane_lines[0], max_distance);
   // update blindspot vertices
-  int max_distance_barrier =  100;
+  float max_distance_barrier = 100;
   int max_idx_barrier = std::min(max_idx, get_path_length_idx(lane_lines[0], max_distance_barrier));
   mapLineToPolygon(model.getLaneLines()[1], 0.2, -0.05, &left_blindspot_vertices, max_idx_barrier);
   mapLineToPolygon(model.getLaneLines()[2], 0.2, -0.05, &right_blindspot_vertices, max_idx_barrier);
 }
-
 
 void ModelRendererSP::drawPath(QPainter &painter, const cereal::ModelDataV2::Reader &model, const QRect &surface_rect) {
   auto *s = uiState();
@@ -31,17 +33,17 @@ void ModelRendererSP::drawPath(QPainter &painter, const cereal::ModelDataV2::Rea
     //painter.setBrush(QColor::fromRgbF(1.0, 0.0, 0.0, 0.4));  // Red with alpha for blind spot
 
     if (left_blindspot && !left_blindspot_vertices.isEmpty()) {
-      QLinearGradient gradient(0, 0, surface_rect.width(), 0);  // Horizontal gradient from left to right
-      gradient.setColorAt(0.0, QColor(255, 165, 0, 102));   // Orange with alpha
-      gradient.setColorAt(1.0, QColor(255, 255, 0, 102));   // Yellow with alpha
+      QLinearGradient gradient(0, 0, surface_rect.width(), 0); // Horizontal gradient from left to right
+      gradient.setColorAt(0.0, QColor(255, 165, 0, 102)); // Orange with alpha
+      gradient.setColorAt(1.0, QColor(255, 255, 0, 102)); // Yellow with alpha
       painter.setBrush(gradient);
       painter.drawPolygon(left_blindspot_vertices);
     }
 
     if (right_blindspot && !right_blindspot_vertices.isEmpty()) {
-      QLinearGradient gradient(surface_rect.width(), 0, 0, 0);  // Horizontal gradient from right to left
-      gradient.setColorAt(0.0, QColor(255, 165, 0, 102));   // Orange with alpha
-      gradient.setColorAt(1.0, QColor(255, 255, 0, 102));   // Yellow with alpha
+      QLinearGradient gradient(surface_rect.width(), 0, 0, 0); // Horizontal gradient from right to left
+      gradient.setColorAt(0.0, QColor(255, 165, 0, 102)); // Orange with alpha
+      gradient.setColorAt(1.0, QColor(255, 255, 0, 102)); // Yellow with alpha
       painter.setBrush(gradient);
       painter.drawPolygon(right_blindspot_vertices);
     }
