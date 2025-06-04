d02ef8b586fdd3746c21dd59e326fe35110b568b
Jason Wen <haibin.wen3@gmail.com>
2025-06-04 13:34:13 -0400

    shorter

--- selfdrive/ui/sunnypilot/qt/offroad/settings/lateral/blinker_pause_lateral_settings.cc
+++ selfdrive/ui/sunnypilot/qt/offroad/settings/lateral/blinker_pause_lateral_settings.cc
@@ -8,21 +8,12 @@
 
 #include "selfdrive/ui/sunnypilot/qt/offroad/settings/lateral/blinker_pause_lateral_settings.h"
 
-BlinkerPauseLateralSettings::BlinkerPauseLateralSettings(
-  const QString &param,
-  const QString &title,
-  const QString &description,
-  const QString &icon,
-  QWidget *parent) : ExpandableToggleRow(param, title, description, icon, parent) {
+BlinkerPauseLateralSettings::BlinkerPauseLateralSettings(const QString &param, const QString &title, const QString &description, const QString &icon, QWidget *parent)
+    : ExpandableToggleRow(param, title, description, icon, parent) {
 
-  pauseLateralSpeed = new OptionControlSP(
-  "BlinkerMinLateralControlSpeed",
-  "",
-  "",
-  "",
-  {0, 255}, 5);
-  addItem(pauseLateralSpeed);
+  pauseLateralSpeed = new OptionControlSP("BlinkerMinLateralControlSpeed", "", "", "", {0, 255}, 5);
   connect(pauseLateralSpeed, &OptionControlSP::updateLabels, this, &BlinkerPauseLateralSettings::refresh);
+  addItem(pauseLateralSpeed);
 
   refresh();
 }
