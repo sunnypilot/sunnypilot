d306aea6e81fe0e7f4ac47bc99f78083903c0674
rav4kumar <meetkumardesai@gmail.com>
2025-06-09 21:15:58 -0700

    origin/v-tsc

--- system/manager/manager.py
+++ system/manager/manager.py
@@ -73,6 +73,7 @@ def manager_init() -> None:
     ("ToyotaAutoHold", "0"),
     ("ToyotaEnhancedBsm", "0"),
     ("ToyotaTSS2Long", "0"),
+    ("VisionTurnSpeedControl", "1"),
 
     ("SpeedLimitControl", "0"),
     ("SpeedLimitControlPolicy", "3"),
