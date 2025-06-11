74c6c8aeac9e61ea39a01faa5ac9081c2f203013
discountchubbs <alexgrant990@gmail.com>
2025-06-11 06:06:55 -0700

    Set this in init

--- sunnypilot/models/modeld_lagd.py
+++ sunnypilot/models/modeld_lagd.py
@@ -12,6 +12,7 @@ class ModeldLagd:
   def __init__(self):
     self.params = Params()
     self.lag = 0.0
+    self.software_delay = 0.2
 
   def lagd_main(self, CP, sm, model):
     if self.params.get_bool("LagdToggle"):
@@ -20,15 +21,17 @@ class ModeldLagd:
       result = lateral_delay + lat_smooth
       cloudlog.debug(f"LAGD USING LIVE DELAY: {lateral_delay:.3f} + {lat_smooth:.3f} = {result:.3f}")
       return result
+
     steer_actuator_delay = CP.steerActuatorDelay
     lat_smooth = model.LAT_SMOOTH_SECONDS
-    result = (steer_actuator_delay + 0.2) + lat_smooth
-    cloudlog.debug(f"LAGD USING STEER ACTUATOR: {steer_actuator_delay:.3f} + 0.2 + {lat_smooth:.3f} = {result:.3f}")
+    delay = self.software_delay
+    result = (steer_actuator_delay + delay) + lat_smooth
+    cloudlog.debug(f"LAGD USING STEER ACTUATOR: {steer_actuator_delay:.3f} + {delay:.3f} + {lat_smooth:.3f} = {result:.3f}")
     return result
 
   def lagd_torqued_main(self, CP, msg):
     if self.params.get_bool("LagdToggle"):
       self.lag = msg.lateralDelay
     else:
-      self.lag = CP.steerActuatorDelay + 0.2
+      self.lag = CP.steerActuatorDelay + self.software_delay
     return self.lag
