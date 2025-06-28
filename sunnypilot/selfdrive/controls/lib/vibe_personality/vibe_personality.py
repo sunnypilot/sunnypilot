b36dde70944250b2265756a2331b04f5e333525f
rav4kumar <meetkumardesai@gmail.com>
2025-06-28 09:44:15 -0700

    tune

--- sunnypilot/selfdrive/controls/lib/vibe_personality/vibe_personality.py
+++ sunnypilot/selfdrive/controls/lib/vibe_personality/vibe_personality.py
@@ -16,7 +16,6 @@ LongPersonality = log.LongitudinalPersonality
 MAX_ACCEL_PROFILES = {
   LongPersonality.relaxed:    [2.00, 2.00, 1.72, 0.83, .572, .455, .365, .32, .10],  # eco-like
   LongPersonality.standard:   [2.00, 2.00, 1.84, 1.00, .64,  .55, .42, .37, .15],   # normal
-  #LongPersonality.aggressive: [2.00, 2.00, 1.95, 1.25, .78,  .67, .50, .43, .20],   # sport-like
   LongPersonality.aggressive: [2.00, 2.00, 1.92, 1.25, .78,  .67, .50, .43, .20],   # sport-like
 }
 MAX_ACCEL_BREAKPOINTS = [0., 7., 9., 11., 16., 20., 25., 30., 55.]
@@ -24,7 +23,7 @@ MAX_ACCEL_BREAKPOINTS = [0., 7., 9., 11., 16., 20., 25., 30., 55.]
 MIN_ACCEL_PROFILES = {
   LongPersonality.relaxed:    [-.071143, -.07130, -.07120, -.07155, -1.4, -1.4],  # eco-like
   LongPersonality.standard:   [-.071143, -.07132, -.07122, -.07165, -1.5, -1.5],  # normal
-  LongPersonality.aggressive: [-.030000, -1.7000, -1.4000, -.07175, -1.6, -1.6],  # sport-like
+  LongPersonality.aggressive: [-.010000, -1.6000, -1.4000, -.07175, -1.6, -1.6],  # sport-like
 }
 MIN_ACCEL_BREAKPOINTS =       [0.,       2.0,     8.,     13.,    22.2, 40.]
 
