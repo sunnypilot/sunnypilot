0be833fc8acaeba05971ef99f0bf7575f3438481
nayan <nayan8teen@gmail.com>
2025-07-03 15:20:59 -0700

    ui: slc #975

--- selfdrive/ui/sunnypilot/qt/offroad/settings/longitudinal_panel.cc
+++ selfdrive/ui/sunnypilot/qt/offroad/settings/longitudinal_panel.cc
@@ -8,6 +8,21 @@
 #include "selfdrive/ui/sunnypilot/qt/offroad/settings/longitudinal_panel.h"
 
 LongitudinalPanel::LongitudinalPanel(QWidget *parent) : QWidget(parent) {
+  setStyleSheet(R"(
+    #back_btn {
+      font-size: 50px;
+      margin: 0px;
+      padding: 15px;
+      border-width: 0;
+      border-radius: 30px;
+      color: #dddddd;
+      background-color: #393939;
+    }
+    #back_btn:pressed {
+      background-color:  #4a4a4a;
+    }
+  )");
+
   main_layout = new QStackedLayout(this);
   ListWidget *list = new ListWidget(this, false);
 
@@ -52,7 +67,28 @@ LongitudinalPanel::LongitudinalPanel(QWidget *parent) : QWidget(parent) {
     "../assets/offroad/icon_shell.png");
   list->addItem(vibeFollowPersonalityControl);
 
+  slcControl = new SpeedLimitControl(
+    "SpeedLimitControl",
+    tr("Speed Limit Control (SLC)"),
+    tr("When you engage ACC, you will be prompted to set the cruising speed to the speed limit of the road adjusted by the Offset and Source Policy specified, or the current driving speed. "
+      "The maximum cruising speed will always be the MAX set speed."),
+    "",
+    this);
+  list->addItem(slcControl);
+
+  connect(slcControl, &SpeedLimitControl::slcSettingsButtonClicked, [=]() {
+    cruisePanelScroller->setLastScrollPosition();
+    main_layout->setCurrentWidget(slcScreen);
+  });
+
+  slcScreen = new SpeedLimitControlSubpanel(this);
+  connect(slcScreen, &SpeedLimitControlSubpanel::backPress, [=]() {
+    cruisePanelScroller->restoreScrollPosition();
+    main_layout->setCurrentWidget(cruisePanelScreen);
+  });
+
   main_layout->addWidget(cruisePanelScreen);
+  main_layout->addWidget(slcScreen);
   main_layout->setCurrentWidget(cruisePanelScreen);
   refresh(offroad);
 }
