f84a4e2a79f958cfc29224308317cd9bae1322f9
rav4kumar <meetkumardesai@gmail.com>
2025-07-04 10:26:28 -0700

    slc onroad ui

--- selfdrive/ui/qt/onroad/hud.h
+++ selfdrive/ui/qt/onroad/hud.h
@@ -21,6 +21,35 @@ protected:
   void drawSetSpeed(QPainter &p, const QRect &surface_rect);
   void drawCurrentSpeed(QPainter &p, const QRect &surface_rect);
   void drawText(QPainter &p, int x, int y, const QString &text, int alpha = 255);
+  void drawSpeedLimitSigns(QPainter &p, const QRect &rect);
+  void drawVisionTurnControl(QPainter &p, const QRect &surface_rect);
+  QColor interpColor(float x, const std::vector<float> &x_vals, const std::vector<QColor> &colors);
+
+  // Additional drawing methods from implementation
+  void drawUpcomingSpeedLimit(QPainter &p, const QRect &surface_rect);
+  void drawSLCStateIndicator(QPainter &p, const QRect &surface_rect);
+  void drawRoadName(QPainter &p, const QRect &surface_rect);
+
+  // Display flags
+  bool show_slc = false;
+  bool over_speed_limit = false;
+
+  // Speed Limit Control (SLC)
+  float slc_speed_limit = 0.0;
+  float slc_speed_offset = 0.0;
+  cereal::LongitudinalPlanSP::SpeedLimitControlState slc_state = cereal::LongitudinalPlanSP::SpeedLimitControlState::INACTIVE;
+  float dist_to_speed_limit = 0.0;
+
+  // Speed violation levels
+  int speed_violation_level = 0;
+
+  // Upcoming speed limit data
+  bool speed_limit_ahead_valid = false;
+  float speed_limit_ahead = 0.0;
+  float speed_limit_ahead_distance = 0.0;
+
+  // Road information
+  QString road_name;
 
   float speed = 0;
   float set_speed = 0;
