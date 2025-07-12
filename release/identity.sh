1ac21473d9df141676ecfb96852472aaea619799
Jason Wen <haibin.wen3@gmail.com>
2025-07-12 12:13:25 -0400

    id

--- release/identity.sh
+++ release/identity.sh
@@ -1,4 +1,4 @@
-export GIT_COMMITTER_NAME="Vehicle Researcher"
-export GIT_COMMITTER_EMAIL="user@comma.ai"
-export GIT_AUTHOR_NAME="Vehicle Researcher"
-export GIT_AUTHOR_EMAIL="user@comma.ai"
+export GIT_COMMITTER_NAME="Jason Wen"
+export GIT_COMMITTER_EMAIL="jason.wen@sunnypilot.ai"
+export GIT_AUTHOR_NAME="Jason Wen"
+export GIT_AUTHOR_EMAIL="jason.wen@sunnypilot.ai"
