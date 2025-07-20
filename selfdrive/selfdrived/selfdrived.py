296f507da1f43a312849b6375f1caa01f1452104
mouxangithub <mouxan@163.com>
2025-07-20 21:24:34 +0800

    refactor(selfdrived): 修改启动事件逻辑
    - 移除了根据远程构建元数据和测试频道确定启动事件的逻辑
    - 固定启动事件为 EventName.startup
    - 保留了汽车未识别和被动模式下的启动事件逻辑

--- selfdrive/selfdrived/selfdrived.py
+++ selfdrive/selfdrived/selfdrived.py
@@ -135,7 +135,7 @@ class SelfdriveD(CruiseHelper):
     self.ignored_processes.update({'mapd'})
 
     # Determine startup event
-    self.startup_event = EventName.startup if build_metadata.openpilot.comma_remote and build_metadata.tested_channel else EventName.startupMaster
+    self.startup_event = EventName.startup #if build_metadata.openpilot.comma_remote and build_metadata.tested_channel else EventName.startupMaster
     if not car_recognized:
       self.startup_event = EventName.startupNoCar
     elif car_recognized and self.CP.passive:
