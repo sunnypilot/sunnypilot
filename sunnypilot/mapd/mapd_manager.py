a2c8b5328adc443104fd4a40f74c2ae403830bd9
rav4kumar <meetkumardesai@gmail.com>
2025-07-19 17:44:42 -0700

    bug/fix-mapd-folder

--- sunnypilot/mapd/mapd_manager.py
+++ sunnypilot/mapd/mapd_manager.py
@@ -12,21 +12,19 @@ import os
 import glob
 import shutil
 
-from openpilot.common.basedir import BASEDIR
 from openpilot.common.params import Params
 from openpilot.common.realtime import Ratekeeper, config_realtime_process
 from openpilot.common.swaglog import cloudlog
 from openpilot.selfdrive.selfdrived.alertmanager import set_offroad_alert
 from openpilot.sunnypilot.mapd.live_map_data.osm_map_data import OsmMapData
 from openpilot.system.hardware.hw import Paths
+from openpilot.sunnypilot.mapd import MAPD_PATH
+from openpilot.sunnypilot.mapd.mapd_installer import MapdInstallManager, VERSION
 
 # PFEIFER - MAPD {{
 params = Params()
 mem_params = Params("/dev/shm/params") if platform.system() != "Darwin" else params
 # }} PFEIFER - MAPD
-
-MAPD_BIN_DIR = os.path.join(BASEDIR, 'third_party/mapd_pfeiferj')
-MAPD_PATH = os.path.join(MAPD_BIN_DIR, 'mapd')
 
 
 def get_files_for_cleanup() -> list[str]:
@@ -115,8 +113,8 @@ def update_osm_db() -> None:
   if not mem_params.get("LastGPSPosition"):
     mem_params.put("LastGPSPosition", "{}")
 
-
 def main_thread():
+  MapdInstallManager.update_installed_version(VERSION)
   config_realtime_process([0, 1, 2, 3], 5)
 
   rk = Ratekeeper(1, print_delay_threshold=None)
