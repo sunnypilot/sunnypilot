cf2aa67a04006cc53886f7e2b6cd3c3c32ac72c1
程序员小刘 <48978046+mouxangithub@users.noreply.github.com>
2025-07-24 12:57:53 +0800

    Update model.h

--- selfdrive/ui/sunnypilot/qt/onroad/model.h
+++ selfdrive/ui/sunnypilot/qt/onroad/model.h
@@ -28,14 +28,6 @@ private:
                                int height, int width,
                                const QString &label);
 
-  // Lead status display methods
-  void drawLeadStatus(QPainter &painter, int height, int width);
-  void drawLeadStatusAtPosition(QPainter &painter,
-                               const cereal::RadarState::LeadData::Reader &lead_data,
-                               const QPointF &chevron_pos,
-                               int height, int width,
-                               const QString &label);
-
   QPolygonF left_blindspot_vertices;
   QPolygonF right_blindspot_vertices;
 
