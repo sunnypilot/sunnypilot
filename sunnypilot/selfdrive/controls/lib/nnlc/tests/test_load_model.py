fad00d9f7f9ca4009fafd5d34b1fe4114fff2234
DevTekVE <devtekve@gmail.com>
2025-08-05 20:20:13 +0200

    lint

--- sunnypilot/selfdrive/controls/lib/nnlc/tests/test_load_model.py
+++ sunnypilot/selfdrive/controls/lib/nnlc/tests/test_load_model.py
@@ -1,5 +1,5 @@
 # from parameterized import parameterized
-# 
+#
 # from opendbc.car.car_helpers import interfaces
 # from opendbc.car.honda.values import CAR as HONDA
 # from opendbc.car.hyundai.values import CAR as HYUNDAI
@@ -8,24 +8,24 @@
 # from openpilot.selfdrive.car.helpers import convert_to_capnp
 # from openpilot.selfdrive.controls.lib.latcontrol_torque import LatControlTorque
 # from openpilot.sunnypilot.selfdrive.car import interfaces as sunnypilot_interfaces
-# 
-# 
+#
+#
 # class TestNNTorqueModel:
-# 
+#
 #   @parameterized.expand([HONDA.HONDA_CIVIC, TOYOTA.TOYOTA_RAV4, HYUNDAI.HYUNDAI_SANTA_CRUZ_1ST_GEN])
 #   def test_load_model(self, car_name):
 #     params = Params()
 #     params.put_bool("NeuralNetworkLateralControl", True)
-# 
+#
 #     CarInterface = interfaces[car_name]
 #     CP = CarInterface.get_non_essential_params(car_name)
 #     CP_SP = CarInterface.get_non_essential_params_sp(CP, car_name)
 #     CI = CarInterface(CP, CP_SP)
-# 
+#
 #     sunnypilot_interfaces.setup_interfaces(CI, params)
-# 
+#
 #     CP_SP = convert_to_capnp(CP_SP)
-# 
+#
 #     controller = LatControlTorque(CP.as_reader(), CP_SP.as_reader(), CI)
-# 
+#
 #     assert controller.extension.has_nn_model
--- sunnypilot/selfdrive/controls/lib/nnlc/tests/test_nnlc.py
+++ sunnypilot/selfdrive/controls/lib/nnlc/tests/test_nnlc.py
@@ -1,6 +1,6 @@
 # import numpy as np
 # from parameterized import parameterized
-# 
+#
 # from cereal import car, log, messaging
 # from opendbc.car.car_helpers import interfaces
 # from opendbc.car.honda.values import CAR as HONDA
@@ -14,8 +14,8 @@
 # from openpilot.common.mock.generators import generate_livePose
 # from openpilot.sunnypilot.selfdrive.car import interfaces as sunnypilot_interfaces
 # from openpilot.selfdrive.modeld.constants import ModelConstants
-# 
-# 
+#
+#
 # def generate_modelV2():
 #   model = messaging.new_message('modelV2')
 #   position = log.XYZTData.new_message()
@@ -35,43 +35,43 @@
 #   acceleration.x = [float(x) for x in np.zeros_like(ModelConstants.T_IDXS)]
 #   acceleration.y = [float(y) for y in np.zeros_like(ModelConstants.T_IDXS)]
 #   model.modelV2.acceleration = acceleration
-# 
+#
 #   return model
-# 
-# 
+#
+#
 # class TestNeuralNetworkLateralControl:
-# 
+#
 #   @parameterized.expand([HONDA.HONDA_CIVIC, TOYOTA.TOYOTA_RAV4, HYUNDAI.HYUNDAI_SANTA_CRUZ_1ST_GEN])
 #   def test_saturation(self, car_name):
 #     params = Params()
 #     params.put_bool("NeuralNetworkLateralControl", True)
-# 
+#
 #     CarInterface = interfaces[car_name]
 #     CP = CarInterface.get_non_essential_params(car_name)
 #     CP_SP = CarInterface.get_non_essential_params_sp(CP, car_name)
 #     CI = CarInterface(CP, CP_SP)
-# 
+#
 #     sunnypilot_interfaces.setup_interfaces(CI, params)
-# 
+#
 #     CP_SP = convert_to_capnp(CP_SP)
 #     VM = VehicleModel(CP)
-# 
+#
 #     controller = LatControlTorque(CP.as_reader(), CP_SP.as_reader(), CI)
-# 
+#
 #     CS = car.CarState.new_message()
 #     CS.vEgo = 30
 #     CS.steeringPressed = False
-# 
+#
 #     params = log.LiveParametersData.new_message()
-# 
+#
 #     lp = generate_livePose()
 #     pose = Pose.from_live_pose(lp.livePose)
-# 
+#
 #     mdl = generate_modelV2()
 #     sm = {'modelV2': mdl.modelV2}
 #     model_v2 = sm['modelV2']
 #     controller.extension.model_v2 = model_v2
-# 
+#
 #     # Saturate for curvature limited and controller limited
 #     test_lag = 0.3
 #     for _ in range(1000):
@@ -79,13 +79,13 @@
 #       controller.extension.update_lateral_lag(test_lag)
 #       _, _, lac_log = controller.update(True, CS, VM, params, False, 0, pose, True)
 #     assert lac_log.saturated
-# 
+#
 #     for _ in range(1000):
 #       controller.extension.update_model_v2(model_v2)
 #       controller.extension.update_lateral_lag(test_lag)
 #       _, _, lac_log = controller.update(True, CS, VM, params, False, 0, pose, False)
 #     assert not lac_log.saturated
-# 
+#
 #     for _ in range(1000):
 #       controller.extension.update_model_v2(model_v2)
 #       controller.extension.update_lateral_lag(test_lag)
