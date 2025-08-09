a15517c7ff9d4c80d7a9ac1bd137083bd16e984d
Jason Wen <haibin.wen3@gmail.com>
2025-08-08 23:48:50 -0400

    type hint

--- sunnypilot/livedelay/lagd_toggle.py
+++ sunnypilot/livedelay/lagd_toggle.py
@@ -4,6 +4,9 @@ Copyright (c) 2021-, Haibin Wen, sunnypilot, and a number of other contributors.
 This file is part of sunnypilot and is licensed under the MIT License.
 See the LICENSE.md file in the root directory for more details.
 """
+from cereal import log
+
+from opendbc.car import structs
 from openpilot.common.params import Params
 from openpilot.selfdrive.modeld.modeld import LAT_SMOOTH_SECONDS
 from openpilot.sunnypilot.models.helpers import get_active_bundle
@@ -17,7 +20,7 @@ def get_lat_delay(params, stock_lateral_delay, updated):
 
 
 class LagdToggle:
-  def __init__(self, CP):
+  def __init__(self, CP: structs.CarParams):
     self.CP = CP
     self.params = Params()
     self.lag = 0.0
@@ -33,16 +36,16 @@ class LagdToggle:
     self.lagd_toggle = self.params.get_bool("LagdToggle")
     self.software_delay = self.params.get("LagdToggleDelay")
 
-  def read_params(self):
+  def read_params(self) -> None:
     self.lagd_toggle = self.params.get_bool("LagdToggle")
     self.software_delay = self.params.get("LagdToggleDelay")
 
-  def _maybe_update_desc(self, desc):
+  def _maybe_update_desc(self, desc: str) -> None:
     if desc != self._last_desc:
       self.params.put_nonblocking("LagdToggleDesc", desc)
       self._last_desc = desc
 
-  def update(self, lag_msg):
+  def update(self, lag_msg: log.LiveDelayData) -> None:
     self.read_params()
 
     if not self.lagd_toggle:
