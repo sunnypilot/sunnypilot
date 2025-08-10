990b3d499808d0bb5e787979cda269248d9f4ee4
discountchubbs <alexgrant990@gmail.com>
2025-08-09 19:00:25 -0700

    Handle decoding directly in ui bc why not

--- selfdrive/ui/sunnypilot/qt/offroad/settings/models_panel.cc
+++ selfdrive/ui/sunnypilot/qt/offroad/settings/models_panel.cc
@@ -359,8 +359,39 @@ void ModelsPanel::updateLabels() {
 
   // Update lagdToggle description with current value
   QString desc = tr("Enable this for the car to learn and adapt its steering response time. "
-                   "Disable to use a fixed steering response time. Keeping this on provides the stock openpilot experience. "
-                   "The Current value is updated automatically when the vehicle is Onroad.");
+                   "Disable to use a fixed steering response time. Keeping this on provides the stock openpilot experience.");
+  bool lagdEnabled = params.getBool("LagdToggle");
+  if (lagdEnabled) {
+    std::string liveDelayBytes = params.get("LiveDelay");
+    if (!liveDelayBytes.empty()) {
+      capnp::FlatArrayMessageReader msg(kj::ArrayPtr<const capnp::word>(
+        reinterpret_cast<const capnp::word*>(liveDelayBytes.data()),
+        liveDelayBytes.size() / sizeof(capnp::word)));
+      auto event = msg.getRoot<cereal::Event>();
+      if (event.hasLiveDelay()) {
+        auto liveDelay = event.getLiveDelay();
+        float lateralDelay = liveDelay.getLateralDelay();
+        desc += QString("<br><br><b><span style=\"color:#e0e0e0\">%1</span></b> <span style=\"color:#e0e0e0\">%2 s</span>")
+                .arg(tr("Live Steer Delay:")).arg(QString::number(lateralDelay, 'f', 2));
+      }
+    }
+  } else {
+    std::string carParamsBytes = params.get("CarParamsPersistent");
+    if (!carParamsBytes.empty()) {
+      capnp::FlatArrayMessageReader msg(kj::ArrayPtr<const capnp::word>(
+        reinterpret_cast<const capnp::word*>(carParamsBytes.data()),
+        carParamsBytes.size() / sizeof(capnp::word)));
+      auto carParams = msg.getRoot<cereal::CarParams>();
+      float steerDelay = carParams.getSteerActuatorDelay();
+      float softwareDelay = QString::fromStdString(params.get("LagdToggleDelay")).toFloat();
+      float totalLag = steerDelay + softwareDelay;
+      desc += QString("<br><br><span style=\"color:#e0e0e0\">"
+                      "<b>%1</b> %2 s + <b>%3</b> %4 s = <b>%5</b> %6 s</span>")
+           .arg(tr("Actuator Delay:"), QString::number(steerDelay, 'f', 2),
+                tr("Software Delay:"), QString::number(softwareDelay, 'f', 2),
+                tr("Total Delay:"), QString::number(totalLag, 'f', 2));
+    }
+  }
   lagd_toggle_control->setDescription(desc);
 
   delay_control->setVisible(!params.getBool("LagdToggle"));
