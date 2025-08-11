9a04c439e602aefa3fb5140250647e3222ba24f6
Jason Wen <haibin.wen3@gmail.com>
2025-08-11 09:33:14 -0400

    Subaru: Stop and Go auto-resume support

--- common/params_keys.h
+++ common/params_keys.h
@@ -187,6 +187,8 @@ inline static std::unordered_map<std::string, ParamKeyAttributes> keys = {
 
     // sunnypilot car specific params
     {"HyundaiLongitudinalTuning", {PERSISTENT | BACKUP, INT, "0"}},
+    {"SubaruStopAndGo", {PERSISTENT | BACKUP}},
+    {"SubaruStopAndGoManualParkingBrake", {PERSISTENT | BACKUP}},
 
     {"DynamicExperimentalControl", {PERSISTENT | BACKUP, BOOL, "0"}},
     {"BlindSpot", {PERSISTENT | BACKUP, BOOL, "0"}},
