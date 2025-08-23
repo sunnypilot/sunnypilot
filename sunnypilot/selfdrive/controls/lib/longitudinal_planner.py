661b35c592f4076fc2513bf7bba6163f7730630e
nayan <nayan8teen@gmail.com>
2025-08-23 19:54:25 -0400

    dec/longitudinal planner: acc to e2e transition smoother #1100

--- sunnypilot/selfdrive/controls/lib/longitudinal_planner.py
+++ sunnypilot/selfdrive/controls/lib/longitudinal_planner.py
@@ -4,9 +4,11 @@ Copyright (c) 2021-, Haibin Wen, sunnypilot, and a number of other contributors.
 This file is part of sunnypilot and is licensed under the MIT License.
 See the LICENSE.md file in the root directory for more details.
 """
+import math
 
 from cereal import messaging, custom
 from opendbc.car import structs
+from opendbc.car.interfaces import ACCEL_MIN
 from openpilot.selfdrive.car.cruise import V_CRUISE_UNSET
 from openpilot.sunnypilot.selfdrive.controls.lib.dec.dec import DynamicExperimentalController
 from openpilot.sunnypilot.selfdrive.controls.lib.vision_turn_controller import VisionTurnController
@@ -19,6 +21,7 @@ class LongitudinalPlannerSP:
   def __init__(self, CP: structs.CarParams, mpc):
     self.dec = DynamicExperimentalController(CP, mpc)
     self.v_tsc = VisionTurnController(CP)
+    self.transition_init()
     self.generation = int(model_bundle.generation) if (model_bundle := get_active_bundle()) else None
 
   @property
@@ -38,6 +41,32 @@ class LongitudinalPlannerSP:
     v_cruise_v_tsc = self.v_tsc.v_turn if self.v_tsc.is_active else V_CRUISE_UNSET
 
     return min(v_cruise, v_cruise_v_tsc)
+
+  def transition_init(self) -> None:
+    self._transition_counter = 0
+    self._transition_steps = 20
+    self._last_mode = 'acc'
+
+  def handle_mode_transition(self, mode: str) -> None:
+    if self._last_mode != mode:
+      if mode == 'blended':
+        self._transition_counter = 0
+      self._last_mode = mode
+
+  def blend_accel_transition(self, mpc_accel: float, e2e_accel: float, v_ego: float) -> float:
+    if self.dec.enabled():
+      if self._transition_counter < self._transition_steps:
+        self._transition_counter += 1
+        progress = self._transition_counter / self._transition_steps
+        if v_ego > 5.0 and e2e_accel < 0.0:
+          if mpc_accel < 0.0 and e2e_accel > mpc_accel:
+            return mpc_accel
+          # use k3.0 and normalize midpoint at 0.5
+          sigmoid = 1.0 / (1.0 + math.exp(-3.0 * (abs(e2e_accel / ACCEL_MIN) - 0.5)))
+          blend_factor = 1.0 - (1.0 - progress) * (1.0 - sigmoid)
+          blended = mpc_accel + (e2e_accel - mpc_accel) * blend_factor
+          return blended
+    return min(mpc_accel, e2e_accel)
 
   def update(self, sm: messaging.SubMaster) -> None:
     self.dec.update(sm)
