ca1a626d7aa968b3a975383bc2019610cbfd987c
Jason Wen <haibin.wen3@gmail.com>
2025-08-25 08:16:08 -0400

    fix

--- sunnypilot/selfdrive/selfdrived/events.py
+++ sunnypilot/selfdrive/selfdrived/events.py
@@ -1,6 +1,6 @@
 import cereal.messaging as messaging
 from cereal import log, car, custom
-from openpilot.common.conversions import Conversions as CV
+from openpilot.common.constants import CV
 from openpilot.sunnypilot.selfdrive.selfdrived.events_base import EventsBase, Priority, ET, Alert, \
   NoEntryAlert, ImmediateDisableAlert, EngagementAlert, NormalPermanentAlert, AlertCallbackType, wrong_car_mode_alert
 
