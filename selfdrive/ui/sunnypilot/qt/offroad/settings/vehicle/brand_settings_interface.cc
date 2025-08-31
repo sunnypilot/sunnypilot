63dc9cc60e34b5a15af7e8e4417683c279d7ce3b
Candy0707 <j890707@gmail.com>
2025-08-31 20:35:29 +0800

    Fast Brand vehicle Settings

--- selfdrive/ui/sunnypilot/qt/offroad/settings/vehicle/brand_settings_interface.cc
+++ selfdrive/ui/sunnypilot/qt/offroad/settings/vehicle/brand_settings_interface.cc
@@ -7,15 +7,59 @@
 
 #include "selfdrive/ui/sunnypilot/qt/offroad/settings/vehicle/brand_settings_interface.h"
 
-BrandSettingsInterface::BrandSettingsInterface(QWidget *parent) : QWidget(parent) {
+BrandSettingsInterface::BrandSettingsInterface(QWidget *parent) : QWidget(parent)
+{
   QVBoxLayout *main_layout = new QVBoxLayout(this);
   main_layout->setContentsMargins(0, 0, 0, 0);
 
+  title = new QLabel(tr("<span style='font-family: \"Noto Color Emoji\";'>🚗</span> Brand vehicle settings <span style='font-family: Noto Color Emoji;'>⚙️</span>"));
+  // title->setStyleSheet("font-size: 50px; font-weight: 500;");
+  //  設定字體大小與粗細
+  title->setStyleSheet(
+      "font-size: 50px; "
+      "font-weight: 500; ");
+  // 置中對齊
+  title->setAlignment(Qt::AlignCenter);
+  main_layout->addWidget(title, Qt::AlignHCenter);
+
   list = new ListWidget(this, false);
+  list->setSpacing(20);
   main_layout->addWidget(list);
 }
 
-void BrandSettingsInterface::updatePanel(bool _offroad) {
+void BrandSettingsInterface::updatePanel(bool _offroad)
+{
   offroad = _offroad;
   updateSettings();
+}
+
+void BrandSettingsInterface::SetToggles(const std::vector<std::tuple<QString, QString, QString, QString, bool>> &toggle_defs)
+{
+  ListWidget *widget = new ListWidget(this);
+  for (auto &[param, t_title, t_desc, t_icon, t_needs_restart] : toggle_defs)
+  {
+    // 建立 ParamControl
+    ParamControl *toggle = new ParamControl(param, t_title, t_desc, t_icon, this);
+
+    // 判斷是否鎖定
+    bool locked = params.getBool((param + "Lock").toStdString());
+    toggle->setEnabled(!locked);
+
+    // 如果需要重啟且未鎖定，附加描述文字和連線事件
+    if (t_needs_restart && !locked)
+    {
+      toggle->setDescription(toggle->getDescription() + tr(" Changing this setting will restart openpilot if the car is powered on."));
+
+      QObject::connect(uiState(), &UIState::engagedChanged, [toggle](bool engaged)
+                       { toggle->setEnabled(!engaged); });
+
+      QObject::connect(toggle, &ParamControl::toggleFlipped, [=](bool state)
+                       { params.putBool("OnroadCycleRequested", true); });
+    }
+
+    // 加到 ListWidget
+    ListToggles[param] = toggle;
+    widget->addItem(toggle);
+  }
+  list->addItem(widget);
 }
--- selfdrive/ui/sunnypilot/qt/offroad/settings/vehicle/toyota_settings.cc
+++ selfdrive/ui/sunnypilot/qt/offroad/settings/vehicle/toyota_settings.cc
@@ -7,8 +7,20 @@
 
 #include "selfdrive/ui/sunnypilot/qt/offroad/settings/vehicle/toyota_settings.h"
 
-ToyotaSettings::ToyotaSettings(QWidget *parent) : BrandSettingsInterface(parent) {
+ToyotaSettings::ToyotaSettings(QWidget *parent) : BrandSettingsInterface(parent)
+{
+    // 自己的 5 個開關設定
+  std::vector<std::tuple<QString, QString, QString, QString, bool>> toggle_defs = {
+      {"Test1", tr("Test 1"), tr("Description for Test1"), "../assets/icons/chffr_wheel.png", true},
+      {"Test2", tr("Test 2"), tr("Description for Test2"), "../assets/icons/chffr_wheel.png", true},
+      {"Test3", tr("Test 3"), tr("Description for Test3"), "../assets/icons/chffr_wheel.png", true},
+      {"Test4", tr("Test 4"), tr("Description for Test4"), "../assets/icons/chffr_wheel.png", true},
+      {"Test5", tr("Test 5"), tr("Description for Test5"), "../assets/icons/chffr_wheel.png", true},
+  };
+  SetToggles(toggle_defs);
 }
 
-void ToyotaSettings::updateSettings() {
+void ToyotaSettings::updateSettings()
+{
+
 }
