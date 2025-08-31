f57b04f060e1108e3d558854e7088910a270738b
rav4kumar <meetkumardesai@gmail.com>
2025-08-31 13:50:43 -0700

    misc. toggles.

--- common/params_keys.h
+++ common/params_keys.h
@@ -171,6 +171,7 @@ inline static std::unordered_map<std::string, ParamKeyAttributes> keys = {
     {"ToyotaAutoHold", {PERSISTENT | BACKUP, BOOL, "0"}},
     {"ToyotaEnhancedBsm", {PERSISTENT | BACKUP, BOOL, "0"}},
     {"ToyotaTSS2Long", {PERSISTENT | BACKUP, BOOL, "0"}},
+    {"ToyotaStockLongitudinal", {PERSISTENT | BACKUP, BOOL, "0"}},
     {"ToyotaDriveMode", {PERSISTENT | BACKUP, BOOL, "0"}},
     {"RoadEdgeLaneChangeEnabled", {PERSISTENT | BACKUP, BOOL, "0"}},
 
