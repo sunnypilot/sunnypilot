83b8ee4b00cf9d9b484e7d638f78be05ad6d9e50
Jason Wen <haibin.wen3@gmail.com>
2025-09-11 23:13:21 -0400

    move around

--- selfdrive/ui/sunnypilot/ui.h
+++ selfdrive/ui/sunnypilot/ui.h
@@ -9,11 +9,11 @@
 
 #include <optional>
 
-#include "selfdrive/ui/qt/util.h"
 #include "selfdrive/ui/sunnypilot/qt/network/sunnylink/models/user_model.h"
 #include "selfdrive/ui/sunnypilot/qt/network/sunnylink/models/role_model.h"
 #include "selfdrive/ui/sunnypilot/qt/network/sunnylink/models/sponsor_role_model.h"
 #include "selfdrive/ui/ui.h"
+#include "selfdrive/ui/qt/util.h"
 
 class UIStateSP : public UIState {
   Q_OBJECT
