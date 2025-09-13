cc68e46f6985387b1249b31eb3e5bd30d3faea39
D <me@kumo.dev>
2025-09-13 17:54:49 +1000

    bitnimble fixes

--- common/params_keys.h
+++ common/params_keys.h
@@ -160,8 +160,8 @@ inline static std::unordered_map<std::string, ParamKeyAttributes> keys = {
     {"QuietMode", {PERSISTENT | BACKUP, BOOL, "0"}},
     {"RainbowMode", {PERSISTENT | BACKUP, BOOL, "0"}},
     {"ShowAdvancedControls", {PERSISTENT | BACKUP, BOOL, "0"}},
-    {"PersistTetheringAcrossReboot", PERSISTENT | BACKUP},
-    {"TetheringEnabled", PERSISTENT | BACKUP},
+    {"PersistTetheringAcrossReboot", {PERSISTENT | BACKUP, BOOL}},
+    {"TetheringEnabled", {PERSISTENT | BACKUP, BOOL}},
 
     // MADS params
     {"Mads", {PERSISTENT | BACKUP, BOOL, "1"}},
