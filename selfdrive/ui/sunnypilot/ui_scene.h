e7a3ea1b32b36fe220c2e78faecb874c9eeb99e8
DevTekVE <devtekve@gmail.com>
2025-09-14 12:18:59 +0200

    (TICI) sync `master` and `master-tici` (#1245)
    * SL: bugfix parameter handling in sunnylink restore and remote setting (#1234)
    
    * refactor: improve parameter handling in sunnylink for robustness
    
    - Updated `get_param_as_byte` to return `None` for nonexistent parameters.
    - Enhanced param compression and encoding in `sunnylinkd`.
    
    * refactor: centralize parameter restoration with new helper function
    
    - Added `save_param_from_base64_encoded_string` to handle param decoding and saving.
    - Updated backup manager and sunnylinkd to use the new method.
    - Improved code readability and reduced duplication in parameter handling logic.
    
    * don't bother
    
    * clean
    
    (cherry picked from commit b7f8dd11a559f9cf7a427990e6d158d96a10a707)
    
    * UI: Developer UI (#1233)
    
    (cherry picked from commit 1bb4ca2547448f2189f67cb54db150e78bc4d487)
    
    * Revert & Reapply "UI: Developer UI" temporarily due to QT version mismatch (#1237)
    
    * Revert "UI: Developer UI (#1233)"
    
    This reverts commit 1bb4ca2547448f2189f67cb54db150e78bc4d487.
    
    * Reapply "UI: Developer UI (#1233)"
    
    This reverts commit b0a77049dacbdd59bc21d154820ba8cadf4ad201.
    
    * QColorConstants is not on device's QT version. Thanks @kumar for the fix
    
    (cherry picked from commit 810a2d9448df89ef84ecc1c794b63fad55350d69)
    
    * Revert "UI: Developer UI" (#1238)
    
    * Revert "Revert & Reapply "UI: Developer UI" temporarily due to QT version mismatch (#1237)"
    
    This reverts commit 810a2d9448df89ef84ecc1c794b63fad55350d69.
    
    * Revert "UI: Developer UI (#1233)"
    
    This reverts commit 1bb4ca2547448f2189f67cb54db150e78bc4d487.
    
    (cherry picked from commit 1be13fdc55a24a72bc33703c7343c9421fdd0842)
    
    * Reapply "UI: Developer UI" (#1238) (#1239)
    
    This reverts commit 1be13fdc55a24a72bc33703c7343c9421fdd0842.
    
    Co-authored-by: Jason Wen <haibin.wen3@gmail.com>
    (cherry picked from commit 4f44d6e6436041a4f09a4d5c530405fc019c2492)
    
    ---------
    
    Co-authored-by: Nayan <nayan8teen@gmail.com>
    Co-authored-by: Jason Wen <haibin.wen3@gmail.com>

---
+++ selfdrive/ui/sunnypilot/ui_scene.h
@@ -0,0 +1,12 @@
+/**
+  * Copyright (c) 2021-, Haibin Wen, sunnypilot, and a number of other contributors.
+  *
+  * This file is part of sunnypilot and is licensed under the MIT License.
+  * See the LICENSE.md file in the root directory for more details.
+ */
+
+#pragma once
+
+typedef struct UISceneSP : UIScene {
+  int dev_ui_info = 0;
+} UISceneSP;
