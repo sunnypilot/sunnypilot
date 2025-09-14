bffb2fb6fabd384a39517e46d841b45b97a58210
DevTekVE <devtekve@gmail.com>
2025-09-14 20:22:51 +0200

    bugfix: param store to support the latest param changes (#1244)
    * bugfix: update parameter handling to use custom CarControlSP.Param and improve param retrieval
    
    * bump opendbc

--- sunnypilot/selfdrive/controls/lib/param_store.py
+++ sunnypilot/selfdrive/controls/lib/param_store.py
@@ -4,39 +4,41 @@ Copyright (c) 2021-, Haibin Wen, sunnypilot, and a number of other contributors.
 This file is part of sunnypilot and is licensed under the MIT License.
 See the LICENSE.md file in the root directory for more details.
 """
-import capnp
-
 from cereal import custom
-
 from opendbc.car import structs
 from openpilot.common.params import Params
+
+from sunnypilot.sunnylink.utils import get_param_as_byte
 
 
 class ParamStore:
   keys: list[str]
-  values: dict[str, str]
+  _params: dict[str, custom.CarControlSP.Param]
 
   def __init__(self, CP: structs.CarParams):
     universal_params: list[str] = []
     brand_params: list[str] = []
 
     self.keys = universal_params + brand_params
-    self.values = {}
-    self.cached_params_list: list[capnp.lib.capnp._DynamicStructBuilder] | None = None
+    self._params = {}
 
     self.frame = 0
 
   def update(self, params: Params) -> None:
-    if self.frame % 300 == 0:
-      old_values = dict(self.values)
-      self.values = {k: params.get(k) or "0" for k in self.keys}
-      if old_values != self.values:
-        self.cached_params_list = None
-
     self.frame += 1
+    if self.frame % 300 != 0:
+      return
 
-  def publish(self) -> list[capnp.lib.capnp._DynamicStructBuilder]:
-    if self.cached_params_list is None:
-      # TODO-SP: Why are we doing a list instead of a dictionary here?
-      self.cached_params_list = [custom.CarControlSP.Param(key=k, value=self.values[k]) for k in self.keys]
-    return self.cached_params_list
+    for key in self.keys:
+      param_type = params.get_type(key).name.lower()  # Using string instead of number because its "loose" dependency, and could change by OP at anytime.
+
+      # Over engineering opportunity: It's possible this conversion is slow, we may check the value as params returns it for cache purposes. Not today.
+      param_value = get_param_as_byte(key, params)
+      if (existing_param := self._params.get(key)) is not None and existing_param.value == param_value:
+        continue
+
+      self._params[key] = custom.CarControlSP.Param(key=key, value=param_value, type=param_type)
+
+  @property
+  def param_list(self) -> list[custom.CarControlSP.Param]:
+    return [v for k,v in self._params.items()]
