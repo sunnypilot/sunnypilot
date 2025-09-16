699a67bc5adfcbb6939ac49ed18b045dac164c77
Jason Wen <haibin.wen3@gmail.com>
2025-09-16 16:40:32 -0400

    not needed

--- sunnypilot/selfdrive/controls/lib/smart_cruise_control/smart_cruise_control.py
+++ sunnypilot/selfdrive/controls/lib/smart_cruise_control/smart_cruise_control.py
@@ -9,8 +9,6 @@ from opendbc.car import structs
 
 
 class SmartCruiseControl:
-  def __init__(self, CP: structs.CarParams):
-    self.CP = CP
 
   def update(self, sm: messaging.SubMaster, v_ego: float, a_ego: float, v_cruise: float) -> tuple[float, float]:
     targets = {
