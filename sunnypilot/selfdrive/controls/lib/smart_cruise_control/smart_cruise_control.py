9a6bc398eb45901caf839f90e34d2548ef119e8c
Jason Wen <haibin.wen3@gmail.com>
2025-09-16 16:43:37 -0400

    unused

--- sunnypilot/selfdrive/controls/lib/smart_cruise_control/smart_cruise_control.py
+++ sunnypilot/selfdrive/controls/lib/smart_cruise_control/smart_cruise_control.py
@@ -5,7 +5,6 @@ This file is part of sunnypilot and is licensed under the MIT License.
 See the LICENSE.md file in the root directory for more details.
 """
 from cereal import messaging
-from opendbc.car import structs
 
 
 class SmartCruiseControl:
