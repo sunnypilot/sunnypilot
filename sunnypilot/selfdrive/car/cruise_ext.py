fb743d313e441e09c07b23d8497ebe98f3fa5c14
Jason Wen <haibin.wen3@gmail.com>
2025-09-18 16:47:40 -0400

    Reapply "Intelligent Cruise Button Management (ICBM)" (#1257) (#1258)
    * Reapply "Intelligent Cruise Button Management (ICBM)" (#1257)
    
    This reverts commit 4441671227ab6936c60a5f8898a4c03ef7bee2b6.
    
    * ICBM: `pcmCruiseSpeed` is true during initialization
    
    * bump
    
    * fix tests

--- sunnypilot/selfdrive/car/cruise_ext.py
+++ sunnypilot/selfdrive/car/cruise_ext.py
@@ -7,18 +7,45 @@ See the LICENSE.md file in the root directory for more details.
 import numpy as np
 
 from cereal import car
+from opendbc.car import structs
 from openpilot.common.params import Params
+from openpilot.sunnypilot.selfdrive.car.intelligent_cruise_button_management.helpers import get_minimum_set_speed
 
 ButtonType = car.CarState.ButtonEvent.Type
 
+CRUISE_BUTTON_TIMER = {ButtonType.decelCruise: 0, ButtonType.accelCruise: 0,
+                       ButtonType.setCruise: 0, ButtonType.resumeCruise: 0,
+                       ButtonType.cancel: 0, ButtonType.mainCruise: 0}
+
+V_CRUISE_MIN = 8
+
+
+def update_manual_button_timers(CS: car.CarState, button_timers: dict[car.CarState.ButtonEvent.Type, int]) -> None:
+  # increment timer for buttons still pressed
+  for k in button_timers:
+    if button_timers[k] > 0:
+      button_timers[k] += 1
+
+  for b in CS.buttonEvents:
+    if b.type.raw in button_timers:
+      # Start/end timer and store current state on change of button pressed
+      button_timers[b.type.raw] = 1 if b.pressed else 0
+
+
 
 class VCruiseHelperSP:
-  def __init__(self) -> None:
+  def __init__(self, CP: structs.CarParams, CP_SP: structs.CarParamsSP) -> None:
+    self.CP = CP
+    self.CP_SP = CP_SP
     self.params = Params()
+    self.v_cruise_min = 0
+    self.enabled_prev = False
 
     self.custom_acc_enabled = self.params.get_bool("CustomAccIncrementsEnabled")
     self.short_increment = self.params.get("CustomAccShortPressIncrement", return_default=True)
     self.long_increment = self.params.get("CustomAccLongPressIncrement", return_default=True)
+
+    self.enable_button_timers = CRUISE_BUTTON_TIMER
 
   def read_custom_set_speed_params(self) -> None:
     self.custom_acc_enabled = self.params.get_bool("CustomAccIncrementsEnabled")
@@ -39,3 +66,26 @@ class VCruiseHelperSP:
     v_cruise_delta = v_cruise_delta * actual_increment
 
     return round_to_nearest, v_cruise_delta
+
+  def get_minimum_set_speed(self, is_metric: bool) -> None:
+    if self.CP_SP.pcmCruiseSpeed:
+      self.v_cruise_min = V_CRUISE_MIN
+      return
+
+    self.v_cruise_min = get_minimum_set_speed(is_metric)
+
+  def update_enabled_state(self, CS: car.CarState, enabled: bool) -> bool:
+    # special enabled state for non pcmCruiseSpeed, unchanged for non pcmCruise
+    if not self.CP_SP.pcmCruiseSpeed:
+      update_manual_button_timers(CS, self.enable_button_timers)
+      button_pressed = any(self.enable_button_timers[k] > 0 for k in self.enable_button_timers)
+
+      if enabled and not self.enabled_prev:
+        self.enabled_prev = not button_pressed
+        enabled = False
+      elif not enabled:
+        self.enabled_prev = enabled
+
+      return enabled and self.enabled_prev
+
+    return enabled
