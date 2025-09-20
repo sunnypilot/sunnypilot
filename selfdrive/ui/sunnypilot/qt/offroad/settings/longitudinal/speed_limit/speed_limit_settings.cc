7fed91f66914830f733562ea7857d5865e5c7cb0
Jason Wen <haibin.wen3@gmail.com>
2025-09-20 17:18:05 -0400

    ui: fix scrolling behavior in Speed Limit settings

--- selfdrive/ui/sunnypilot/qt/offroad/settings/longitudinal/speed_limit/speed_limit_settings.cc
+++ selfdrive/ui/sunnypilot/qt/offroad/settings/longitudinal/speed_limit/speed_limit_settings.cc
@@ -43,10 +43,12 @@ SpeedLimitSettings::SpeedLimitSettings(QWidget *parent) : QStackedWidget(parent)
 
   speedLimitSource = new PushButtonSP(tr("Customize Source"));
   connect(speedLimitSource, &QPushButton::clicked, [&]() {
+    speedLimitScroller->setLastScrollPosition();
     setCurrentWidget(speedLimitPolicyScreen);
     speedLimitPolicyScreen->refresh();
   });
   connect(speedLimitPolicyScreen, &SpeedLimitPolicy::backPress, [&]() {
+    speedLimitScroller->restoreScrollPosition();
     setCurrentWidget(subPanelFrame);
     showEvent(new QShowEvent());
   });
@@ -92,7 +94,8 @@ SpeedLimitSettings::SpeedLimitSettings(QWidget *parent) : QStackedWidget(parent)
   connect(speed_limit_offset_settings, &ButtonParamControlSP::buttonClicked, this, &SpeedLimitSettings::refresh);
 
   refresh();
-  subPanelLayout->addWidget(list);
+  speedLimitScroller = new ScrollViewSP(list, this);
+  subPanelLayout->addWidget(speedLimitScroller);
   addWidget(subPanelFrame);
   addWidget(speedLimitPolicyScreen);
   setCurrentWidget(subPanelFrame);
