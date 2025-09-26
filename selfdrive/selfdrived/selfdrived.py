b53adc7c47e2613bdc14456d98b5848a9cb36026
nayan <nayan8teen@gmail.com>
2025-09-25 23:12:45 -0400

    events..for real

--- selfdrive/selfdrived/selfdrived.py
+++ selfdrive/selfdrived/selfdrived.py
@@ -303,6 +303,12 @@ class SelfdriveD(CruiseHelper):
     elif lane_turn_direction == custom.TurnDirection.turnRight:
       self.events_sp.add(custom.OnroadEventSP.EventName.laneTurnRight)
 
+    # E2E Lead Departure & Green Light Alerts
+    # TODO-SP: Can be handled in longitudinal_planner directly once SLC is merged
+    if (self.sm['longitudinalPlan'].e2eAlerts.greenLightAlert
+            or self.sm['longitudinalPlan'].e2eAlerts.leadDepartAlert):
+      self.events_sp.add(custom.OnroadEventSP.EventName.e2eChime)
+
     for i, pandaState in enumerate(self.sm['pandaStates']):
       # All pandas must match the list of safetyConfigs, and if outside this list, must be silent or noOutput
       if i < len(self.CP.safetyConfigs):
