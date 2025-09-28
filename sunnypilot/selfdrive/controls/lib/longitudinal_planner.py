10468f4a779d9b5c3edd2a60157634a09b429792
Jason Wen <haibin.wen3@gmail.com>
2025-09-28 13:07:24 -0400

    stop

--- sunnypilot/selfdrive/controls/lib/longitudinal_planner.py
+++ sunnypilot/selfdrive/controls/lib/longitudinal_planner.py
@@ -64,7 +64,7 @@ class LongitudinalPlannerSP:
     # Speed Limit Assist
     has_speed_limit = self.resolver.speed_limit_valid or self.resolver.speed_limit_last_valid
     self.sla.update(long_enabled, long_override, v_ego, a_ego, v_cruise_cluster, self.resolver.speed_limit,
-                    self.resolver.speed_limit_final_last, has_speed_limit, self.resolver.distance, self.events_sp, CS)
+                    self.resolver.speed_limit_final_last, has_speed_limit, self.resolver.distance, self.events_sp)
 
     targets = {
       LongitudinalPlanSource.cruise: (v_cruise, a_ego),
