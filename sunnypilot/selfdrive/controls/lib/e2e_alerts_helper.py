82e12381b93978ed15a146c4a708482b3d8f1dff
Jason Wen <haibin.wen3@gmail.com>
2025-09-30 23:41:35 -0400

    a bit more

--- sunnypilot/selfdrive/controls/lib/e2e_alerts_helper.py
+++ sunnypilot/selfdrive/controls/lib/e2e_alerts_helper.py
@@ -12,40 +12,40 @@ from openpilot.common.realtime import DT_MDL
 from openpilot.sunnypilot import PARAMS_UPDATE_PERIOD
 from openpilot.sunnypilot.selfdrive.selfdrived.events import EventsSP
 
+TRIGGER_THRESHOLD = 30
+
 
 class E2EAlertsHelper:
-
   def __init__(self):
     self._params = Params()
     self._frame = -1
 
     self.greenLightAlert = False
-    self.greenLightAlertEnabled = self._params.get_bool("GreenLightAlert")
+    self.enabled = self._params.get_bool("GreenLightAlert")
 
   def _read_params(self) -> None:
     if self._frame % int(PARAMS_UPDATE_PERIOD / DT_MDL) == 0:
-      self.greenLightAlertEnabled = self._params.get_bool("GreenLightAlert")
+      self.enabled = self._params.get_bool("GreenLightAlert")
+
     self._frame += 1
 
   def update(self, sm: messaging.SubMaster, events_sp: EventsSP) -> None:
     self._read_params()
+
+    if not self.enabled:
+      return
 
     model_x: list[float] = sm['modelV2'].position.x
     max_idx = len(model_x) - 1
     lead_status: bool = sm['radarState'].leadOne.status
     standstill: bool = sm['carState'].standstill
     gasPressed: bool = sm['carState'].gasPressed
-    _greenLightAlert = False
 
     # Green light alert
-    if (self.greenLightAlertEnabled
-            and standstill
-            and model_x[max_idx] > 30
-            and not lead_status
-            and not gasPressed):
-      _greenLightAlert = True
-
-    self.greenLightAlert = _greenLightAlert
+    if model_x[max_idx] > TRIGGER_THRESHOLD and standstill and not lead_status and not gasPressed:
+      self.greenLightAlert = True
+    else:
+      self.greenLightAlert = False
 
     if self.greenLightAlert:
       events_sp.add(custom.OnroadEventSP.EventName.e2eChime)
