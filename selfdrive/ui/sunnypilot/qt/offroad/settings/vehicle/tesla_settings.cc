cbac9c345fb552c151b4e91a0e79d5013f46fc83
Amy Jeanes <amyjeanes23@gmail.com>
2025-10-02 22:10:49 +0100

    Tesla: Coop Steering

--- selfdrive/ui/sunnypilot/qt/offroad/settings/vehicle/tesla_settings.cc
+++ selfdrive/ui/sunnypilot/qt/offroad/settings/vehicle/tesla_settings.cc
@@ -6,8 +6,33 @@
  */
 
 #include "selfdrive/ui/sunnypilot/qt/offroad/settings/vehicle/tesla_settings.h"
+#include "common/params.h"
+#include "common/util.h"
 
 TeslaSettings::TeslaSettings(QWidget *parent) : BrandSettingsInterface(parent) {
+  constexpr int coopSteeringMinKmh = 23; // minimum speed for cooperative steering (enforced by Tesla firmware)
+  Params params;
+  bool is_metric = params.getBool("IsMetric");
+  QString unit = is_metric ? "km/h" : "mph";
+  int display_value;
+  if (is_metric) {
+    display_value = coopSteeringMinKmh;
+  } else {
+    display_value = static_cast<int>(std::round(coopSteeringMinKmh * KM_TO_MILE));
+  }
+  const QString coop_desc = tr("Allows the driver to provide limited steering input while openpilot is engaged. Only works above %1 %2.")
+                                .arg(display_value)
+                                .arg(unit);
+
+  coopSteeringToggle = new ParamControlSP(
+    "TeslaCoopSteering",
+    tr("Cooperative Steering"),
+    coop_desc,
+    "",
+    this
+  );
+  list->addItem(coopSteeringToggle);
+  coopSteeringToggle->showDescription();
 }
 
 void TeslaSettings::updateSettings() {
