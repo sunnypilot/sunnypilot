c9f77b3f5e19506e700d419364571c092da3c641
Jason Wen <haibin.wen3@gmail.com>
2025-10-05 03:01:24 -0400

    make sure to initialize

--- sunnypilot/selfdrive/car/interfaces.py
+++ sunnypilot/selfdrive/car/interfaces.py
@@ -22,8 +22,18 @@ def log_fingerprint(CP: structs.CarParams) -> None:
     sentry.capture_fingerprint(CP.carFingerprint, CP.brand)
 
 
-def _initialize_neural_network_lateral_control(CI: CarInterfaceBase, CP: structs.CarParams, CP_SP: structs.CarParamsSP,
-                                               params: Params = None, enabled: bool = False) -> None:
+def _enforce_torque_lateral_control(CP: structs.CarParams, params: Params = None, enabled: bool = False) -> bool:
+  if params is None:
+    params = Params()
+
+  if CP.steerControlType != structs.CarParams.SteerControlType.angle:
+    enabled = params.get_bool("EnforceTorqueControl")
+
+  return enabled
+
+
+def _initialize_neural_network_lateral_control(CP: structs.CarParams, CP_SP: structs.CarParamsSP,
+                                               params: Params = None, enabled: bool = False) -> bool:
   if params is None:
     params = Params()
 
@@ -35,12 +45,11 @@ def _initialize_neural_network_lateral_control(CI: CarInterfaceBase, CP: structs
   if nnlc_model_name != "MOCK" and CP.steerControlType != structs.CarParams.SteerControlType.angle:
     enabled = params.get_bool("NeuralNetworkLateralControl")
 
-  if enabled:
-    CI.configure_torque_tune(CP.carFingerprint, CP.lateralTuning)
-
   CP_SP.neuralNetworkLateralControl.model.path = nnlc_model_path
   CP_SP.neuralNetworkLateralControl.model.name = nnlc_model_name
   CP_SP.neuralNetworkLateralControl.fuzzyFingerprint = not exact_match
+
+  return enabled
 
 
 def _initialize_intelligent_cruise_button_management(CP: structs.CarParams, CP_SP: structs.CarParamsSP, params: Params = None) -> None:
@@ -52,12 +61,19 @@ def _initialize_intelligent_cruise_button_management(CP: structs.CarParams, CP_S
     CP_SP.pcmCruiseSpeed = False
 
 
+def _initialize_torque_lateral_control(CI: CarInterfaceBase, CP: structs.CarParams, enforce_torque: bool, nnlc_enabled: bool) -> None:
+  if nnlc_enabled or enforce_torque:
+    CI.configure_torque_tune(CP.carFingerprint, CP.lateralTuning)
+
+
 def setup_interfaces(CI: CarInterfaceBase, params: Params = None) -> None:
   CP = CI.CP
   CP_SP = CI.CP_SP
 
-  _initialize_neural_network_lateral_control(CI, CP, CP_SP, params)
+  enforce_torque = _enforce_torque_lateral_control(CP, params)
+  nnlc_enabled = _initialize_neural_network_lateral_control(CP, CP_SP, params)
   _initialize_intelligent_cruise_button_management(CP, CP_SP, params)
+  _initialize_torque_lateral_control(CI, CP, enforce_torque, nnlc_enabled)
 
 
 def initialize_params(params) -> list[dict[str, Any]]:
