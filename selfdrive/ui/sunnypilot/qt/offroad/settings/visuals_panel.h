54b060f178fe2879203627940a9983dd4e10d553
royjr <royjr96@gmail.com>
2025-10-08 20:45:17 -0400

    move with

--- selfdrive/ui/sunnypilot/qt/offroad/settings/visuals_panel.h
+++ selfdrive/ui/sunnypilot/qt/offroad/settings/visuals_panel.h
@@ -29,10 +29,10 @@ protected:
  ParamWatcher * param_watcher;
  ButtonParamControlSP *chevron_info_settings;
  ButtonParamControlSP *dev_ui_settings;
- int visual_style_value = 0;
- int visual_style_overhead_value = 0;
  OptionControlSP *visual_radar_tracks_delay_settings;
  ButtonParamControlSP *visual_wide_cam_settings;
+ int visual_style_value = 0;
+ int visual_style_overhead_value = 0;
  ButtonParamControlSP *visual_style_settings;
  ButtonParamControlSP *visual_style_zoom_settings;
  ButtonParamControlSP *visual_style_overhead_settings;
