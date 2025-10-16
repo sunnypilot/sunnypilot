2d779f5db9d46816e580728af55d09931e9961c7
Jason Wen <haibin.wen3@gmail.com>
2025-10-15 22:38:57 -0400

    E2E Helper: universal state machine

--- sunnypilot/selfdrive/controls/lib/e2e_alerts_helper.py
+++ sunnypilot/selfdrive/controls/lib/e2e_alerts_helper.py
@@ -15,10 +15,20 @@ from openpilot.sunnypilot.selfdrive.selfdrived.events import EventsSP
 GREEN_LIGHT_X_THRESHOLD = 30
 
 
+class E2EStates:
+  INACTIVE = 0
+  ARMED = 1
+  CONSUMED = 2
+
+
 class E2EAlertsHelper:
   def __init__(self):
     self._params = Params()
     self.frame = -1
+    self.green_light_state = E2EStates.INACTIVE
+    self.prev_green_light_state = E2EStates.INACTIVE
+    self.lead_depart_state = E2EStates.INACTIVE
+    self.prev_lead_depart_state = E2EStates.INACTIVE
 
     self.green_light_alert = False
     self.green_light_alert_enabled = self._params.get_bool("GreenLightAlert")
@@ -29,64 +39,104 @@ class E2EAlertsHelper:
     self.green_light_alert_count = 0
     self.last_lead_distance = -1
     self.last_moving_frame = -1
+    self.last_standstill = False
+
+    self.allowed = False
+    self.has_lead = False
 
   def _read_params(self) -> None:
     if self.frame % int(PARAMS_UPDATE_PERIOD / DT_MDL) == 0:
       self.green_light_alert_enabled = self._params.get_bool("GreenLightAlert")
       self.lead_depart_alert_enabled = self._params.get_bool("LeadDepartAlert")
 
-  def update(self, sm: messaging.SubMaster, events_sp: EventsSP) -> None:
-    self._read_params()
-
+  def update_alert_trigger(self, sm: messaging.SubMaster):
     CS = sm['carState']
     CC = sm['carControl']
 
     model_x = sm['modelV2'].position.x
     max_idx = len(model_x) - 1
-    has_lead = sm['radarState'].leadOne.status
+    self.has_lead = sm['radarState'].leadOne.status
     lead_dRel = sm['radarState'].leadOne.dRel
+
     standstill = CS.standstill
     moving = not standstill and CS.vEgo > 0.1
-    _allowed = standstill and not CS.gasPressed and not CC.enabled
 
     if moving:
       self.last_moving_frame = self.frame
     recent_moving = self.last_moving_frame == -1 or (self.frame - self.last_moving_frame) * DT_MDL < 2.0
 
-    if standstill and not recent_moving:
-      self.alert_allowed = True
-    elif not standstill:
-      self.alert_allowed = False
-      self.green_light_alert_count = 0
-      self.last_lead_distance = -1
+    self.allowed = not moving and not CS.gasPressed and not CC.enabled and not recent_moving
 
     # Green Light Alert
-    _green_light_alert = False
-    if self.green_light_alert_enabled and _allowed and not has_lead and model_x[max_idx] > GREEN_LIGHT_X_THRESHOLD:
-      if self.alert_allowed:
+    green_light_trigger = False
+    if self.green_light_state == E2EStates.ARMED:
+      if model_x[max_idx] > GREEN_LIGHT_X_THRESHOLD:
         self.green_light_alert_count += 1
       else:
         self.green_light_alert_count = 0
 
-      if self.green_light_alert_count > 2 and self.alert_allowed:
-        _green_light_alert = True
-        self.alert_allowed = False
-    else:
+      if self.green_light_alert_count > 2:
+        green_light_trigger = True
+    elif self.green_light_state != E2EStates.ARMED:
       self.green_light_alert_count = 0
 
-    self.green_light_alert = _green_light_alert
-
     # Lead Departure Alert
-    _lead_depart_alert = False
-    if self.lead_depart_alert_enabled and _allowed and has_lead:
+    lead_depart_trigger = False
+    if self.lead_depart_state == E2EStates.ARMED:
       if self.last_lead_distance == -1 or lead_dRel < self.last_lead_distance:
         self.last_lead_distance = lead_dRel
 
       if self.last_lead_distance != -1 and (lead_dRel - self.last_lead_distance > 1.0) and self.alert_allowed:
-        _lead_depart_alert = True
-        self.alert_allowed = False
+        lead_depart_trigger = True
+    elif self.lead_depart_state != E2EStates.ARMED:
+      self.last_lead_distance = -1
 
-    self.lead_depart_alert = _lead_depart_alert
+    return green_light_trigger, lead_depart_trigger
+
+  @staticmethod
+  def update_state_machine(state: int, enabled: bool, allowed: bool, triggered: bool) -> tuple[int, bool]:
+    if state != E2EStates.INACTIVE:
+      if not allowed or not enabled:
+        state = E2EStates.INACTIVE
+
+      else:
+        if state == E2EStates.ARMED:
+          if triggered:
+            state = E2EStates.CONSUMED
+
+          elif not allowed:
+            state = E2EStates.INACTIVE
+
+        elif state == E2EStates.CONSUMED:
+          pass
+
+    elif state == E2EStates.INACTIVE:
+      if allowed and enabled:
+        state = E2EStates.ARMED
+
+    return state, triggered
+
+  def update(self, sm: messaging.SubMaster, events_sp: EventsSP) -> None:
+    self._read_params()
+
+    green_light_trigger, lead_depart_trigger = self.update_alert_trigger(sm)
+
+    self.prev_green_light_state = self.green_light_state
+    self.prev_lead_depart_state = self.lead_depart_state
+
+    self.green_light_state, self.green_light_alert = self.update_state_machine(
+      self.green_light_state,
+      self.green_light_alert_enabled,
+      self.allowed and not self.has_lead,
+      green_light_trigger
+    )
+
+    self.lead_depart_state, self.lead_depart_alert = self.update_state_machine(
+      self.lead_depart_state,
+      self.lead_depart_alert_enabled,
+      self.allowed and self.has_lead,
+      lead_depart_trigger
+    )
 
     if self.green_light_alert or self.lead_depart_alert:
       events_sp.add(custom.OnroadEventSP.EventName.e2eChime)
