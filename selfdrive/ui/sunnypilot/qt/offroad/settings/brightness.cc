7070863cea9ed0fcf3cd8f8b6b2b961ce5fdcf4a
nayan <nayan8teen@gmail.com>
2025-10-18 12:14:47 -0400

    better

--- selfdrive/ui/sunnypilot/qt/offroad/settings/brightness.cc
+++ selfdrive/ui/sunnypilot/qt/offroad/settings/brightness.cc
@@ -7,38 +7,12 @@
 
 #include "selfdrive/ui/sunnypilot/qt/offroad/settings/brightness.h"
 
-// Map of Brightness Options
-const QMap<QString, QString> Brightness::brightness_options = {
-  {"0", "1"},  // Auto (Dark)
-  {"1", "0"},  // Auto
-  {"2", "5"},
-  {"3", "10"},
-  {"4", "15"},
-  {"5", "20"},
-  {"6", "25"},
-  {"7", "30"},
-  {"8", "35"},
-  {"9", "40"},
-  {"10", "45"},
-  {"11", "50"},
-  {"12", "55"},
-  {"13", "60"},
-  {"14", "65"},
-  {"15", "70"},
-  {"16", "75"},
-  {"17", "80"},
-  {"18", "85"},
-  {"19", "90"},
-  {"20", "95"},
-  {"21", "100"}
-};
-
 Brightness::Brightness() : OptionControlSP(
   "Brightness",
   tr("Global Brightness"),
   tr("Overrides the brightness of the device. This applies to both onroad and offroad screens. "),
   "../assets/offroad/icon_blank.png",
-  {0, 21}, 1, true, &brightness_options) {
+  {-5, 100}, 5, true) {
 
   refresh();
 }
