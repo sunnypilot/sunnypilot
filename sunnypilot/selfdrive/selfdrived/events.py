e4451049c61ed8d4b487c87229559c1761948b9e
Jason Wen <haibin.wen3@gmail.com>
2025-10-17 23:20:32 -0400

    lint

--- sunnypilot/selfdrive/selfdrived/events.py
+++ sunnypilot/selfdrive/selfdrived/events.py
@@ -4,7 +4,6 @@ from openpilot.common.constants import CV
 from openpilot.sunnypilot.selfdrive.selfdrived.events_base import EventsBase, Priority, ET, Alert, \
   NoEntryAlert, ImmediateDisableAlert, EngagementAlert, NormalPermanentAlert, AlertCallbackType, wrong_car_mode_alert
 from openpilot.sunnypilot.selfdrive.controls.lib.speed_limit import PCM_LONG_REQUIRED_MAX_SET_SPEED, CONFIRM_SPEED_THRESHOLD
-from openpilot.sunnypilot.selfdrive.controls.lib.speed_limit.helpers import compare_cluster_target
 
 
 AlertSize = log.SelfdriveState.AlertSize
