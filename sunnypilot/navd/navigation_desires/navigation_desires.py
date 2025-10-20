ad9bde8b1fdd1a3f63935dd93a0f2365993ab667
discountchubbs <alexgrant990@gmail.com>
2025-10-19 17:17:37 -0700

    non blocking polling
    SOME attribute protection. kids crying, so need to stop for now!

--- sunnypilot/navd/navigation_desires/navigation_desires.py
+++ sunnypilot/navd/navigation_desires/navigation_desires.py
@@ -14,8 +14,7 @@ class NavigationDesires:
   def __init__(self):
     self.sm = messaging.SubMaster(['navigationd'])
     self.desire = log.Desire.none
-    self.update_counter = -1
-    self.turn_speed_limit = 20 * CV.MPH_TO_MS
+    self._turn_speed_limit = 20 * CV.MPH_TO_MS
     self._params = Params()
     self.param_counter = -1
     self.nav_allowed: bool = False
@@ -27,24 +26,20 @@ class NavigationDesires:
 
   def update(self, CS: car.CarState, lateral_active: bool):
     self.update_params()
-    if not self.nav_allowed:
+    self.sm.update(0)
+    nav_msg = self.sm['navigationd']
+    if not self.nav_allowed or not nav_msg.valid or not lateral_active:
       self.desire = log.Desire.none
     else:
-      self.update_counter += 1
-      if self.update_counter % 6 == 0:  # ~.33 of a second at 20hz I think
-        self.sm.update()
-        nav_msg = self.sm['navigationd']
-        self.desire = log.Desire.none  # default for this update cycle
-        if nav_msg.valid and lateral_active:
-          upcoming = nav_msg.upcomingTurn
-          if upcoming == 'slightLeft' and (not CS.leftBlindspot or CS.vEgo < self.turn_speed_limit):
-            self.desire = log.Desire.keepLeft
-          elif upcoming == 'slightRight' and (not CS.rightBlindspot or CS.vEgo < self.turn_speed_limit):
-            self.desire = log.Desire.keepRight
-          elif upcoming == 'left' and CS.leftBlinker and not CS.rightBlinker and not CS.leftBlindspot and CS.vEgo < self.turn_speed_limit:
-            self.desire = log.Desire.turnLeft
-          elif upcoming == 'right' and CS.rightBlinker and not CS.leftBlinker and not CS.rightBlindspot and CS.vEgo < self.turn_speed_limit:
-            self.desire = log.Desire.turnRight
+      upcoming = nav_msg.upcomingTurn
+      if upcoming == 'slightLeft' and (not CS.leftBlindspot or CS.vEgo < self._turn_speed_limit):
+        self.desire = log.Desire.keepLeft
+      elif upcoming == 'slightRight' and (not CS.rightBlindspot or CS.vEgo < self._turn_speed_limit):
+        self.desire = log.Desire.keepRight
+      elif upcoming == 'left' and CS.leftBlinker and not CS.rightBlinker and not CS.leftBlindspot and CS.vEgo < self._turn_speed_limit:
+        self.desire = log.Desire.turnLeft
+      elif upcoming == 'right' and CS.rightBlinker and not CS.leftBlinker and not CS.rightBlindspot and CS.vEgo < self._turn_speed_limit:
+        self.desire = log.Desire.turnRight
 
   def get_desire(self) -> log.Desire:
     return self.desire
