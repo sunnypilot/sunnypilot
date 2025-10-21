b01397c3dcd9ae658ebc266ba6190d0b988b4d53
Jason Wen <haibin.wen3@gmail.com>
2025-10-21 01:13:09 -0400

    version: a new beginning

--- release/ci/publish.sh
+++ release/ci/publish.sh
@@ -30,7 +30,7 @@ if [ -z "$GIT_ORIGIN" ]; then
 fi
 
 # "Tagging"
-echo "#define COMMA_VERSION \"$VERSION\"" > ${OUTPUT_DIR}/common/version.h
+echo "#define SUNNYPILOT_VERSION \"$VERSION\"" > ${OUTPUT_DIR}/sunnypilot/common/version.h
 
 ## set git identity
 #source $DIR/identity.sh
--- tools/replay/consoleui.cc
+++ tools/replay/consoleui.cc
@@ -10,6 +10,8 @@
 #include "common/util.h"
 #include "common/version.h"
 
+#include "sunnypilot/common/version.h"
+
 namespace {
 
 const int BORDER_SIZE = 3;
@@ -119,7 +121,7 @@ void ConsoleUI::initWindows() {
 
   // set the title bar
   wbkgd(w[Win::Title], A_REVERSE);
-  mvwprintw(w[Win::Title], 0, 3, "sunnypilot replay %s", COMMA_VERSION);
+  mvwprintw(w[Win::Title], 0, 3, "sunnypilot replay %s", SUNNYPILOT_VERSION);
 
   // show windows on the real screen
   refresh();
