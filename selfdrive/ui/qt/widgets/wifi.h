e7ae00c75a41410280520cb96591faccdf950cd9
DevTekVE <devtekve@gmail.com>
2025-10-31 19:28:26 +0100

    sunnylink: introduce community popup with QR code embedding
    - Added `SunnylinkCommunityPopup` widget to promote the sunnypilot Community Forum.
    - Integrated a QR code generator and display for quick access.
    - Updated `WiFiPromptWidget` to include a "Learn More" button triggering the community popup.

--- selfdrive/ui/qt/widgets/wifi.h
+++ selfdrive/ui/qt/widgets/wifi.h
@@ -3,11 +3,16 @@
 #include <QFrame>
 #include <QWidget>
 
+#include "selfdrive/ui/sunnypilot/qt/offroad/settings/sunnylink/community_widget.h"
+
 class WiFiPromptWidget : public QFrame {
   Q_OBJECT
 
 public:
   explicit WiFiPromptWidget(QWidget* parent = 0);
+
+private:
+  SunnylinkCommunityPopup *community_popup;
 
 signals:
   void openSettings(int index = 0, const QString &param = "");
