682d738ffac3432549643a9889e1424192f88291
Amy Jeanes <amyjeanes23@gmail.com>
2025-11-02 02:47:30 +0000

    Tesla: Coop Steering (#1283)
    * Tesla: Coop Steering
    
    * bump
    
    * bump
    
    * sync with opendbc/master
    
    * resolve comment
    
    * add oscillation warning and add confirmation
    
    * styling desc
    
    * beta
    
    ---------
    
    Co-authored-by: Jason Wen <haibin.wen3@gmail.com>

--- common/params_keys.h
+++ common/params_keys.h
@@ -208,6 +208,7 @@ inline static std::unordered_map<std::string, ParamKeyAttributes> keys = {
     {"HyundaiLongitudinalTuning", {PERSISTENT | BACKUP, INT, "0"}},
     {"SubaruStopAndGo", {PERSISTENT | BACKUP, BOOL, "0"}},
     {"SubaruStopAndGoManualParkingBrake", {PERSISTENT | BACKUP, BOOL, "0"}},
+    {"TeslaCoopSteering", {PERSISTENT | BACKUP, BOOL, "0"}},
 
     {"DynamicExperimentalControl", {PERSISTENT | BACKUP, BOOL, "0"}},
     {"BlindSpot", {PERSISTENT | BACKUP, BOOL, "0"}},
