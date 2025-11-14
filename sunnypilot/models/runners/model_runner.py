90cbb094829501706b79eaab356a623328a4f23f
James Vecellio-Grant <159560811+Discountchubbs@users.noreply.github.com>
2025-11-13 21:03:00 -0800

    tinygrad: sync tinygrad and recompile models (#1464)
    * ref
    
    * add in the fixes
    
    * Update fetcher.py
    
    * Update helpers.py
    
    * force push same commit so we can restart CI

--- sunnypilot/models/runners/model_runner.py
+++ sunnypilot/models/runners/model_runner.py
@@ -14,13 +14,10 @@ CUSTOM_MODEL_PATH = Paths.model_root()
 # Set QCOM environment variable for TICI devices, potentially enabling hardware acceleration
 USBGPU = "USBGPU" in os.environ
 if USBGPU:
-  os.environ['AMD'] = '1'
+  os.environ['DEV'] = 'AMD'
   os.environ['AMD_IFACE'] = 'USB'
-elif TICI:
-  os.environ['QCOM'] = '1'
 else:
-  os.environ['LLVM'] = '1'
-  os.environ['JIT'] = '2'  # TODO: This may cause issues
+  os.environ['DEV'] = 'QCOM' if TICI else 'CPU'
 
 
 class ModelData:
