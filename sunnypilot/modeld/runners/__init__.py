ae13598d72c100ac8b7e2247ca9730940c9378f6
discountchubbs <alexgrant990@gmail.com>
2025-11-15 08:25:27 -0800

    del snpe

--- sunnypilot/modeld/runners/__init__.py
+++ sunnypilot/modeld/runners/__init__.py
@@ -4,20 +4,15 @@ from openpilot.sunnypilot.modeld.runners.runmodel_pyx import RunModel, Runtime
 assert Runtime
 
 USE_THNEED = int(os.getenv('USE_THNEED', str(int(TICI))))
-USE_SNPE = int(os.getenv('USE_SNPE', str(int(TICI))))
 
 class ModelRunner(RunModel):
   THNEED = 'THNEED'
-  SNPE = 'SNPE'
   ONNX = 'ONNX'
 
   def __new__(cls, paths, *args, **kwargs):
     if ModelRunner.THNEED in paths and USE_THNEED:
       from openpilot.sunnypilot.modeld.runners.thneedmodel_pyx import ThneedModel as Runner
       runner_type = ModelRunner.THNEED
-    elif ModelRunner.SNPE in paths and USE_SNPE:
-      from openpilot.sunnypilot.modeld.runners.snpemodel_pyx import SNPEModel as Runner
-      runner_type = ModelRunner.SNPE
     elif ModelRunner.ONNX in paths:
       from openpilot.sunnypilot.modeld.runners.onnxmodel import ONNXModel as Runner
       runner_type = ModelRunner.ONNX
