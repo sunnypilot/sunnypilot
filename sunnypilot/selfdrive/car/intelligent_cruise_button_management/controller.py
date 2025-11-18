0c98642043d04890d804865eceabe24088e9766b
downquark7 <mail2emn@gmail.com>
2025-11-17 21:38:38 -0600

    Refine v_cruise_equal to use a tolerance check
    I believe this change fixes a bug that affects icbm on nissan platforms where the set speed oscillates up and down by a couple mph instead of stopping at the target speed.

--- sunnypilot/selfdrive/car/intelligent_cruise_button_management/controller.py
+++ sunnypilot/selfdrive/car/intelligent_cruise_button_management/controller.py
@@ -47,7 +47,7 @@ class IntelligentCruiseButtonManagement:
 
   @property
   def v_cruise_equal(self) -> bool:
-    return self.v_target == self.v_cruise_cluster
+    return abs(self.v_target - self.v_cruise_cluster) < 0.1
 
   def update_calculations(self, CS: car.CarState, LP_SP: custom.LongitudinalPlanSP) -> None:
     speed_conv = CV.MS_TO_KPH if self.is_metric else CV.MS_TO_MPH
