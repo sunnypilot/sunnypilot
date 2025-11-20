5d489bf37c2cd06abbb23f8f6146c6f025f2a447
nayan <nayan8teen@gmail.com>
2025-11-20 11:17:04 -0500

    lambdas

--- selfdrive/ui/sunnypilot/layouts/settings/visuals.py
+++ selfdrive/ui/sunnypilot/layouts/settings/visuals.py
@@ -15,65 +15,65 @@ class VisualsLayout(Widget):
 
   def _initialize_items(self):
     self._blind_spot_toggle = toggle_item_sp(
-      title=tr("Show Blind Spot Warnings"),
+      title=lambda: tr("Show Blind Spot Warnings"),
       description=(
         "Enabling this will display warnings when a vehicle is detected in your blind spot as long as your car has BSM supported."),
       param="BlindSpot",
     )
     self._rainbow_toggle = toggle_item_sp(
-      title=tr("Enable Tesla Rainbow Mode"),
-      description=tr("A beautiful rainbow effect on the path the model wants to take.") +
-                  tr("It does not affect driving in any way."),
+      title=lambda: tr("Enable Tesla Rainbow Mode"),
+      description=lambda: tr("A beautiful rainbow effect on the path the model wants to take.") +
+                          tr("It does not affect driving in any way."),
       param="RainbowMode",
     )
     self._standstill_timer_toggle = toggle_item_sp(
-      title=tr("Enable Standstill Timer"),
-      description=tr("Show a timer on the HUD when the car is at a standstill."),
+      title=lambda: tr("Enable Standstill Timer"),
+      description=lambda: tr("Show a timer on the HUD when the car is at a standstill."),
       param="StandstillTimer",
     )
     self._roadname_toggle = toggle_item_sp(
       param="RoadNameToggle",
-      title=tr("Display Road Name"),
-      description=tr("Displays the name of the road the car is traveling on. ") +
-                  tr("The OpenStreetMap database of the location must be downloaded from the OSM panel to fetch the road name."),
+      title=lambda: tr("Display Road Name"),
+      description=lambda: tr("Displays the name of the road the car is traveling on. ") +
+                          tr("The OpenStreetMap database of the location must be downloaded from the OSM panel to fetch the road name."),
     )
     self._green_light_toggle = toggle_item_sp(
       param="GreenLightAlert",
-      title=tr("Green Traffic Light Alert (Beta)"),
-      description=tr("A chime and on-screen alert will play when the traffic light you are waiting for turns green and you have no vehicle in front of you. ") +
-                  tr("Note: This chime is only designed as a notification. It is the driver's responsibility to observe their environment and make decisions accordingly."),
+      title=lambda: tr("Green Traffic Light Alert (Beta)"),
+      description=lambda: tr("A chime and on-screen alert will play when the traffic light you are waiting for turns green and you have no vehicle in front of you. ") +
+                          tr("Note: This chime is only designed as a notification. It is the driver's responsibility to observe their environment and make decisions accordingly."),
     )
     self._lead_depart_toggle = toggle_item_sp(
       param="LeadDepartAlert",
-      title=tr("Lead Departure Alert (Beta)"),
-     description=tr("A chime and on-screen alert will play when you are stopped, and the vehicle in front of you start moving. ") +
-                 tr("Note: This chime is only designed as a notification. It is the driver's responsibility to observe their environment and make decisions accordingly."),
+      title=lambda: tr("Lead Departure Alert (Beta)"),
+     description=lambda: tr("A chime and on-screen alert will play when you are stopped, and the vehicle in front of you start moving. ") +
+                         tr("Note: This chime is only designed as a notification. It is the driver's responsibility to observe their environment and make decisions accordingly."),
     )
     self._true_vego_ui_toggle = toggle_item_sp(
       param="TrueVEgoUI",
-      title=tr("Speedometer: Always Display True Speed"),
-      description=tr("Always display the true vehicle current speed from wheel speed sensors."),
+      title=lambda: tr("Speedometer: Always Display True Speed"),
+      description=lambda: tr("Always display the true vehicle current speed from wheel speed sensors."),
     )
     self._hide_vego_ui_toggle = toggle_item_sp(
       param="HideVEgoUI",
-      title=tr("Speedometer: Hide from Onroad Screen"),
-      description=tr("When enabled, the speedometer on the onroad screen is not displayed."),
+      title=lambda: tr("Speedometer: Hide from Onroad Screen"),
+      description=lambda: tr("When enabled, the speedometer on the onroad screen is not displayed."),
     )
     self._turn_signals_toggle = toggle_item_sp(
       param="ShowTurnSignals",
-      title=tr("Display Turn Signals"),
-      description=tr("When enabled, visual turn indicators are drawn on the HUD."),
+      title=lambda: tr("Display Turn Signals"),
+      description=lambda: tr("When enabled, visual turn indicators are drawn on the HUD."),
     )
     self._chevron_info = multiple_button_item_sp(
-      title=tr("Display Metrics Below Chevron"),
-      description=tr("Display useful metrics below the chevron that tracks the lead car (only applicable to cars with sunnypilot longitudinal control)."),
-      buttons=[tr("Off"), tr("Distance"), tr("Speed"), tr("Time"), tr("All")],
+      title=lambda: tr("Display Metrics Below Chevron"),
+      description=lambda: tr("Display useful metrics below the chevron that tracks the lead car (only applicable to cars with sunnypilot longitudinal control)."),
+      buttons=[lambda: tr("Off"), lambda: tr("Distance"), lambda: tr("Speed"), lambda: tr("Time"), lambda: tr("All")],
       param="ChevronInfo",
     )
     self._dev_ui_info = multiple_button_item_sp(
-      title=tr("Developer UI"),
-      description=tr("Display real-time parameters and metrics from various sources."),
-      buttons=[tr("Off"), tr("Right"), tr("Right & Bottom")],
+      title=lambda: tr("Developer UI"),
+      description=lambda: tr("Display real-time parameters and metrics from various sources."),
+      buttons=[lambda: tr("Off"), lambda: tr("Right"), lambda: tr("Right & Bottom")],
       param="DevUIInfo",
       button_width=350,
       inline=False
