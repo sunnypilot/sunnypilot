c96facc3d8a45e2c63332bb89ae6c828066d27d5
amoghmunikote <amogh.munikote@gmail.com>
2025-11-27 00:12:33 -0800

    Removed widgets from homescreen

--- selfdrive/ui/layouts/home.py
+++ selfdrive/ui/layouts/home.py
@@ -180,8 +180,9 @@ class HomeLayout(Widget):
     gui_label(version_rect, self._version_text, 48, rl.WHITE, alignment=rl.GuiTextAlignment.TEXT_ALIGN_RIGHT)
 
   def _render_home_content(self):
-    self._render_left_column()
-    self._render_right_column()
+    # Intentionally blank: remove all widgets from the home screen
+    # Keep header (which draws the hoofpilot text on the top-right)
+    return
 
   def _render_update_view(self):
     self.update_alert.render(self.content_rect)
