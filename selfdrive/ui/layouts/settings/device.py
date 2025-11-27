35f10c6ae218a2dd24905a321b01564851fa9eb9
amoghmunikote <amogh.munikote@gmail.com>
2025-11-26 22:49:36 -0800

    Updated konik stable messages in mici and tizi

--- selfdrive/ui/layouts/settings/device.py
+++ selfdrive/ui/layouts/settings/device.py
@@ -21,7 +21,7 @@ from openpilot.system.ui.widgets.scroller_tici import Scroller
 
 # Description constants
 DESCRIPTIONS = {
-  'pair_device': tr_noop("Pair your device with comma connect (connect.comma.ai) and claim your comma prime offer."),
+  'pair_device': tr_noop("Pair your device with Konik Stable (stable.konik.ai)."),
   'driver_camera': tr_noop("Preview the driver facing camera to ensure that driver monitoring has good visibility. (vehicle must be off)"),
   'reset_calibration': tr_noop("hoofpilot requires the device to be mounted within 4° left or right and within 5° up or 9° down."),
   'review_guide': tr_noop("Review the rules, features, and limitations of hoofpilot"),
