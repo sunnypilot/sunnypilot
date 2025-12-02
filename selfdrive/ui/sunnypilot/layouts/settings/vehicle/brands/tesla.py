d5d13c4bc305c4ad44ebb706793723bd3e1bec4b
Jason Wen <haibin.wen3@gmail.com>
2025-12-02 14:35:05 -0500

    do not allow while offroad

--- selfdrive/ui/sunnypilot/layouts/settings/vehicle/brands/tesla.py
+++ selfdrive/ui/sunnypilot/layouts/settings/vehicle/brands/tesla.py
@@ -27,6 +27,7 @@ class TeslaSettings(BrandSettings):
     display_value_coop = COOP_STEERING_MIN_KMH if is_metric else round(COOP_STEERING_MIN_KMH * KM_TO_MILE)
     display_value_oem = OEM_STEERING_MIN_KMH if is_metric else round(OEM_STEERING_MIN_KMH * KM_TO_MILE)
 
+    coop_steering_disabled_msg = tr("Enable \"Always Offroad\" in Device panel, or turn vehicle off to toggle.")
     coop_steering_warning = tr(f"Warning: May experience steering oscillations below {display_value_oem} {unit} during turns, " +
                                "recommend disabling this feature if you experience these.")
     coop_steering_desc = (
@@ -34,6 +35,9 @@ class TeslaSettings(BrandSettings):
       f"{tr('Allows the driver to provide limited steering input while openpilot is engaged.')}<br>" +
       f"{tr(f'Only works above {display_value_coop} {unit}.')}"
     )
+
+    if not ui_state.is_offroad():
+      coop_steering_desc = f"<b>{coop_steering_disabled_msg}</b><br><br>{coop_steering_desc}"
 
     self.coop_steering_toggle.set_description(coop_steering_desc)
     self.coop_steering_toggle.action_item.set_enabled(ui_state.is_offroad())
