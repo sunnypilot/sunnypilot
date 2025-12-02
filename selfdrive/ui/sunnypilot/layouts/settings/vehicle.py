ffee18ff12ba0c078702a2d8b0270c80fe552137
discountchubbs <alexgrant990@gmail.com>
2025-12-01 16:36:41 -0800

    # Conflicts: #	selfdrive/ui/sunnypilot/layouts/settings/steering.py #	selfdrive/ui/sunnypilot/layouts/settings/vehicle.py #	system/ui/sunnypilot/widgets/list_view.py

--- selfdrive/ui/sunnypilot/layouts/settings/vehicle.py
+++ selfdrive/ui/sunnypilot/layouts/settings/vehicle.py
@@ -4,24 +4,37 @@ Copyright (c) 2021-, Haibin Wen, sunnypilot, and a number of other contributors.
 This file is part of sunnypilot and is licensed under the MIT License.
 See the LICENSE.md file in the root directory for more details.
 """
-from openpilot.common.params import Params
-from openpilot.system.ui.widgets.scroller_tici import Scroller
+from openpilot.system.ui.lib.multilang import tr
 from openpilot.system.ui.widgets import Widget
+from openpilot.system.ui.widgets.list_view import ButtonAction
+from openpilot.system.ui.widgets.scroller_tici import Scroller
+
+from openpilot.selfdrive.ui.sunnypilot.layouts.vehicle_settings.platform_selector import PlatformSelector, LegendWidget
+from openpilot.selfdrive.ui.ui_state import ui_state
+from openpilot.system.ui.sunnypilot.widgets.list_view import ListItemSP
 
 
 class VehicleLayout(Widget):
   def __init__(self):
     super().__init__()
+    self._platform_selector = PlatformSelector(self._update_brand_settings)
 
-    self._params = Params()
-    items = self._initialize_items()
-    self._scroller = Scroller(items, line_separator=True, spacing=0)
+    self._vehicle_item = ListItemSP(title=self._platform_selector.text, action_item=ButtonAction(text=tr("Select")),
+                                    callback=self._platform_selector._on_clicked)
+    self._vehicle_item.title_color = self._platform_selector.color
+    self._legend_widget = LegendWidget(self._platform_selector)
 
-  def _initialize_items(self):
-    items = [
+    self.items = [self._vehicle_item, self._legend_widget]
+    self._scroller = Scroller(self.items, line_separator=True, spacing=0)
 
-    ]
-    return items
+  def _update_brand_settings(self):
+    self._vehicle_item._title = self._platform_selector.text
+    self._vehicle_item.title_color = self._platform_selector.color
+    vehicle_text = tr("Remove") if ui_state.params.get("CarPlatformBundle") else tr("Select")
+    self._vehicle_item.action_item.set_text(vehicle_text)
+
+  def _update_state(self):
+    self._update_brand_settings()
 
   def _render(self, rect):
     self._scroller.render(rect)
