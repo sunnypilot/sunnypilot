512c070dd625ae6ce5a1eec0f1d7b60da72c2cbc
Jason Wen <haibin.wen3@gmail.com>
2025-12-05 00:04:03 -0500

    colored sponsors

--- selfdrive/ui/sunnypilot/layouts/settings/sunnylink.py
+++ selfdrive/ui/sunnypilot/layouts/settings/sunnylink.py
@@ -326,8 +326,9 @@ class SunnylinkLayout(Widget):
     self.handle_backup_restore_progress()
 
     sponsor_btn_text = tr("THANKS ♥") if ui_state.sunnylink_state.is_sponsor() else tr("SPONSOR")
+    tier_name = ui_state.sunnylink_state.get_sponsor_tier().name.capitalize() or tr("Not Sponsor")
     self._sponsor_btn.action_item.set_text(sponsor_btn_text)
-    self._sponsor_btn.action_item.set_value(ui_state.sunnylink_state.get_sponsor_tier().name.capitalize() or tr("Not Sponsor"))
+    self._sponsor_btn.action_item.set_value(tier_name, ui_state.sunnylink_state.get_sponsor_tier_color())
     self._sponsor_btn.action_item.set_enabled(self._sunnylink_enabled and not ui_state.sunnylink_state.is_sponsor())
 
     pair_btn_text = tr("Paired") if ui_state.sunnylink_state.is_paired() else tr("Not Paired")
