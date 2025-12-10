ff3276119db8e5d19c5803d80311a08ae868f7fd
Christopher Haucke <132518562+CHaucke89@users.noreply.github.com>
2025-12-10 14:22:50 -0500

    Delay lateral reengagement

--- sunnypilot/selfdrive/controls/lib/blinker_pause_lateral.py
+++ sunnypilot/selfdrive/controls/lib/blinker_pause_lateral.py
@@ -17,13 +17,17 @@ class BlinkerPauseLateral:
     self.enabled = self.params.get_bool("BlinkerPauseLateralControl")
     self.is_metric = self.params.get_bool("IsMetric")
     self.min_speed = 0
+    self.reengage_delay = int(self.params.get("BlinkerLateralReengageDelay"))
+    self.blinker_off_timer = 0.0
+    self.prev_one_blinker = False
 
   def get_params(self) -> None:
     self.enabled = self.params.get_bool("BlinkerPauseLateralControl")
     self.is_metric = self.params.get_bool("IsMetric")
     self.min_speed = self.params.get("BlinkerMinLateralControlSpeed")
+    self.reengage_delay = int(self.params.get("BlinkerLateralReengageDelay"))
 
-  def update(self, CS: car.CarState) -> bool:
+  def update(self, CS: car.CarState, DT_CTRL: float = 0.01) -> bool:
     if not self.enabled:
       return False
 
@@ -31,4 +35,13 @@ class BlinkerPauseLateral:
     speed_factor = CV.KPH_TO_MS if self.is_metric else CV.MPH_TO_MS
     min_speed_ms = self.min_speed * speed_factor
 
-    return bool(one_blinker and CS.vEgo < min_speed_ms)
+    below_speed = CS.vEgo < min_speed_ms
+
+    if one_blinker and below_speed:
+      self.blinker_off_timer = self.reengage_delay
+    elif self.blinker_off_timer > 0:
+      self.blinker_off_timer -= DT_CTRL
+
+    self.prev_one_blinker = one_blinker
+
+    return bool((one_blinker and below_speed) or self.blinker_off_timer > 0)
