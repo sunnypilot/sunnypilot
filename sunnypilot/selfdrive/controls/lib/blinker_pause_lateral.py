844f160035cfa38bddb2c84c8ad94025e7e33f06
Christopher Haucke <132518562+CHaucke89@users.noreply.github.com>
2025-12-12 06:59:49 -0500

    Sentry wants an int

--- sunnypilot/selfdrive/controls/lib/blinker_pause_lateral.py
+++ sunnypilot/selfdrive/controls/lib/blinker_pause_lateral.py
@@ -24,7 +24,7 @@ class BlinkerPauseLateral:
   def get_params(self) -> None:
     self.enabled = self.params.get_bool("BlinkerPauseLateralControl")
     self.is_metric = self.params.get_bool("IsMetric")
-    self.min_speed = self.params.get("BlinkerMinLateralControlSpeed")
+    self.min_speed = int(self.params.get("BlinkerMinLateralControlSpeed"))
     self.reengage_delay = int(self.params.get("BlinkerLateralReengageDelay"))
 
   def update(self, CS: car.CarState, DT_CTRL: float = 0.01) -> bool:
