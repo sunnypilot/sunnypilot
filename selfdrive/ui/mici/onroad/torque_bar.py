5578b7e7540e194b36e78a8033aa7964875d3af7
royjr <royjr96@gmail.com>
2025-12-19 14:36:10 -0500

    ui: lateral-only and longitudinal-only UI statuses support (#1539)
    * init
    
    * add only colors
    
    * fix LAT_ONLY on mici
    
    * better ball
    
    * hide wheel on LONG_ONLY
    
    * hide torquebar on LONG_ONLY
    
    * simpler
    
    * dont block demo
    
    * path only on long
    
    * lanelines only on lat
    
    * hide on override
    
    * better
    
    * same LANE_LINE_COLORS for mads
    
    * use mads colors
    
    * Revert "use mads colors"
    
    This reverts commit 556321e5debe44e33d4ad98f440f0ed9f961fdf5.
    
    * slight decouple confidence ball
    
    * slight decouple model renderer
    
    * slight decouple augmented road view
    
    * decouple status update
    
    * decouple and override with our own, no overriding with steering if long only
    
    * fix
    
    * fix it
    
    ---------
    
    Co-authored-by: Jason Wen <haibin.wen3@gmail.com>

--- selfdrive/ui/mici/onroad/torque_bar.py
+++ selfdrive/ui/mici/onroad/torque_bar.py
@@ -185,13 +185,13 @@ class TorqueBar(Widget):
 
     # animate alpha and angle span
     if not self._demo:
-      self._torque_line_alpha_filter.update(ui_state.status != UIStatus.DISENGAGED)
+      self._torque_line_alpha_filter.update(ui_state.status not in (UIStatus.DISENGAGED, UIStatus.LONG_ONLY))
     else:
       self._torque_line_alpha_filter.update(1.0)
 
     torque_line_bg_alpha = np.interp(abs(self._torque_filter.x), [0.5, 1.0], [0.25, 0.5])
     torque_line_bg_color = rl.Color(255, 255, 255, int(255 * torque_line_bg_alpha * self._torque_line_alpha_filter.x))
-    if ui_state.status != UIStatus.ENGAGED and not self._demo:
+    if ui_state.status not in (UIStatus.ENGAGED, UIStatus.LAT_ONLY) and not self._demo:
       torque_line_bg_color = rl.Color(255, 255, 255, int(255 * 0.15 * self._torque_line_alpha_filter.x))
 
     # draw curved line polygon torque bar
@@ -234,7 +234,7 @@ class TorqueBar(Widget):
       max(0, abs(self._torque_filter.x) - 0.75) * 4,
     )
 
-    if ui_state.status != UIStatus.ENGAGED and not self._demo:
+    if ui_state.status not in (UIStatus.ENGAGED, UIStatus.LAT_ONLY) and not self._demo:
       start_color = end_color = rl.Color(255, 255, 255, int(255 * 0.35 * self._torque_line_alpha_filter.x))
 
     gradient = Gradient(
