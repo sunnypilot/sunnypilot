e8d34f9c8dd60c5b7430457a61dfba107124113c
royjr <royjr96@gmail.com>
2025-12-21 18:36:11 -0500

    Revert "fix"
    This reverts commit e380d119117d0d3bc38761c6a568e7d630d709fb.

--- selfdrive/selfdrived/events.py
+++ selfdrive/selfdrived/events.py
@@ -800,7 +800,6 @@ EVENTS: dict[int, dict[str, Alert | AlertCallbackType]] = {
       AlertStatus.normal, AlertSize.full,
       Priority.LOWEST, VisualAlert.none, AudibleAlert.none, .2, creation_delay=0.5),
     ET.NO_ENTRY: NoEntryAlert("Neutral Gear"),
-  },
 
   # On cars that use stock ACC the car can decide to cancel ACC for various reasons.
   # When this happens we can no long control the car so the user needs to be warned immediately.
