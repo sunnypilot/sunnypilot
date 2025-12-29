0881c814d92c002e6fd2757f3ce69f979ab8920f
royjr <royjr96@gmail.com>
2025-12-29 01:51:24 -0500

    simpler

--- selfdrive/ui/mici/onroad/augmented_road_view.py
+++ selfdrive/ui/mici/onroad/augmented_road_view.py
@@ -164,14 +164,6 @@ class AugmentedRoadView(CameraView):
     # debug
     self._pm = messaging.PubMaster(['uiDebug'])
 
-    self._hide_camera = False
-
-  def show_event(self):
-    self._update_params()
-
-  def _update_params(self):
-    self._hide_camera = ui_state.params.get_bool("HideCamera")
-
   def is_swiping_left(self) -> bool:
     """Check if currently swiping left (for scroller to disable)."""
     return self._bookmark_icon.is_swiping_left()
@@ -215,16 +207,9 @@ class AugmentedRoadView(CameraView):
     )
 
     # Render the base camera view
-    if self._hide_camera:
-      if self._switching:
-        self._handle_switch()
-      self._ensure_connection()
-      if self.client:
-        self.client.recv(timeout_ms=0)
-      self._calc_frame_matrix(self._content_rect)
+    super()._render(self._content_rect)
+    if ui_state.hide_camera:
       rl.draw_rectangle_rec(self._content_rect, rl.BLACK)
-    else:
-      super()._render(self._content_rect)
 
     # Draw all UI overlays
     self._model_renderer.render(self._content_rect)
