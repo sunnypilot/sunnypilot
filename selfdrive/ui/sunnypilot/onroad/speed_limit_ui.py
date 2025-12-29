ffaf0a990e4770cee6813e7d11886fe988d63b63
rav4kumar <meetkumardesai@gmail.com>
2025-12-29 13:52:55 -0700

    sunny will be mad
    Reapply "test"
    
    This reverts commit 7a35fd3053425b06c1e4cebf68b6c9676eb6e279.
    
    Revert "test"
    
    This reverts commit 1a79155d3cfc8e6e09a6fc38d13747883745ef6a.
    
    test

--- selfdrive/ui/sunnypilot/onroad/speed_limit_ui.py
+++ selfdrive/ui/sunnypilot/onroad/speed_limit_ui.py
@@ -1,10 +1,10 @@
-import math
-import os
 from enum import IntEnum
 from dataclasses import dataclass
+import math
 import pyray as rl
 
 from openpilot.common.constants import CV
+from openpilot.common.filter_simple import FirstOrderFilter
 from openpilot.selfdrive.ui.ui_state import ui_state
 from openpilot.system.ui.lib.application import gui_app, FontWeight
 from openpilot.system.ui.lib.multilang import tr
@@ -16,6 +16,7 @@ METER_TO_MILE = 0.000621371
 AHEAD_THRESHOLD = 5
 UI_FREQ = 20
 
+
 @dataclass(frozen=True)
 class Colors:
   WHITE = rl.WHITE
@@ -26,17 +27,16 @@ class Colors:
   SUB_BG = rl.Color(0, 0, 0, 180)
   MUTCD_LINES = rl.Color(255, 255, 255, 100)
 
+
 class SpeedLimitMode(IntEnum):
   OFF = 0
   WARNING = 1
   ASSIST = 2
 
+
 class SpeedLimitRenderer(Widget):
   def __init__(self):
     super().__init__()
-    asset_path = os.path.join(os.path.dirname(__file__), "../assets")
-    self.arrow_up = self._load_tex(asset_path, "img_plus_arrow_up.png")
-    self.arrow_down = self._load_tex(asset_path, "img_minus_arrow_down.png")
 
     self.speed_limit = 0.0
     self.speed_limit_last = 0.0
@@ -58,19 +58,19 @@ class SpeedLimitRenderer(Widget):
     self.speed = 0.0
     self.set_speed = 0.0
     self.is_metric = False
-    self.road_name = ""
 
     self.font_bold = gui_app.font(FontWeight.BOLD)
     self.font_demi = gui_app.font(FontWeight.SEMI_BOLD)
     self.font_norm = gui_app.font(FontWeight.NORMAL)
+    self._sign_alpha_filter = FirstOrderFilter(1.0, 0.5, 1 / UI_FREQ)
+
+    arrow_size = 90
+    self._arrow_up = gui_app.texture("../../sunnypilot/selfdrive/assets/img_plus_arrow_up.png", arrow_size, arrow_size)
+    self._arrow_down = gui_app.texture("../../sunnypilot/selfdrive/assets/img_minus_arrow_down.png", arrow_size, arrow_size)
 
   @property
   def speed_conv(self):
     return CV.MS_TO_KPH if self.is_metric else CV.MS_TO_MPH
-
-  def _load_tex(self, base, name):
-    path = os.path.join(base, name)
-    return rl.load_texture(path.encode()) if os.path.exists(path) else None
 
   def _update_state(self):
     sm = ui_state.sm
@@ -98,7 +98,6 @@ class SpeedLimitRenderer(Widget):
       self.speed_limit_ahead_valid = lmd.speedLimitAheadValid
       self.speed_limit_ahead = lmd.speedLimitAhead * conv
       self.speed_limit_ahead_dist = lmd.speedLimitAheadDistance
-      self.road_name = lmd.roadName
 
       if self.speed_limit_ahead_dist < self.speed_limit_ahead_dist_prev and self.speed_limit_ahead_frame < AHEAD_THRESHOLD:
         self.speed_limit_ahead_frame += 1
@@ -114,31 +113,31 @@ class SpeedLimitRenderer(Widget):
 
   def _draw_text_centered(self, font, text, size, pos_center, color):
     sz = measure_text_cached(font, text, size)
-    origin = rl.Vector2(pos_center.x - sz.x/2, pos_center.y - sz.y/2)
-    rl.draw_text_ex(font, text, origin, size, 0, color)
+    rl.draw_text_ex(font, text, rl.Vector2(pos_center.x - sz.x / 2, pos_center.y - sz.y / 2), size, 0, color)
 
   def _render(self, rect: rl.Rectangle):
     w, h = (200, 204) if self.is_metric else (172, 204)
     x, y = rect.x + (298 if self.is_metric else 290), rect.y + 50
     sign_rect = rl.Rectangle(x, y, w, h)
 
-    show_sign = True
     if self.speed_limit_assist_state == 1:
       self.assist_frame += 1
-      show_sign = (self.assist_frame % UI_FREQ) >= (UI_FREQ / 2.5)
-      self._draw_pre_active_arrow(sign_rect)
+      pulse_value = 0.65 + 0.35 * math.sin(self.assist_frame * math.pi / UI_FREQ)
+      alpha = self._sign_alpha_filter.update(pulse_value)
+      show_sign = True
     else:
       self.assist_frame = 0
+      alpha = self._sign_alpha_filter.update(1.0)
       show_sign = self.speed_limit_assist_active or self.speed_limit_valid or self.speed_limit_last_valid
 
     if show_sign:
-      self._draw_sign_main(sign_rect)
-      if self.speed_limit_assist_state != 1:
+      self._draw_sign_main(sign_rect, alpha)
+      if self.speed_limit_assist_state == 1:
+        self._draw_pre_active_arrow(sign_rect)
+      else:
         self._draw_ahead_info(sign_rect)
 
-    self._draw_road_name(rect)
-
-  def _draw_sign_main(self, rect):
+  def _draw_sign_main(self, rect, alpha=1.0):
     has_limit = self.speed_limit_valid or self.speed_limit_last_valid
     is_overspeed = has_limit and round(self.speed_limit_final_last) < round(self.speed)
 
@@ -155,117 +154,111 @@ class SpeedLimitRenderer(Widget):
       txt_color = Colors.GREY
 
     if self.is_metric:
-      self._render_vienna(rect, limit_str, sub_text, txt_color, has_limit)
+      self._render_vienna(rect, limit_str, sub_text, txt_color, has_limit, alpha)
     else:
-      self._render_mutcd(rect, limit_str, sub_text, txt_color, has_limit)
+      self._render_mutcd(rect, limit_str, sub_text, txt_color, has_limit, alpha)
 
-  def _render_vienna(self, rect, val, sub, color, has_limit):
-    center = rl.Vector2(rect.x + rect.width/2, rect.y + rect.height/2)
+  def _draw_pre_active_arrow(self, sign_rect):
+    set_speed_rounded = round(self.set_speed)
+    limit_rounded = round(self.speed_limit_final_last)
+
+    bounce_frequency = 2.0 * math.pi / (UI_FREQ * 2.5)
+    bounce_offset = int(20 * math.sin(self.assist_frame * bounce_frequency))
+
+    sign_margin = 12
+    arrow_spacing = int(sign_margin * 1.4)
+    arrow_x = sign_rect.x + sign_rect.width + arrow_spacing
+
+    if set_speed_rounded < limit_rounded:
+      arrow_y = sign_rect.y + (sign_rect.height - self._arrow_up.height) / 2 + bounce_offset
+      rl.draw_texture(self._arrow_up, int(arrow_x), int(arrow_y), rl.WHITE)
+    elif set_speed_rounded > limit_rounded:
+      arrow_y = sign_rect.y + (sign_rect.height - self._arrow_down.height) / 2 - bounce_offset
+      rl.draw_texture(self._arrow_down, int(arrow_x), int(arrow_y), rl.WHITE)
+
+  def _render_vienna(self, rect, val, sub, color, has_limit, alpha=1.0):
+    center = rl.Vector2(rect.x + rect.width / 2, rect.y + rect.height / 2)
     radius = rect.width / 2
 
-    rl.draw_circle_v(center, radius, Colors.WHITE)
-    rl.draw_ring(center, radius * 0.88, radius, 0, 360, 36, Colors.RED)
+    white = rl.Color(255, 255, 255, int(255 * alpha))
+    red = rl.Color(255, 0, 0, int(255 * alpha))
+
+    if hasattr(color, 'r'):
+      text_color = rl.Color(color.r, color.g, color.b, int(255 * alpha))
+    else:
+      text_color = rl.Color(color[0], color[1], color[2], int(255 * alpha))
+
+    black = rl.Color(0, 0, 0, int(255 * alpha))
+    dark_grey = rl.Color(77, 77, 77, int(255 * alpha))
+
+    rl.draw_circle_v(center, radius, white)
+    rl.draw_ring(center, radius * 0.88, radius, 0, 360, 36, red)
 
     f_size = 70 if len(val) >= 3 else 85
-    self._draw_text_centered(self.font_bold, val, f_size, center, color)
+    self._draw_text_centered(self.font_bold, val, f_size, center, text_color)
 
     if sub and has_limit:
       s_radius = radius * 0.4
-      s_center = rl.Vector2(rect.x + rect.width - s_radius/1.25, rect.y + s_radius/2)
+      s_center = rl.Vector2(rect.x + rect.width - s_radius / 1.25, rect.y + s_radius / 2)
 
-      rl.draw_circle_v(s_center, s_radius, Colors.BLACK)
-      rl.draw_ring(s_center, s_radius - 3, s_radius, 0, 360, 36, Colors.DARK_GREY)
+      rl.draw_circle_v(s_center, s_radius, black)
+      rl.draw_ring(s_center, s_radius - 3, s_radius, 0, 360, 36, dark_grey)
 
       f_scale = 0.5 if len(sub) < 3 else 0.45
-      self._draw_text_centered(self.font_bold, sub, int(s_radius * 2 * f_scale), s_center, Colors.WHITE)
+      self._draw_text_centered(self.font_bold, sub, int(s_radius * 2 * f_scale), s_center, white)
 
-  def _render_mutcd(self, rect, val, sub, color, has_limit):
-    rl.draw_rectangle_rounded(rect, 0.16, 12, Colors.WHITE)
+  def _render_mutcd(self, rect, val, sub, color, has_limit, alpha=1.0):
+    white = rl.Color(255, 255, 255, int(255 * alpha))
+    black = rl.Color(0, 0, 0, int(255 * alpha))
+    dark_grey = rl.Color(77, 77, 77, int(255 * alpha))
+
+    if hasattr(color, 'r'):
+      text_color = rl.Color(color.r, color.g, color.b, int(255 * alpha))
+    else:
+      text_color = rl.Color(color[0], color[1], color[2], int(255 * alpha))
+
+    rl.draw_rectangle_rounded(rect, 0.16, 12, white)
     inner = rl.Rectangle(rect.x + 10, rect.y + 10, rect.width - 20, rect.height - 20)
-    rl.draw_rectangle_rounded_lines_ex(inner, 0.1, 12, 4, Colors.BLACK)
+    rl.draw_rectangle_rounded_lines_ex(inner, 0.1, 12, 4, black)
 
-    self._draw_text_centered(self.font_demi, "SPEED", 40, rl.Vector2(rect.x + rect.width/2, rect.y + 40), Colors.BLACK)
-    self._draw_text_centered(self.font_demi, "LIMIT", 40, rl.Vector2(rect.x + rect.width/2, rect.y + 80), Colors.BLACK)
-    self._draw_text_centered(self.font_bold, val, 90, rl.Vector2(rect.x + rect.width/2, rect.y + 135), color)
+    self._draw_text_centered(self.font_demi, "SPEED", 40, rl.Vector2(rect.x + rect.width / 2, rect.y + 40), black)
+    self._draw_text_centered(self.font_demi, "LIMIT", 40, rl.Vector2(rect.x + rect.width / 2, rect.y + 80), black)
+    self._draw_text_centered(self.font_bold, val, 90, rl.Vector2(rect.x + rect.width / 2, rect.y + 135), text_color)
 
     if sub and has_limit:
       box_sz = rect.width * 0.3
       overlap = box_sz * 0.2
-      s_rect = rl.Rectangle(rect.x + rect.width - box_sz/1.5 + overlap, rect.y - box_sz/1.25 + overlap, box_sz, box_sz)
+      s_rect = rl.Rectangle(rect.x + rect.width - box_sz / 1.5 + overlap, rect.y - box_sz / 1.25 + overlap, box_sz, box_sz)
 
-      rl.draw_rectangle_rounded(s_rect, 0.2, 8, Colors.BLACK)
-      rl.draw_rectangle_rounded_lines_ex(s_rect, 0.2, 8, 6, Colors.DARK_GREY)
+      rl.draw_rectangle_rounded(s_rect, 0.2, 8, black)
+      rl.draw_rectangle_rounded_lines_ex(s_rect, 0.2, 8, 6, dark_grey)
 
       f_scale = 0.6 if len(sub) < 3 else 0.475
-      self._draw_text_centered(self.font_bold, sub, int(box_sz * f_scale),
-                               rl.Vector2(s_rect.x + box_sz/2, s_rect.y + box_sz/2), Colors.WHITE)
+      self._draw_text_centered(self.font_bold, sub, int(box_sz * f_scale), rl.Vector2(s_rect.x + box_sz / 2, s_rect.y + box_sz / 2), white)
 
   def _draw_ahead_info(self, sign_rect):
     source_is_map = self.speed_limit_source == 0 or str(self.speed_limit_source).lower() == "map"
-    valid = (self.speed_limit_ahead_valid and
-             self.speed_limit_ahead > 0 and
-             self.speed_limit_ahead != self.speed_limit)
+    valid = self.speed_limit_ahead_valid and self.speed_limit_ahead > 0 and self.speed_limit_ahead != self.speed_limit
 
     if not (valid and source_is_map):
       return
 
-    rect = rl.Rectangle(sign_rect.x + (sign_rect.width - 170)/2, sign_rect.y + sign_rect.height + 10, 170, 160)
+    rect = rl.Rectangle(sign_rect.x + (sign_rect.width - 170) / 2, sign_rect.y + sign_rect.height + 10, 170, 160)
     rl.draw_rectangle_rounded(rect, 0.1, 10, Colors.SUB_BG)
     rl.draw_rectangle_rounded_lines_ex(rect, 0.1, 10, 3, Colors.MUTCD_LINES)
 
-    mid_x = rect.x + rect.width/2
+    mid_x = rect.x + rect.width / 2
     self._draw_text_centered(self.font_demi, "AHEAD", 40, rl.Vector2(mid_x, rect.y + 24), Colors.GREY)
     self._draw_text_centered(self.font_bold, str(round(self.speed_limit_ahead)), 70, rl.Vector2(mid_x, rect.y + 75), Colors.WHITE)
     self._draw_text_centered(self.font_norm, self._format_dist(self.speed_limit_ahead_dist), 40, rl.Vector2(mid_x, rect.y + 130), Colors.GREY)
 
-  def _draw_pre_active_arrow(self, rect):
-    if not self.arrow_up or not self.arrow_down:
-      return
-
-    bounce = 20 * math.sin(self.assist_frame * (2.0 * math.pi / UI_FREQ))
-    x = rect.x + rect.width + 16
-    y = rect.y + rect.height / 2 - 45
-
-    tex = self.arrow_up if self.set_speed < self.speed_limit_final_last else self.arrow_down
-    direction = 1 if self.set_speed < self.speed_limit_final_last else -1
-    rl.draw_texture_ex(tex, rl.Vector2(x, y + (bounce * direction)), 0, 1.0, Colors.WHITE)
-
-  def _draw_road_name(self, rect: rl.Rectangle):
-    if not self.road_name:
-      return
-
-    text = self.road_name
-    text_size = measure_text_cached(self.font_demi, text, 46)
-
-    padding = 40
-    rect_width = max(200, min(text_size.x + padding, rect.width - 40))
-
-    road_rect = rl.Rectangle(
-      rect.x + rect.width / 2 - rect_width / 2,
-      rect.y - 4,
-      rect_width,
-      60
-    )
-
-    rl.draw_rectangle_rounded(road_rect, 0.2, 10, rl.Color(0, 0, 0, 120))
-
-    max_text_width = road_rect.width - 20
-    if text_size.x > max_text_width:
-      while text_size.x > max_text_width and len(text) > 3:
-        text = text[:-1]
-        text_size = measure_text_cached(self.font_demi, text + "...", 46)
-      text = text + "..."
-
-    self._draw_text_centered(self.font_demi, text, 46,
-                             rl.Vector2(road_rect.x + road_rect.width/2,
-                                        road_rect.y + road_rect.height/2),
-                             rl.Color(255, 255, 255, 200))
-
   def _format_dist(self, d):
     if self.is_metric:
-      if d < 50: return tr("Near")
-      return f"{d/1000:.1f} km" if d >= 1000 else f"{round(d, -1 if d < 200 else -2)} m"
+      if d < 50:
+        return tr("Near")
+      return f"{d / 1000:.1f} km" if d >= 1000 else f"{round(d, -1 if d < 200 else -2)} m"
 
     d_ft = d * METER_TO_FOOT
-    if d_ft < 100: return tr("Near")
+    if d_ft < 100:
+      return tr("Near")
     return f"{d * METER_TO_MILE:.1f} mi" if d_ft >= 900 else f"{round(d_ft, -1 if d_ft < 500 else -2)} ft"
