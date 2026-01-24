8c36739ebd346f3a52e89978f4dcf657841b61da
Kumar <36933347+rav4kumar@users.noreply.github.com>
2026-01-23 21:27:12 -0700

    [TIZI/TICI] ui: Rocket Fuel (#1337)
    * rocket
    
    * raylib
    
    * extra
    
    ---------
    
    Co-authored-by: Jason Wen <haibin.wen3@gmail.com>

--- selfdrive/ui/sunnypilot/onroad/hud_renderer.py
+++ selfdrive/ui/sunnypilot/onroad/hud_renderer.py
@@ -6,15 +6,20 @@ See the LICENSE.md file in the root directory for more details.
 """
 import pyray as rl
 
+from openpilot.selfdrive.ui.ui_state import ui_state
 from openpilot.selfdrive.ui.onroad.hud_renderer import HudRenderer
 from openpilot.selfdrive.ui.sunnypilot.onroad.developer_ui import DeveloperUiRenderer
+from openpilot.selfdrive.ui.sunnypilot.onroad.rocket_fuel import RocketFuel
 
 
 class HudRendererSP(HudRenderer):
   def __init__(self):
     super().__init__()
     self.developer_ui = DeveloperUiRenderer()
+    self.rocket_fuel = RocketFuel()
 
   def _render(self, rect: rl.Rectangle) -> None:
     super()._render(rect)
     self.developer_ui.render(rect)
+    if ui_state.rocket_fuel:
+      self.rocket_fuel.render(rect, ui_state.sm)
