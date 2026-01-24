5c01365125c1b1fff9239a8cde5f9482e3265022
Kumar <36933347+rav4kumar@users.noreply.github.com>
2026-01-24 00:04:42 -0700

    [TIZI/TICI] ui: Road Name (#1654)
    * road name
    
    * decouple
    
    * rename
    
    ---------
    
    Co-authored-by: Jason Wen <haibin.wen3@gmail.com>

--- selfdrive/ui/sunnypilot/onroad/hud_renderer.py
+++ selfdrive/ui/sunnypilot/onroad/hud_renderer.py
@@ -9,6 +9,7 @@ import pyray as rl
 from openpilot.selfdrive.ui.ui_state import ui_state
 from openpilot.selfdrive.ui.onroad.hud_renderer import HudRenderer
 from openpilot.selfdrive.ui.sunnypilot.onroad.developer_ui import DeveloperUiRenderer
+from openpilot.selfdrive.ui.sunnypilot.onroad.road_name import RoadNameRenderer
 from openpilot.selfdrive.ui.sunnypilot.onroad.rocket_fuel import RocketFuel
 from openpilot.selfdrive.ui.sunnypilot.onroad.turn_signal import TurnSignalController
 
@@ -17,17 +18,20 @@ class HudRendererSP(HudRenderer):
   def __init__(self):
     super().__init__()
     self.developer_ui = DeveloperUiRenderer()
+    self.road_name_renderer = RoadNameRenderer()
     self.rocket_fuel = RocketFuel()
     self.turn_signal_controller = TurnSignalController()
 
   def _update_state(self) -> None:
     super()._update_state()
+    self.road_name_renderer.update()
     self.turn_signal_controller.update()
 
   def _render(self, rect: rl.Rectangle) -> None:
     super()._render(rect)
     self.developer_ui.render(rect)
 
+    self.road_name_renderer.render(rect)
     self.turn_signal_controller.render()
 
     if ui_state.rocket_fuel:
