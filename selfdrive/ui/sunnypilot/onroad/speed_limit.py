26b7473f0486f5764fa418ec3332818ec2dace2c
Jason Wen <haibin.wen3@gmail.com>
2026-01-26 01:16:40 -0500

    more cleanup

--- selfdrive/ui/sunnypilot/onroad/speed_limit.py
+++ selfdrive/ui/sunnypilot/onroad/speed_limit.py
@@ -4,7 +4,6 @@ Copyright (c) 2021-, Haibin Wen, sunnypilot, and a number of other contributors.
 This file is part of sunnypilot and is licensed under the MIT License.
 See the LICENSE.md file in the root directory for more details.
 """
-from enum import IntEnum
 from dataclasses import dataclass
 import math
 import pyray as rl
