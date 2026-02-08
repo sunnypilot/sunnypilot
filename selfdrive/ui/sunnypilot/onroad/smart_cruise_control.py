a07695fc080c78ea693ea481b2f2823a89b374f5
Jason Wen <haibin.wen3@gmail.com>
2026-02-08 18:09:53 -0500

    punch

--- selfdrive/ui/sunnypilot/onroad/smart_cruise_control.py
+++ selfdrive/ui/sunnypilot/onroad/smart_cruise_control.py
@@ -24,6 +24,7 @@ class SmartCruiseControlRenderer(Widget):
     self.long_override = False
 
     self.font = gui_app.font(FontWeight.BOLD)
+    self.scc_tex = rl.load_render_texture(256, 128)
 
   def update(self):
     sm = ui_state.sm
@@ -50,13 +51,11 @@ class SmartCruiseControlRenderer(Widget):
     else:
       self.map_frame = 0
 
-  def _pulse_element(self, frame):
+  @staticmethod
+  def _pulse_element(frame):
     return not (frame % gui_app.target_fps < (gui_app.target_fps / 2.5))
 
   def _draw_icon(self, rect_center_x, rect_height, x_offset, y_offset, name):
-    x = rect_center_x
-    y = rect_height / 4
-
     text = name
     font_size = 36
     padding_v = 5
@@ -65,25 +64,44 @@ class SmartCruiseControlRenderer(Widget):
     sz = measure_text_cached(self.font, text, font_size)
     box_height = int(sz.y + padding_v * 2)
 
-    bg_rect = rl.Rectangle(
-        x - (box_width / 2) + x_offset,
-        y - (box_height / 2) + y_offset,
-        box_width,
-        box_height
-    )
+    texture_width = 256
+    texture_height = 128
+
+    rl.begin_texture_mode(self.scc_tex)
+    rl.clear_background(rl.Color(0, 0, 0, 0))
 
     if self.long_override:
-        box_color = rl.Color(145, 155, 149, 241)
+      box_color = rl.Color(145, 155, 149, 241)
     else:
-        box_color = rl.Color(0, 255, 0, 255)
+      box_color = rl.Color(0, 255, 0, 255)
 
-    rl.draw_rectangle_rounded(bg_rect, 0.2, 10, box_color)
+    # Center box in texture
+    box_x = (texture_width - box_width) // 2
+    box_y = (texture_height - box_height) // 2
 
-    text_pos_x = bg_rect.x + (bg_rect.width - sz.x) / 2
-    text_pos_y = bg_rect.y + (bg_rect.height - sz.y) / 2
+    rl.draw_rectangle_rounded(rl.Rectangle(box_x, box_y, box_width, box_height), 0.2, 10, box_color)
 
-    # Using Black text nicely simulates "subtracted" text given high contrast with Green/Grey
-    rl.draw_text_ex(self.font, text, rl.Vector2(text_pos_x, text_pos_y), font_size, 0, rl.BLACK)
+    # Draw text with custom blend mode to punch hole
+    rl.rl_set_blend_factors(rl.RL_ZERO, rl.RL_ONE_MINUS_SRC_ALPHA, 0x8006)
+    rl.rl_set_blend_mode(rl.BLEND_CUSTOM)
+
+    text_pos_x = box_x + (box_width - sz.x) / 2
+    text_pos_y = box_y + (box_height - sz.y) / 2
+
+    rl.draw_text_ex(self.font, text, rl.Vector2(text_pos_x, text_pos_y), font_size, 0, rl.WHITE)
+
+    rl.rl_set_blend_mode(rl.BLEND_ALPHA)  # Reset
+    rl.end_texture_mode()
+
+    screen_y = rect_height / 4 + y_offset
+
+    dest_x = rect_center_x + x_offset - texture_width / 2
+    dest_y = screen_y - texture_height / 2
+
+    src_rect = rl.Rectangle(0, 0, texture_width, -texture_height)
+    dst_rect = rl.Rectangle(dest_x, dest_y, texture_width, texture_height)
+
+    rl.draw_texture_pro(self.scc_tex.texture, src_rect, dst_rect, rl.Vector2(0, 0), 0, rl.WHITE)
 
   def _render(self, rect: rl.Rectangle):
     x_offset = -260
