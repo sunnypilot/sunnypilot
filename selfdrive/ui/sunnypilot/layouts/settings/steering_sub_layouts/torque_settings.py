426852e329b4ee983bcd92e9411d4679f9208917
Jason Wen <haibin.wen3@gmail.com>
2026-02-13 15:09:15 -0500

    more

--- selfdrive/ui/sunnypilot/layouts/settings/steering_sub_layouts/torque_settings.py
+++ selfdrive/ui/sunnypilot/layouts/settings/steering_sub_layouts/torque_settings.py
@@ -4,15 +4,23 @@ Copyright (c) 2021-, Haibin Wen, sunnypilot, and a number of other contributors.
 This file is part of sunnypilot and is licensed under the MIT License.
 See the LICENSE.md file in the root directory for more details.
 """
+import json
+import math
+import os
 from collections.abc import Callable
 import pyray as rl
 
+from openpilot.common.basedir import BASEDIR
 from openpilot.selfdrive.ui.ui_state import ui_state
+from openpilot.system.ui.lib.application import gui_app, FontWeight
 from openpilot.system.ui.lib.multilang import tr
-from openpilot.system.ui.sunnypilot.widgets.list_view import toggle_item_sp, option_item_sp
+from openpilot.system.ui.sunnypilot.widgets.list_view import toggle_item_sp, option_item_sp, button_item_sp
+from openpilot.system.ui.widgets import Widget, DialogResult
 from openpilot.system.ui.widgets.network import NavButton
+from openpilot.system.ui.widgets.option_dialog import MultiOptionDialog
 from openpilot.system.ui.widgets.scroller_tici import Scroller
-from openpilot.system.ui.widgets import Widget
+
+TORQUE_VERSIONS_PATH = os.path.join(BASEDIR, "sunnypilot", "selfdrive", "controls", "lib", "latcontrol_torque_versions.json")
 
 
 class TorqueSettingsLayout(Widget):
@@ -20,10 +28,16 @@ class TorqueSettingsLayout(Widget):
     super().__init__()
     self._back_button = NavButton(tr("Back"))
     self._back_button.set_click_callback(back_btn_callback)
+    self._torque_version_dialog: MultiOptionDialog | None = None
     items = self._initialize_items()
     self._scroller = Scroller(items, line_separator=True, spacing=0)
 
   def _initialize_items(self):
+    _torque_control_versions = button_item_sp(
+        title=lambda: tr("Torque Control Tune Version"),
+        button_text=self._get_current_torque_version_label,
+        callback=self._show_torque_version_dialog,
+    )
     self._self_tune_toggle = toggle_item_sp(
       param="LiveTorqueParamsToggle",
       title=lambda: tr("Self-Tune"),
@@ -73,6 +87,7 @@ class TorqueSettingsLayout(Widget):
     )
 
     items = [
+      _torque_control_versions,
       self._self_tune_toggle,
       self._relaxed_tune_toggle,
       self._custom_tune_toggle,
@@ -113,3 +128,58 @@ class TorqueSettingsLayout(Widget):
 
   def show_event(self):
     self._scroller.show_event()
+
+  def _get_current_torque_version_label(self):
+    current_val_bytes = ui_state.params.get("TorqueControlTune")
+    if current_val_bytes is None:
+      return tr("Default")
+
+    try:
+      current_val = float(current_val_bytes)
+      with open(TORQUE_VERSIONS_PATH) as f:
+        versions = json.load(f)
+
+      for label, info in versions.items():
+        if math.isclose(float(info["version"]), current_val, rel_tol=1e-5):
+          return label
+    except (ValueError, json.JSONDecodeError, TypeError, FileNotFoundError, KeyError):
+      pass
+
+    return tr("Default")
+
+  def _show_torque_version_dialog(self):
+    try:
+      with open(TORQUE_VERSIONS_PATH) as f:
+        versions = json.load(f)
+    except (json.JSONDecodeError, TypeError, FileNotFoundError):
+      versions = {}
+
+    options_map = {}
+    for label, info in versions.items():
+      try:
+        options_map[label] = float(info["version"])
+      except (ValueError, KeyError):
+        pass
+
+    # Sort options by label in descending order
+    sorted_labels = sorted(options_map.keys(), reverse=True)
+    options = [tr("Default")] + sorted_labels
+
+    current_label = self._get_current_torque_version_label()
+
+    def handle_selection(result: int):
+      if result == DialogResult.CONFIRM and self._torque_version_dialog:
+        selected = self._torque_version_dialog.selection
+        if selected == tr("Default"):
+          ui_state.params.remove("TorqueControlTune")
+        elif selected in options_map:
+          ui_state.params.put("TorqueControlTune", options_map[selected])
+      self._torque_version_dialog = None
+
+    self._torque_version_dialog = MultiOptionDialog(
+      tr("Select Torque Control Tune Version"),
+      options,
+      current_label,
+      option_font_weight=FontWeight.UNIFONT
+    )
+    gui_app.set_modal_overlay(self._torque_version_dialog, callback=handle_selection)
