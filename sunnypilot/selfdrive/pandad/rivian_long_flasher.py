6fb11fd0a379bca4dc7930ae7c7b03937feb76e4
Jason Wen <haibin.wen3@gmail.com>
2026-02-25 02:16:25 -0500

    use brand field directly

--- sunnypilot/selfdrive/pandad/rivian_long_flasher.py
+++ sunnypilot/selfdrive/pandad/rivian_long_flasher.py
@@ -13,8 +13,7 @@ from panda import Panda
 from openpilot.common.params import Params
 from openpilot.common.swaglog import cloudlog
 
-FW_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                       "rivian_long_fw.bin.signed")
+FW_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "rivian_long_fw.bin.signed")
 SECTOR_SIZES = [0x4000] * 4 + [0x10000] + [0x20000] * 11
 
 
