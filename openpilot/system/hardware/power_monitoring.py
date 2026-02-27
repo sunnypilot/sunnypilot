60368aca4f90a2d8bac7b7c308e4538bd2c06aa6
Christopher Haucke <132518562+CHaucke89@users.noreply.github.com>
2026-02-27 06:38:03 -0500

    Custom shutdown voltage logic

--- openpilot/system/hardware/power_monitoring.py
+++ openpilot/system/hardware/power_monitoring.py
@@ -119,6 +119,15 @@ class PowerMonitoring:
 
     return 0 < sp_max_time_val_s <= offroad_time
 
+  def battery_voltage_below_threshold(self, car_voltage):
+    try:
+      param = self.params.get("CustomShutdownVoltage", return_default=True)
+      low_voltage_custom = param * 1e3 if param is not None and param > VBATT_PAUSE_CHARGING else VBATT_PAUSE_CHARGING * 1e3
+    except Exception:
+      low_voltage_custom = VBATT_PAUSE_CHARGING * 1e3
+
+    return car_voltage <= low_voltage_custom and param >= VBATT_PAUSE_CHARGING
+
   # See if we need to shutdown
   def should_shutdown(self, ignition: bool, in_car: bool, offroad_timestamp: float | None, started_seen: bool):
     if offroad_timestamp is None:
@@ -127,7 +136,7 @@ class PowerMonitoring:
     now = time.monotonic()
     should_shutdown = False
     offroad_time = (now - offroad_timestamp)
-    low_voltage_shutdown = (self.car_voltage_mV < (VBATT_PAUSE_CHARGING * 1e3) and
+    low_voltage_shutdown = (self.battery_voltage_below_threshold(self.car_voltage_mV) and
                             offroad_time > VOLTAGE_SHUTDOWN_MIN_OFFROAD_TIME_S)
     should_shutdown |= self.max_time_offroad_exceeded(offroad_time)
     should_shutdown |= low_voltage_shutdown
