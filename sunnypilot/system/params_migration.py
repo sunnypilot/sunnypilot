3a9f2e1f9699e16c922d13a6087b98205b3600c6
Jason Wen <haibin.wen3@gmail.com>
2026-02-28 00:01:40 -0500

    comment

--- sunnypilot/system/params_migration.py
+++ sunnypilot/system/params_migration.py
@@ -14,13 +14,14 @@ def run_migration(_params):
   if _params.get("OnroadScreenOffBrightnessMigrated") != ONROAD_BRIGHTNESS_MIGRATION_VERSION:
     try:
       val = _params.get("OnroadScreenOffBrightness")
-      if val >= 2:
+      if val >= 2:  # old: 5%, new: Screen Off
         new_val = val + 1
         _params.put("OnroadScreenOffBrightness", new_val)
-        _params.put("OnroadScreenOffBrightnessMigrated", ONROAD_BRIGHTNESS_MIGRATION_VERSION)
         log_str = f"Successfully migrated OnroadScreenOffBrightness from {val} to {new_val}."
       else:
         log_str = "Migration not required for OnroadScreenOffBrightness."
+
+      _params.put("OnroadScreenOffBrightnessMigrated", ONROAD_BRIGHTNESS_MIGRATION_VERSION)
       cloudlog.info(log_str + f" Setting OnroadScreenOffBrightnessMigrated to {ONROAD_BRIGHTNESS_MIGRATION_VERSION}")
     except Exception as e:
       cloudlog.exception(f"Error migrating OnroadScreenOffBrightness: {e}")
