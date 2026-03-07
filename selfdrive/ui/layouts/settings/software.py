68cbccd55e82e4104ff340eceacedf365b4636c9
Jason Wen <haibin.wen3@gmail.com>
2026-03-07 01:34:53 -0500

    [TIZI/TICI] ui: branch switcher is always available

--- selfdrive/ui/layouts/settings/software.py
+++ selfdrive/ui/layouts/settings/software.py
@@ -69,7 +69,6 @@ class SoftwareLayout(Widget):
 
     # Branch switcher
     self._branch_btn = button_item(lambda: tr("Target Branch"), lambda: tr("SELECT"), callback=self._on_select_branch)
-    self._branch_btn.set_visible(not ui_state.params.get_bool("IsTestedBranch"))
     self._branch_btn.action_item.set_value(ui_state.params.get("UpdaterTargetBranch") or "")
     self._branch_dialog: MultiOptionDialog | None = None
 
