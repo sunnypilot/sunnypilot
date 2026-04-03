45d629a9e4c8a4a9211a2c23bd23a85caca6e51f
J506918 <763986756@qq.com>
2026-04-03 10:38:29 +0800

    Add files via upload
    添加一组11代雅阁指纹

--- opendbc_repo/opendbc/car/honda/fingerprints.py
+++ opendbc_repo/opendbc/car/honda/fingerprints.py
@@ -1035,11 +1035,13 @@ FW_VERSIONS = {
   CAR.HONDA_ACCORD_11G: {
     (Ecu.fwdRadar, 0x18dab0f1, None): [
       b'8S302-30A-A040\x00\x00',
+      b'8S302-30E-H040\x00\x00',
     ],
     (Ecu.fwdCamera, 0x18dab5f1, None): [
       b'8S102-30A-A050\x00\x00',
       b'8S102-30A-A060\x00\x00',
       b'8S102-30A-A070\x00\x00',
+      b'8S102-30E-H040\x00\x00',
     ],
   },
   CAR.HONDA_CRV_6G: {
