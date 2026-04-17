270d00fd8851014eb41f5f71a7c9b9c7ee5e72c0
Jason Wen <haibin.wen3@gmail.com>
2026-04-17 02:20:20 -0400

    controls: always default Torque Lateral Control to v0 Tune

--- sunnypilot/selfdrive/controls/controlsd_ext.py
+++ sunnypilot/selfdrive/controls/controlsd_ext.py
@@ -38,7 +38,7 @@ class ControlsExt(ModelStateBase):
     enforce_torque_control = self.params.get_bool("EnforceTorqueControl")
     torque_versions = self.params.get("TorqueControlTune")
     if not enforce_torque_control:
-      return lac
+      return LatControlTorqueV0(self.CP, self.CP_SP, CI, dt)
 
     if torque_versions == 0.0:  # v0
       return LatControlTorqueV0(self.CP, self.CP_SP, CI, dt)
