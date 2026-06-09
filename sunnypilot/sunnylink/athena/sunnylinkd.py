afae872023634aa9032966a71c22354ac8205838
Jason Wen <haibin.wen3@gmail.com>
2026-06-08 23:40:58 -0400

    sunnylink: deprecate legacy params metadata

--- sunnypilot/sunnylink/athena/sunnylinkd.py
+++ sunnypilot/sunnylink/athena/sunnylinkd.py
@@ -41,7 +41,6 @@ LOCAL_PORT_WHITELIST = {8022}
 SUNNYLINK_LOG_ATTR_NAME = "user.sunny.upload"
 SUNNYLINK_RECONNECT_TIMEOUT_S = 70  # FYI changing this will also would require a change on sidebar.cc
 DISALLOW_LOG_UPLOAD = threading.Event()
-METADATA_PATH = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "params_metadata.json")
 
 params = Params()
 
@@ -168,39 +167,6 @@ def toggleLogUpload(enabled: bool):
 def getParamsAllKeys() -> list[str]:
   keys: list[str] = [k.decode('utf-8') for k in Params().all_keys()]
   return keys
-
-
-@dispatcher.add_method
-def getParamsAllKeysV1() -> dict[str, str]:
-  try:
-    with open(METADATA_PATH) as f:
-      metadata = json.load(f)
-  except Exception:
-    cloudlog.exception("sunnylinkd.getParamsAllKeysV1.metadata.exception")
-    metadata = {}
-
-  try:
-    available_keys: list[str] = [k.decode('utf-8') for k in Params().all_keys()]
-
-    params_dict: dict[str, list[dict[str, str | bool | int | object | dict | None]]] = {"params": []}
-    for key in available_keys:
-      value = get_param_as_byte(key, get_default=True)
-
-      param_entry = {
-        "key": key,
-        "type": int(params.get_type(key).value),
-        "default_value": base64.b64encode(value).decode('utf-8') if value else None,
-      }
-
-      if key in metadata:
-        meta_copy = metadata[key].copy()
-        param_entry["_extra"] = meta_copy
-
-      params_dict["params"].append(param_entry)
-    return {"keys": json.dumps(params_dict.get("params", []))}
-  except Exception:
-    cloudlog.exception("sunnylinkd.getParamsAllKeysV1.exception")
-    raise
 
 
 @dispatcher.add_method
