1ec96f97a7a2b51c3671b41128c381e7bd5605bf
royjr <royjr96@gmail.com>
2026-07-15 14:33:31 -0400

    small white

--- selfdrive/ui/sunnypilot/onroad/radar_tracks.py
+++ selfdrive/ui/sunnypilot/onroad/radar_tracks.py
@@ -55,4 +55,4 @@ class RadarTracks:
       x, y = pt[0] + screen_offset[0], pt[1] + screen_offset[1]
       color = radar_track_color(v_rel, v_ego)
       neutral = (color.r, color.g, color.b) == NEUTRAL_COLOR
-      rl.draw_circle(int(x), int(y), max(1, track_size - 2) if neutral else track_size, color)
+      rl.draw_circle(int(x), int(y), max(1, track_size - 3) if neutral else track_size, color)
