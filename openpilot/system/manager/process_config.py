f166bfcdc5b96b085dba32b9dd0ee0ddae2510a9
royjr <royjr96@gmail.com>
2026-07-25 02:53:13 -0400

    Update process_config.py

--- openpilot/system/manager/process_config.py
+++ openpilot/system/manager/process_config.py
@@ -131,7 +131,7 @@ procs = [
   PythonProcess("micd", "openpilot.system.micd", iscar),
   PythonProcess("timed", "openpilot.system.timed", always_run, enabled=not PC),
 
-  PythonProcess("modeld", "openpilot.selfdrive.modeld.modeld", and_(only_onroad, is_stock_model, not_wgpu)),
+  PythonProcess("modeld", "openpilot.selfdrive.modeld.modeld", and_(and_(only_onroad, is_stock_model), not_wgpu)),
   PythonProcess("dmonitoringmodeld", "openpilot.selfdrive.modeld.dmonitoringmodeld", driverview, enabled=(WEBCAM or not PC)),
 
   PythonProcess("sensord", "openpilot.system.sensord.sensord", only_onroad, enabled=not PC),
