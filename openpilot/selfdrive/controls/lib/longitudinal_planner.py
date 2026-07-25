0265ae5f7629f464daecdba562962a3f33c29bb9
Jason Wen <haibin.wen3@gmail.com>
2026-07-25 01:27:25 -0400

    Revert "plannerd: check all services for validity (#38341)"
    This reverts commit a9ffebe96e88a664ce71e76a86f7b3b6719eb3f4.

--- openpilot/selfdrive/controls/lib/longitudinal_planner.py
+++ openpilot/selfdrive/controls/lib/longitudinal_planner.py
@@ -158,7 +158,7 @@ class LongitudinalPlanner(LongitudinalPlannerSP):
   def publish(self, sm, pm):
     plan_send = messaging.new_message('longitudinalPlan')
 
-    plan_send.valid = sm.all_checks()
+    plan_send.valid = sm.all_checks(service_list=['carState', 'controlsState', 'selfdriveState', 'radarState'])
 
     longitudinalPlan = plan_send.longitudinalPlan
     longitudinalPlan.modelMonoTime = sm.logMonoTime['modelV2']
