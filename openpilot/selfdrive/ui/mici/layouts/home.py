872d25d7cba9ca12fd2b4207fd39d81525fa857e
royjr <royjr96@gmail.com>
2026-07-25 03:16:04 -0400

    icon

--- openpilot/selfdrive/ui/mici/layouts/home.py
+++ openpilot/selfdrive/ui/mici/layouts/home.py
@@ -248,8 +248,10 @@ class MiciHomeLayout(Widget):
 
     # ***** Center-aligned bottom section icons *****
     self._experimental_icon.set_visible(ui_state.experimental_mode)
-    self._egpu_icon.set_visible(ui_state.usbgpu and ui_state.usbgpu_compiled)
-    self._egpu_icon_gray.set_visible(ui_state.usbgpu and not ui_state.usbgpu_compiled)
+    wgpu_running = ui_state.wgpu_enabled and ui_state.sm.alive["modelV2"] and ui_state.sm.valid["modelV2"]
+    self._egpu_icon.set_visible((ui_state.usbgpu and ui_state.usbgpu_compiled) or wgpu_running)
+    self._egpu_icon_gray.set_visible((ui_state.usbgpu and not ui_state.usbgpu_compiled) or
+                                     (ui_state.wgpu_enabled and not wgpu_running))
     self._mic_icon.set_visible(ui_state.recording_audio)
     self._body_icon.set_visible(bool(ui_state.is_body))
 
