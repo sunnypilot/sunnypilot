2a8a6607ded204eb468921a1c4aa8c8898adddde
nayan <nayan8teen@gmail.com>
2026-07-25 20:22:04 -0400

    fix paths

--- openpilot/sunnypilot/models/fetcher.py
+++ openpilot/sunnypilot/models/fetcher.py
@@ -11,7 +11,7 @@ import requests
 from requests.exceptions import (SSLError, RequestException, HTTPError)
 from openpilot.common.params import Params
 from openpilot.common.swaglog import cloudlog
-from openpilot.system.hardware.hw import Paths
+from openpilot.common.hardware.hw import Paths
 from openpilot.sunnypilot.models.helpers import is_bundle_version_compatible
 
 from openpilot.cereal import custom
