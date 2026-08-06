2e7f65e81bfc4b28b2a3f6c432f1b701d25ea064
plin924 <117323804+plin924@users.noreply.github.com>
2026-08-05 18:33:36 -0700

    Update Mazda interface for additional model support
    Added CAR.MAZDA_6 to conditions for dashcamOnly and minSteerSpeed.

--- opendbc_repo/opendbc/car/mazda/interface.py
+++ opendbc_repo/opendbc/car/mazda/interface.py
@@ -17,14 +17,15 @@ class CarInterface(CarInterfaceBase):
     ret.safetyConfigs = [get_safety_config(structs.CarParams.SafetyModel.mazda)]
     ret.radarUnavailable = True
 
-    ret.dashcamOnly = candidate not in (CAR.MAZDA_CX5_2022, CAR.MAZDA_CX9_2021)
+    ret.dashcamOnly = candidate not in (CAR.MAZDA_CX5_2022, CAR.MAZDA_CX9_2021, CAR.MAZDA_6)
 
     ret.steerActuatorDelay = 0.1
     ret.steerLimitTimer = 0.8
 
     CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
 
-    if candidate not in (CAR.MAZDA_CX5_2022,):
+    # UPDATED LINE BELOW: Add CAR.MAZDA_6 to prevent setting minSteerSpeed lockout
+    if candidate not in (CAR.MAZDA_CX5_2022, CAR.MAZDA_6):
       ret.minSteerSpeed = LKAS_LIMITS.DISABLE_SPEED * CV.KPH_TO_MS
 
     ret.centerToFront = ret.wheelbase * 0.41
