8778c914bc143ba8d7a902461cad5b1de0de1e28
Robin Dittrich <r.dittrich@niverplast.com>
2026-08-12 14:00:22 +0200

    helpers.py get_lat_delay fix

--- openpilot/sunnypilot/livedelay/helpers.py
+++ openpilot/sunnypilot/livedelay/helpers.py
@@ -8,7 +8,9 @@ from openpilot.common.params import Params
 
 
 def get_lat_delay(params: Params, stock_lat_delay: float) -> float:
+  # live learning on: use what lagd publishes. 
+  # off: use the fixed steerActuatorDelay + software delay sum that LagdToggle caches.
   if params.get_bool("LagdToggle"):
-    return float(params.get("LagdValueCache", return_default=True))
+    return stock_lat_delay
 
-  return stock_lat_delay
+  return float(params.get("LagdValueCache", return_default=True))
