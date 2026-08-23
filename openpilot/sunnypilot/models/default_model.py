0de7fbf33d65b052efec8d5d1e2d810c3e5b48b2
Jason Wen <haibin.wen3@gmail.com>
2026-08-23 15:06:53 -0400

    new

--- openpilot/sunnypilot/models/default_model.py
+++ openpilot/sunnypilot/models/default_model.py
@@ -9,7 +9,7 @@ from openpilot.sunnypilot.models.model_name import DEFAULT_MODEL, DEFAULT_BIG_MO
 
 
 def get_default_model() -> str:
-  show_big_model = (ui_state.usbgpu and ui_state.usbgpu_compiled
+  show_big_model = (ui_state.usbgpu
                     and (ui_state.usbgpu_active or ui_state.usbgpu_loading or ui_state.is_offroad()))
 
   return DEFAULT_BIG_MODEL if show_big_model else DEFAULT_MODEL
