dc0a1b0dcae1a9da57d2365c0bee1208bb1fb958
royjr <royjr96@gmail.com>
2026-08-24 01:17:32 -0400

    Update selfdrived.py

--- openpilot/selfdrive/selfdrived/selfdrived.py
+++ openpilot/selfdrive/selfdrived/selfdrived.py
@@ -575,7 +575,7 @@ class SelfdriveD(CruiseHelper):
     clear_event_types = set()
     if ET.WARNING not in self.state_machine.current_alert_types:
       clear_event_types.add(ET.WARNING)
-    if self.enabled or self.mads.enabled:
+    if self.enabled or ET.USER_DISABLE in self.state_machine.current_alert_types:
       clear_event_types.add(ET.NO_ENTRY)
 
     pers = LONGITUDINAL_PERSONALITY_MAP[self.personality]
