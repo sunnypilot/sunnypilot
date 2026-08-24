01b3d3932b4850d5d21a98f65ab7683e17672cce
royjr <royjr96@gmail.com>
2026-08-23 22:29:49 -0400

    fade 100

--- openpilot/selfdrive/ui/sunnypilot/mici/onroad/hud_renderer.py
+++ openpilot/selfdrive/ui/sunnypilot/mici/onroad/hud_renderer.py
@@ -16,6 +16,8 @@ class HudRendererSP(HudRenderer):
   def __init__(self):
     super().__init__()
     self.blind_spot_indicators = BlindSpotIndicators()
+    self._egpu_load_prev: bool = False
+    self._egpu_done_time: float = 0.0
 
   def _update_state(self) -> None:
     super()._update_state()
@@ -29,22 +31,35 @@ class HudRendererSP(HudRenderer):
     self._egpu_fade_time = rl.get_time() - SET_SPEED_PERSISTENCE / 2
     super()._draw_model_source(rect)
 
-    if not ui_state.usbgpu_loading or ui_state.sm.recv_frame['selfdriveState'] < ui_state.started_frame:
+    if ui_state.sm.recv_frame['selfdriveState'] < ui_state.started_frame:
+      return
+
+    loading = ui_state.usbgpu_loading
+    if not loading and self._egpu_load_prev:
+      self._egpu_done_time = rl.get_time()
+    self._egpu_load_prev = loading
+
+    if loading:
+      pct, alpha = ui_state.usbgpu_load_progress, 1.0
+    elif self._egpu_done_time and (dt := rl.get_time() - self._egpu_done_time) < SET_SPEED_PERSISTENCE:
+      pct, alpha = 100, 1.0 - dt / SET_SPEED_PERSISTENCE
+    else:
       return
 
     icon = self._txt_egpu
     pos = rl.Vector2(rect.x + rect.width - 10 - icon.width,
                      rect.y + rect.height - 14 - (self._txt_wheel.height + icon.height) / 2)
 
-    pct_text = f"{ui_state.usbgpu_load_progress}%"
+    pct_text = f"{pct}%"
     size = FONT_SIZES.max_speed
+    color = rl.Color(255, 255, 255, int(255 * alpha))
     cell = measure_text_cached(self._font_bold, "0", size)
     widths = [cell.x if c.isdigit() else measure_text_cached(self._font_bold, c, size).x for c in pct_text]
     x = pos.x - 8 - sum(widths)
     y = pos.y + (icon.height - cell.y) / 2
     for c, w in zip(pct_text, widths, strict=True):
       glyph = measure_text_cached(self._font_bold, c, size).x
-      rl.draw_text_ex(self._font_bold, c, rl.Vector2(x + (w - glyph) / 2, y), size, 0, rl.WHITE)
+      rl.draw_text_ex(self._font_bold, c, rl.Vector2(x + (w - glyph) / 2, y), size, 0, color)
       x += w
 
   def _has_blind_spot_detected(self) -> bool:
